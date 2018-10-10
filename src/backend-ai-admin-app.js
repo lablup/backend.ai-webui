bd5592d204e1d6240cf74abb815433ba142d9eda
Jeongkyu Shin <inureyes@gmail.com>
2018-10-10 18:12:57 +0900

    refs #2 : add - migrate current backend.ai layout

--- src/backend-ai-admin-app.js
+++ src/backend-ai-admin-app.js
@@ -11,7 +11,11 @@ import '@polymer/paper-styles/default-theme.js';
 import '@polymer/paper-styles/typography.js';
 import '@polymer/paper-styles/color.js';
 import '@polymer/paper-material/paper-material.js';
-
+import '@polymer/paper-listbox/paper-listbox.js';
+import '@polymer/paper-item/paper-item.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@polymer/iron-icons/iron-icons.js';
+import './polymer.style.js';
 
 class BackendAiAdminApp extends PolymerElement {
   static get properties() {
@@ -32,11 +36,7 @@ class BackendAiAdminApp extends PolymerElement {
 
   static get template() {
     return html`
-    <style is="custom-style" include="paper-material-styles"> 
-      app-header {
-        background-color: #00897B;
-        color: #fff;
-      }
+    <style is="custom-style" include="backend-ai-styles paper-material-styles"> 
       paper-icon-button {
         --paper-icon-button-ink-color: white;
       }
@@ -47,20 +47,82 @@ class BackendAiAdminApp extends PolymerElement {
         --app-drawer-width: 150px;
       }
       </style>
-    <app-drawer-layout>
-      <app-drawer slot="drawer">
-        drawer menus
+      <app-drawer-layout id="app-body" responsive-width="900px" drawer-width="200px">
+      <app-drawer swipe-open slot="drawer" class="drawer-menu">
+          <app-header-layout has-scrolling-region class="vertical layout">
+              <app-header id="portrait-bar" slot="header" effects="waterfall" fixed>
+                  <div class="horizontal layout flex center bar">
+                      <div class="portrait-canvas">
+                          <iron-image width=43 height=43 style="width:43px; height:43px;" src="common/img/logo-black.svg"
+                                      sizing="contain"></iron-image>
+                      </div>
+                      <span class="site-name">cloud.<span class="bold">backend</span>.ai</span>
+                      <span class="flex"></span>
+                  </div>
+              </app-header>
+                <paper-listbox id="sidebar-menu" class="sidebar list" selected="0">
+                    <paper-item link>
+                        <iron-icon id="activities-icon" class="orange" icon="icons:view-quilt"></iron-icon>
+                        Summary</paper-item>
+                    <paper-item link>
+                        <iron-icon class="fg red" icon="icons:subject"></iron-icon>
+                        Jobs
+                    </paper-item>
+                    <paper-item link>
+                        <iron-icon class="fg green" icon="icons:pageview"></iron-icon>
+                        Experiments
+                    </paper-item>
+                    <paper-item disabled>
+                        <iron-icon icon="icons:assessment"></iron-icon>
+                        Statistics
+                    </paper-item>
+                    <paper-item link>
+                        <iron-icon class="fg blue" icon="icons:build"></iron-icon>
+                        Settings
+                        <span class="flex"></span>
+                    </paper-item>
+                </paper-listbox>
+              </neon-animated-pages>
+              <footer>
+                  <div class="terms-of-use" style="margin-bottom:50px;">
+                      <small>
+                          <a href="https://cloud.backend.ai/@lablupinc/terms-of-service-payment">Terms of Service</a>
+                          ·
+                          <a href="https://cloud.backend.ai/@lablupinc/privacy-policy">Privacy Policy</a>
+                      </small>
+                  </div>
+              </footer>
+              <div id="sidebar-navbar-footer" class="horizontal center center-justified layout">
+                  <address>
+                  </address>
+              </div>
+          </app-header-layout>
       </app-drawer>
-      <app-header-layout>
-        <app-header slot="header">
-          <app-toolbar>
-            <paper-icon-button icon="menu" drawer-toggle></paper-icon-button>
-            <div main-title>Backend.AI</div>
-          </app-toolbar>
-        </app-header>
-        <paper-material elevation="2">TEST</paper-material>
+      <app-header-layout main id="main-panel">
+          <app-header slot="header" id="main-toolbar" condenses reveals
+                      effects="waterfall blend-background"
+                      effects-config='{"resize-snapped-title": {"startsAt": 0.8, "duration": "100ms"}, "parallax-background": {"scalar": 0.5}}'>
+              <app-toolbar primary style="height:80px;" class="bar">
+                  <paper-icon-button icon="menu" drawer-toggle></paper-icon-button>
+                  <span title id="main-panel-toolbar-title"></span>
+                  <span class="flex"></span>
+              </app-toolbar>
+          </app-header>
+          <div class="content">
+              <div id="navbar-top" class="navbar-top horizontal flex layout wrap">
+                TEST
+              </div>
+              <section id="content" class="container layout vertical center">
+                TEST
+              </section>
+              <app-toolbar id="app-navbar-footer" style="height:45px;" class="bar layout flex horizontal">
+                  <paper-icon-button icon="menu" drawer-toggle></paper-icon-button>
+                  <paper-icon-button id="back-button" icon="icons:arrow-back"></paper-icon-button>
+                  <div id="lablup-notification-navbar" style="color: #999999; font-size:10px;"></div>
+              </app-toolbar>
+          </div>
       </app-header-layout>
-    </app-drawer-layout>
+  </app-drawer-layout>
 
     `;
   }
