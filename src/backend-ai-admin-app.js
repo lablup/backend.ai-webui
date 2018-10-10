09ee9b4ae37b578591b460fdb23071215430ff47
Jeongkyu Shin <inureyes@gmail.com>
2018-10-10 16:19:40 +0900

    refs #2 : add - polymer components to decorate main skeleton

--- src/backend-ai-admin-app.js
+++ src/backend-ai-admin-app.js
@@ -7,6 +7,11 @@ import '@polymer/paper-checkbox/paper-checkbox.js';
 import { setPassiveTouchGestures } from '@polymer/polymer/lib/utils/settings';
 import '@polymer/app-layout/app-layout.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
+import '@polymer/paper-styles/default-theme.js';
+import '@polymer/paper-styles/typography.js';
+import '@polymer/paper-styles/color.js';
+import '@polymer/paper-material/paper-material.js';
+
 
 class BackendAiAdminApp extends PolymerElement {
   static get properties() {
@@ -27,7 +32,7 @@ class BackendAiAdminApp extends PolymerElement {
 
   static get template() {
     return html`
-    <style>
+    <style is="custom-style" include="paper-material-styles"> 
       app-header {
         background-color: #00897B;
         color: #fff;
@@ -38,7 +43,10 @@ class BackendAiAdminApp extends PolymerElement {
       app-drawer-layout:not([narrow]) [drawer-toggle] {
         display: none;
       }
-    </style>
+      :host {
+        --app-drawer-width: 150px;
+      }
+      </style>
     <app-drawer-layout>
       <app-drawer slot="drawer">
         drawer menus
@@ -50,9 +58,7 @@ class BackendAiAdminApp extends PolymerElement {
             <div main-title>Backend.AI</div>
           </app-toolbar>
         </app-header>
-  
-        main content
-    
+        <paper-material elevation="2">TEST</paper-material>
       </app-header-layout>
     </app-drawer-layout>
 
