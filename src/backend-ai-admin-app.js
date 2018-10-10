d909ea911064c84d94efd2cdd8775a9f26db1a0c
Jeongkyu Shin <inureyes@gmail.com>
2018-10-10 16:01:04 +0900

    refs #2 : add - test codes and packages for UI backbones.

--- src/backend-ai-admin-app.js
+++ src/backend-ai-admin-app.js
@@ -1,13 +1,15 @@
 /**
  */
 
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import { PolymerElement, html } from '@polymer/polymer';
 import '@polymer/polymer/lib/elements/dom-if.js';
 import '@polymer/paper-checkbox/paper-checkbox.js';
 import { setPassiveTouchGestures } from '@polymer/polymer/lib/utils/settings';
+import '@polymer/app-layout/app-layout.js';
+import '@polymer/paper-icon-button/paper-icon-button.js';
 
 class BackendAiAdminApp extends PolymerElement {
-  static get properties () {
+  static get properties() {
     return {
     };
   }
@@ -19,13 +21,41 @@ class BackendAiAdminApp extends PolymerElement {
     setPassiveTouchGestures(true);
   }
 
-  ready(){
+  ready() {
     super.ready();
   }
-  
-  static get template () {
+
+  static get template() {
     return html`
-      <h1>Backend.AI admin App</h1>
+    <style>
+      app-header {
+        background-color: #00897B;
+        color: #fff;
+      }
+      paper-icon-button {
+        --paper-icon-button-ink-color: white;
+      }
+      app-drawer-layout:not([narrow]) [drawer-toggle] {
+        display: none;
+      }
+    </style>
+    <app-drawer-layout>
+      <app-drawer slot="drawer">
+        drawer menus
+      </app-drawer>
+      <app-header-layout>
+        <app-header slot="header">
+          <app-toolbar>
+            <paper-icon-button icon="menu" drawer-toggle></paper-icon-button>
+            <div main-title>Backend.AI</div>
+          </app-toolbar>
+        </app-header>
+  
+        main content
+    
+      </app-header-layout>
+    </app-drawer-layout>
+
     `;
   }
 }
