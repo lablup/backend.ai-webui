9c605725e3708b2a5bdceb236aa4aa90c2b55583
Jeongkyu Shin <inureyes@gmail.com>
2018-10-12 13:32:38 +0900

    refs #3 : modify - testing hash function (WIP)

--- src/backend.ai-client.js
+++ src/backend.ai-client.js
@@ -325,12 +325,12 @@ class Client {
   }
 
   getAuthenticationString_new(method, queryString, dateValue, bodyValue) {
-    window.crypto.subtle.digest({name: "SHA-256",},new Uint8Array(bodyValue)).then((bodyHash)=>{
+    return window.crypto.subtle.digest({name: "SHA-256",},new Uint16Array(this.str2ab(bodyValue))).then((bodyHash)=>{
       return (method + '\n' + queryString + '\n' + dateValue + '\n'
       + 'host:' + this._config.endpointHost + '\n'
       + 'content-type:application/json' + '\n'
       + 'x-backendai-version:' + this._config.apiVersion + '\n'
-      + bodyHash);
+      + this.ab2str(bodyHash));
     });
   }
 
@@ -359,6 +359,19 @@ class Client {
   getByteLength(s,b,i,c){
     for(b=i=0;c=s.charCodeAt(i++);b+=c>>11?3:c>>7?2:1);
     return b;
+  }
+  
+  ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint16Array(buf));
+  }
+  
+  str2ab(str) {
+    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i=0, strLen=str.length; i<strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
   }
 }
 
