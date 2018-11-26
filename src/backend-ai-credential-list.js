8475e9bed06a5fd243a00762b558f07b29768905
Jeongkyu Shin <inureyes@gmail.com>
2018-11-26 13:04:18 +0900

    refs #5 : add - admin-console and credential modules (WIP)

---
+++ src/backend-ai-credential-list.js
@@ -0,0 +1,256 @@
+/**
+@license
+Copyright (c) 2015-2018 Lablup Inc. All rights reserved.
+ */
+
+import { PolymerElement, html } from '@polymer/polymer';
+import '@polymer/polymer/lib/elements/dom-if.js';
+import '@polymer/iron-ajax/iron-ajax';
+import '@polymer/paper-dialog/paper-dialog';
+import '@polymer/paper-icon-button/paper-icon-button';
+import '@polymer/iron-icon/iron-icon';
+import '@polymer/iron-icons/iron-icons';
+import '@polymer/iron-icons/hardware-icons';
+import '@polymer/iron-icons/av-icons';
+
+import '@vaadin/vaadin-grid/vaadin-grid.js';
+import '@polymer/paper-toast/paper-toast';
+import './backend-ai-styles.js';
+
+import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
+
+
+class BackendAICredentialList extends PolymerElement {
+    static get is() {
+        return 'backend-ai-credential-list';
+    }
+
+    static get properties() {
+        return {
+            condition: {
+                type: String,
+                default: 'running'  // finished, running, archived
+            },
+            jobs: {
+                type: Object,
+                value: {}
+            }
+        };
+    }
+
+    ready() {
+        super.ready();
+        document.addEventListener('backend-ai-connected', () => {
+            this._refreshJobData();
+        }, true);
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        afterNextRender(this, function () {
+            if (window.backendaiclient != undefined && window.backendaiclient != null) {
+                this._refreshJobData();
+            }
+        });
+    }
+    _refreshJobData() {
+        let status = 'RUNNING';
+        switch (this.condition) {
+            case 'running':
+                status = 'RUNNING';
+                break;
+            case 'finished':
+                status = 'TERMINATED';
+                break;
+            case 'archived':
+            default:
+                status = 'RUNNING';
+        };
+
+        let fields = ["access_key", "secret_key", 'is_active', 'is_admin'];
+        let q = `query($user_id: {0}, $is_active: Boolean) {` +
+            `  keypairs(user_id: $user_id, is_active: $is_active) {` +
+            `    ${fields.join(" ")}` +
+            `  }` +
+            '}';
+        let v = { 'user_id': user_id,
+            'is_active': is_active,
+        }
+
+        window.backendaiclient.gql(q, v).then(response => {
+            this.jobs = response;
+            setTimeout(() => { this._refreshJobData(status) }, 5000);
+            console.log(this.jobs);
+        }).catch(err => {
+            console.log(err);
+            if (err && err.message) {
+                this.$.notification.text = err.message;
+                this.$.notification.show();
+            }
+        });
+    }
+    _isRunning() {
+        return this.condition === 'running';
+    }
+    _byteToMB(value) {
+        return Math.floor(value / 1000000);
+    }
+
+    _elapsed(start, end) {
+        var startDate = new Date(start);
+        if (this.condition == 'running') {
+            var endDate = new Date();
+        } else {
+            var endDate = new Date(end);
+        }
+        var seconds = Math.floor((endDate.getTime() - startDate.getTime()) / 1000, -1);
+        if (this.condition == 'running') {
+            return 'Running ' + seconds + 'sec.';
+        } else {
+            return 'Reserved for ' + seconds + 'sec.';
+        }
+        return seconds;
+    }
+
+    _indexFrom1(index) {
+        return index + 1;
+    }
+
+    _terminateKernel(e) {
+        const termButton = e.target;
+        const controls = e.target.closest('#controls');
+        const kernelId = controls.kernelId;
+
+        window.backendaiclient.destroyKernel(kernelId).then((req) => {
+            termButton.setAttribute('disabled', '');
+            setNotification('Session will soon be terminated');
+        }).catch(err => {
+            this.$.notification.text = 'Problem occurred during termination.';
+            this.$.notification.show();
+        });
+    }
+    static get template() {
+        return html`
+        <style include="backend-ai-styles iron-flex iron-flex-alignment">
+            vaadin-grid {
+                border: 0;
+                font-size: 14px;
+            }
+            paper-item {
+                height: 30px;
+                --paper-item-min-height: 30px;
+            }
+            iron-icon {
+                width: 16px;
+                height: 16px;
+                min-width: 16px;
+                min-height: 16px;
+                padding: 0;
+            }
+            paper-icon-button {
+                --paper-icon-button: {
+                    width: 25px;
+                    height: 25px;
+                    min-width: 25px;
+                    min-height: 25px;
+                    padding: 3px;
+                    margin-right: 5px;
+                };
+            }
+            div.indicator,
+            span.indicator {
+                font-size: 9px;
+                margin-right: 5px;
+            }
+        </style>
+        <paper-toast id="notification" text="" horizontal-align="right"></paper-toast>
+        
+        <vaadin-grid theme="row-stripes column-borders compact" aria-label="Job list" items="[[jobs.compute_sessions]]">
+            <vaadin-grid-column width="40px" flex-grow="0" resizable>
+                <template class="header">#</template>
+                <template>[[_indexFrom1(index)]]</template>
+            </vaadin-grid-column>
+
+            <vaadin-grid-column resizable>
+                <template class="header">Job ID</template>
+                <template>
+                    <div class="indicator">[[item.sess_id]]</div>
+                </template>
+            </vaadin-grid-column>
+
+            <vaadin-grid-column resizable>
+                <template class="header">Starts</template>
+                <template>
+                    <div class="layout vertical">
+                        <span>[[item.created_at]]</span>
+                        <span class="indicator">([[_elapsed(item.created_at, item.terminated_at)]])</span>
+                    </div>
+                </template>
+            </vaadin-grid-column>
+
+            <vaadin-grid-column resizable>
+              <template class="header">Configuration</template>
+              <template>
+                  <div class="layout horizontal center flex">
+                      <iron-icon class="fg green" icon="hardware:developer-board"></iron-icon>
+                      <span>[[item.cpu_slot]]</span>
+                      <span class="indicator">slot</span>
+                      <iron-icon class="fg green" icon="hardware:memory"></iron-icon>
+                      <span>[[item.mem_slot]]</span>
+                      <span class="indicator">MB[[item.mem_unit]]</span>
+                      <template is="dom-if" if="[[item.gpu_slot]]">
+                        <iron-icon class="fg green" icon="icons:view-module"></iron-icon>
+                        <span>[[item.gpu_slot]]</span>
+                        <span class="indicator">vGPU</span>
+                      </template>
+                      <!-- <iron-icon class="fg yellow" icon="device:storage"></iron-icon> -->
+                      <!-- <span>[[item.storage_capacity]]</span> -->
+                      <!-- <span class="indicator">[[item.storage_unit]]</span> -->
+                  </div>
+              </template>
+            </vaadin-grid-column>
+
+            <vaadin-grid-column resizable>
+              <template class="header">Using</template>
+              <template>
+                  <div class="layout horizontal center flex">
+                      <iron-icon class="fg blue" icon="hardware:memory"></iron-icon>
+                      <div class="vertical start layout">
+                      <span>[[item.cpu_used]]</span>
+                      <span class="indicator">msec.</span>
+                      </div>
+                      <iron-icon class="fg blue" icon="hardware:device-hub"></iron-icon>
+                      <div class="vertical start layout">
+                        <span style="font-size:8px">[[_byteToMB(item.io_read_bytes)]]<span class="indicator">MB</span></span>
+                        <span style="font-size:8px">[[_byteToMB(item.io_write_bytes)]]<span class="indicator">MB</span></span>
+                      </div>
+                  </div>
+              </template>
+            </vaadin-grid-column>
+
+            <vaadin-grid-column resizable>
+              <template class="header">Control</template>
+              <template>
+                  <div id="controls" class="layout horizontal flex center"
+                       kernel-id="[[item.sess_id]]">
+                      <paper-icon-button disabled class="fg"
+                                         icon="assignment"></paper-icon-button>
+                      <template is="dom-if" if="[[_isRunning()]]">
+                          <paper-icon-button disabled class="fg controls-running"
+                                             icon="build"></paper-icon-button>
+                          <paper-icon-button disabled class="fg controls-running"
+                                             icon="alarm-add"></paper-icon-button>
+                          <paper-icon-button disabled class="fg controls-running"
+                                             icon="av:pause"></paper-icon-button>
+                          <paper-icon-button class="fg red controls-running" icon="delete"
+                            on-tap="_terminateKernel"></paper-icon-button>
+                      </template>
+                  </div>
+              </template>
+            </vaadin-grid-column>
+        </vaadin-grid>
+        `;
+    }
+}
+
+customElements.define(BackendAICredentialList.is, BackendAICredentialList);
---
+++ src/backend-ai-credential-view.js
@@ -0,0 +1,179 @@
+/**
+ * Backend.AI-job-view 
+ */
+
+import { PolymerElement, html } from '@polymer/polymer';
+import '@polymer/polymer/lib/elements/dom-if.js';
+import { setPassiveTouchGestures } from '@polymer/polymer/lib/utils/settings';
+import '@polymer/paper-icon-button/paper-icon-button';
+import '@polymer/paper-styles/typography';
+import '@polymer/paper-styles/color';
+import '@polymer/paper-material/paper-material';
+import '@polymer/iron-icon/iron-icon';
+import '@polymer/iron-icons/iron-icons';
+import '@polymer/iron-image/iron-image';
+import '@polymer/iron-flex-layout/iron-flex-layout';
+import '@polymer/iron-flex-layout/iron-flex-layout-classes';
+
+import '@polymer/paper-dialog/paper-dialog';
+import '@polymer/paper-button/paper-button';
+import '@polymer/paper-listbox/paper-listbox';
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu';
+import '@polymer/paper-item/paper-item';
+import '@polymer/neon-animation/animations/scale-up-animation.js';
+import '@polymer/neon-animation/animations/fade-out-animation.js';
+
+import './backend-ai-styles.js';
+import './backend-ai-credential-list.js';
+
+class BackendAICredentialView extends PolymerElement {
+  static get properties() {
+    return {
+    };
+  }
+  static get is() {
+      return 'backend-ai-credential-view';
+  }
+
+  constructor() {
+    super();
+    // Resolve warning about scroll performance 
+    // See https://developers.google.com/web/updates/2016/06/passive-event-listeners
+    setPassiveTouchGestures(true);
+  }
+
+  ready() {
+    super.ready();
+    this.$['launch-session'].addEventListener('tap', this._newSession.bind(this));
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    afterNextRender(this, function () {
+    });
+  }
+
+  static get observers() {
+    return [
+      '_routeChanged(route.*)',
+      '_viewChanged(routeData.view)'
+    ]
+  }
+  
+  _routeChanged(changeRecord) {
+    if (changeRecord.path === 'path') {
+      console.log('Path changed!');
+    }
+  }
+  _viewChanged(view) {
+    // load data for view
+  }
+  _newSession() {
+    this.$['new-session-dialog'].open();
+  }
+
+  _addKeyPair() {
+    let status = 'RUNNING';
+    switch (this.condition) {
+        case 'running':
+            status = 'RUNNING';
+            break;
+        case 'finished':
+            status = 'TERMINATED';
+            break;
+        case 'archived':
+        default:
+            status = 'RUNNING';
+    };
+
+    let fields = ["access_key", "secret_key"]
+    let q = `mutation($user_id: {0}, $input: KeyPairInput!) {{` +
+        `  create_keypair(user_id: $user_id, props: $input) {` +
+        `    ok msg keypair { ${fields.join(" ")} }` +
+        `  }` +
+        `}`;
+    let v = { 'user_id': user_id,
+        'input': {
+            'is_active': is_active,
+            'is_admin': is_admin,
+            'resource_policy': resource_policy,
+            'rate_limit': rate_limit,
+            'concurrency_limit': concurrency_limit,
+
+        },
+    };
+
+    window.backendaiclient.gql(q, v).then(response => {
+        this.jobs = response;
+        setTimeout(() => { this._refreshJobData(status) }, 5000);
+        console.log(this.jobs);
+    }).catch(err => {
+        console.log(err);
+        if (err && err.message) {
+            this.$.notification.text = err.message;
+            this.$.notification.show();
+        }
+    });
+}
+
+  static get template() {
+    return html`
+    <style is="custom-style" include="backend-ai-styles iron-flex iron-flex-alignment iron-positioning">
+      paper-button.launch-button {
+        width: 100%;
+      }
+    </style>
+    <paper-material class="item" elevation="1">
+        <h3 class="paper-material-title">Credentials</h3>
+        <h4 class="horizontal flex center-justified layout">
+            <span>Active</span>
+            <span class="flex"></span>
+            <paper-button id="launch-session" class="fg red">
+                <iron-icon icon="add"></iron-icon>
+                Add
+            </paper-button>
+        </h4>
+        <div>
+            <backend-ai-credential-list condition="active"></backend-ai-job-list>
+        </div>
+        <h4>Inactive</h4>
+        <div>
+            <backend-ai-credential-list condition="inactive"></backend-ai-job-list>
+        </div>
+    
+    </paper-material>
+    <paper-dialog id="new-session-dialog" entry-animation="scale-up-animation" exit-animation="fade-out-animation">
+        <paper-material elevation="1" class="login-panel intro centered" style="margin: 0;">
+            <h3>Start a new session</h3>
+            <form id="login-form" onSubmit="this._launchSession()">
+                <fieldset>
+                    <div class="horizontal center layout">
+                    <paper-dropdown-menu label="Environments">
+                        <paper-listbox slot="dropdown-content" selected="0">
+                            <paper-item>TensorFlow</paper-item>
+                            <paper-item>PyTorch</paper-item>
+                            <paper-item>Python</paper-item>
+                        </paper-listbox>
+                    </paper-dropdown-menu>
+                    <paper-dropdown-menu label="Version">
+                        <paper-listbox slot="dropdown-content" selected="0">
+                            <paper-item>Latest</paper-item>
+                            <paper-item>2</paper-item>
+                            <paper-item>1</paper-item>
+                        </paper-listbox>
+                    </paper-dropdown-menu>
+                    </div>
+                    <br /><br />
+                    <paper-button class="blue launch-button" type="submit" id="launch-button">
+                        <iron-icon icon="rowing"></iron-icon>
+                        Launch
+                    </paper-button>
+                </fieldset>
+            </form>
+        </paper-material>
+    </paper-dialog>
+    `;
+  }
+}
+
+customElements.define(BackendAICredentialView.is, BackendAICredentialView);
