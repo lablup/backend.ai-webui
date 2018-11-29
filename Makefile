131737598050c8d695894121d568ea009ea6fd0b
Jeongkyu Shin <inureyes@gmail.com>
2018-11-30 01:12:00 +0900

    refs #4: add - missing menu scripts

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 EP = electron-packager ./build/es6-unbundled --ignore=node_modules/electron-packager --icon=manifest/backend-ai.icns --ignore=.git --overwrite --ignore="\.git(ignore|modules)" --out=app
 
 mac:
-	cp ./main.electron-packager.js ./build/es6-unbundled/main.js
+	\cp ./main.electron-packager.js ./build/es6-unbundled/main.js
 	cd build/es6-unbundled
 	npm install
 	cd ../..
 	$(EP) --platform=darwin
+clean:
+	cd app
+	\rm -rf ./backend*
--- main.electron-packager.js
+++ main.electron-packager.js
@@ -1,16 +1,307 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, protocol } = require('electron')
+const {app, Menu, Shell, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+app.once('ready', function() {
+  var template;
+  if (process.platform == 'darwin') {
+    template = [
+      {
+        label: app.getName(),
+        submenu: [
+          {
+            label: 'About Backend.AI Admin Console',
+            selector: 'orderFrontStandardAboutPanel:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Services',
+            submenu: []
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Hide Backend.AI Admin Console',
+            accelerator: 'Command+H',
+            selector: 'hide:'
+          },
+          {
+            label: 'Hide Others',
+            accelerator: 'Command+Shift+H',
+            selector: 'hideOtherApplications:'
+          },
+          {
+            label: 'Show All',
+            selector: 'unhideAllApplications:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function() {
+              app.quit();
+            }
+          },
+        ]
+      },
+      {
+        label: 'Edit',
+        submenu: [
+          {
+            label: 'Undo',
+            accelerator: 'Command+Z',
+            selector: 'undo:'
+          },
+          {
+            label: 'Redo',
+            accelerator: 'Shift+Command+Z',
+            selector: 'redo:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Cut',
+            accelerator: 'Command+X',
+            selector: 'cut:'
+          },
+          {
+            label: 'Copy',
+            accelerator: 'Command+C',
+            selector: 'copy:'
+          },
+          {
+            label: 'Paste',
+            accelerator: 'Command+V',
+            selector: 'paste:'
+          },
+          {
+            label: 'Select All',
+            accelerator: 'Command+A',
+            selector: 'selectAll:'
+          },
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'Reload',
+            accelerator: 'Command+R',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow) {
+                focusedWindow.reload();
+              }
+            }
+          },
+          {
+            label: 'Zoom In',
+            accelerator: 'Command+=',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow && focusedWindow.webContents) {
+                focusedWindow.webContents.executeJavaScript('_zoomIn()');
+              }
+            }
+          },
+          {
+            label: 'Zoom Out',
+            accelerator: 'Command+-',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow && focusedWindow.webContents) {
+                focusedWindow.webContents.executeJavaScript('_zoomOut()');
+              }
+            }
+          },
+          {
+            label: 'Actual Size',
+            accelerator: 'Command+0',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow && focusedWindow.webContents) {
+                focusedWindow.webContents.executeJavaScript(
+                  '_zoomActualSize()');
+              }
+            }
+          },
+          {
+            label: 'Toggle Full Screen',
+            accelerator: 'Ctrl+Command+F',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow) {
+                focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+              }
+            }
+          },
+          {
+            label: 'Toggle Developer Tools',
+            accelerator: 'Alt+Command+I',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow) {
+                focusedWindow.toggleDevTools();
+              }
+            }
+          },
+        ]
+      },
+      {
+        label: 'Window',
+        submenu: [
+          {
+            label: 'Minimize',
+            accelerator: 'Command+M',
+            selector: 'performMiniaturize:'
+          },
+          {
+            label: 'Close',
+            accelerator: 'Command+W',
+            selector: 'performClose:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Bring All to Front',
+            selector: 'arrangeInFront:'
+          },
+        ]
+      },
+      {
+        label: 'Help',
+        submenu: [
+          {
+            label: 'Learn More',
+            click: function() {
+              Shell.openExternal('https://www.backend.ai/');
+            }
+          }
+        ]
+      }
+    ];
+  } else {
+    template = [
+      {
+        label: '&File',
+        submenu: [
+          {
+            label: '&Open',
+            accelerator: 'Ctrl+O',
+          },
+          {
+            label: '&Close',
+            accelerator: 'Ctrl+W',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow) {
+                focusedWindow.close();
+              }
+            }
+          },
+        ]
+      },
+      {
+        label: '&View',
+        submenu: [
+          {
+            label: '&Reload',
+            accelerator: 'Ctrl+R',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow) {
+                focusedWindow.reload();
+              }
+            }
+          },
+          {
+            label: 'Zoom In',
+            accelerator: 'Ctrl+=',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow && focusedWindow.webContents) {
+                focusedWindow.webContents.executeJavaScript('_zoomIn()');
+              }
+            }
+          },
+          {
+            label: 'Zoom Out',
+            accelerator: 'Ctrl+-',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow && focusedWindow.webContents) {
+                focusedWindow.webContents.executeJavaScript('_zoomOut()');
+              }
+            }
+          },
+          {
+            label: 'Actual Size',
+            accelerator: 'Ctrl+0',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow && focusedWindow.webContents) {
+                focusedWindow.webContents.executeJavaScript(
+                  '_zoomActualSize()');
+              }
+            }
+          },
+          {
+            label: 'Toggle &Full Screen',
+            accelerator: 'F11',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow) {
+                focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+              }
+            }
+          },
+          {
+            label: 'Toggle &Developer Tools',
+            accelerator: 'Alt+Ctrl+I',
+            click: function() {
+              var focusedWindow = BrowserWindow.getFocusedWindow();
+              if (focusedWindow) {
+                focusedWindow.toggleDevTools();
+              }
+            }
+          },
+        ]
+      },
+      {
+        label: 'Help',
+        submenu: [
+          {
+            label: 'Learn More',
+            click: function() {
+              Shell.openExternal('https://github.com/lablup/backend.ai');
+            }
+          }
+        ]
+      }
+    ];
+  }
+
+  var appmenu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(appmenu);
+});
+
 
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 1280, height: 1024})
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
