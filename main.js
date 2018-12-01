4df228aa173d96d9bf0b0c986ed47f22d415e9bc
Jeongkyu Shin <inureyes@gmail.com>
2018-12-01 22:52:43 +0900

    * Modify - desktop taskbar margin for older windows

--- main.js
+++ main.js
@@ -298,7 +298,7 @@ app.once('ready', function() {
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1280, height: 1024})
+  mainWindow = new BrowserWindow({width: 1280, height: 980})
   // and load the index.html of the app.
   mainWindow.loadFile('build/es6-unbundled/index.html')
   mainWindow.webContents.openDevTools();
