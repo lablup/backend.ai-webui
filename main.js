0e3e631888d830194484a2fa3f638c86e0464cb8
Jeongkyu Shin <inureyes@gmail.com>
2019-01-08 00:27:28 -1000

    * remove unnecessary comments

--- main.js
+++ main.js
@@ -352,9 +352,6 @@ function createWindow () {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
