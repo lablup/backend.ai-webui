e1717e8e956a20c62f7f99f25d2e84cc0452bb54
Jeongkyu Shin <inureyes@gmail.com>
2019-01-25 16:01:43 +0900

    refs #16 : add - data list panel webcomponent

---
+++ src/backend-ai-data-view.js
@@ -0,0 +1,223 @@
+/**
+ @license
+ Copyright (c) 2015-2019 Lablup Inc. All rights reserved.
+ */
+
+import {PolymerElement, html} from '@polymer/polymer';
+import '@polymer/polymer/lib/elements/dom-if.js';
+import {setPassiveTouchGestures} from '@polymer/polymer/lib/utils/settings';
+import '@polymer/paper-icon-button/paper-icon-button';
+import '@polymer/paper-styles/typography';
+import '@polymer/paper-styles/color';
+import '@polymer/paper-material/paper-material';
+import '@polymer/iron-icon/iron-icon';
+import '@polymer/iron-icons/iron-icons';
+import '@polymer/iron-image/iron-image';
+import '@polymer/iron-flex-layout/iron-flex-layout';
+import '@polymer/iron-flex-layout/iron-flex-layout-classes';
+import '@polymer/paper-toast/paper-toast';
+import '@vaadin/vaadin-grid/vaadin-grid.js';
+
+import './backend-ai-styles.js';
+import './lablup-activity-panel.js';
+import {afterNextRender} from '@polymer/polymer/lib/utils/render-status.js';
+
+class BackendAIData extends PolymerElement {
+  static get properties() {
+    return {
+      folders: {
+        type: Object,
+        value: {}
+      },
+      is_admin: {
+        type: Boolean,
+        value: false
+      },
+      authenticated: {
+        type: Boolean,
+        value: false
+      },
+      visible: {
+        type: Boolean,
+        value: false
+      }
+    };
+  }
+
+  constructor() {
+    super();
+    // Resolve warning about scroll performance 
+    // See https://developers.google.com/web/updates/2016/06/passive-event-listeners
+    setPassiveTouchGestures(true);
+  }
+
+  ready() {
+    super.ready();
+    document.addEventListener('backend-ai-connected', () => {
+      this.is_admin = window.backendaiclient.is_admin;
+      this.authenticated = true;
+      this._refreshFolderList();
+    }, true);
+  }
+
+  static get observers() {
+    return [
+      '_routeChanged(route.*)',
+      '_viewChanged(routeData.view)',
+      '_menuChanged(visible)'
+    ]
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    afterNextRender(this, function () {
+    });
+  }
+
+  _refreshFolderList() {
+    let l = window.backendaiclient.vfolder.list();
+    l.then((value) => {
+      this.folders = value;
+      console.log(this.folders);
+    });
+  }
+
+  _routeChanged(changeRecord) {
+    if (changeRecord.path === 'path') {
+      console.log('Path changed!');
+    }
+  }
+
+  _viewChanged(view) {
+    // load data for view
+  }
+
+  _menuChanged(visible) {
+    if (!visible) {
+    }
+  }
+
+  _countObject(obj) {
+    return Object.keys(obj).length;
+  }
+
+  _indexFrom1(index) {
+    return index + 1;
+  }
+
+  _hasPermission(item, perm) {
+    if (item.permission.includes(perm)) {
+      return true;
+    }
+    return false;
+  }
+  _deleteFolder() {
+
+  }
+
+  static get template() {
+    // language=HTML
+    return html`
+      <style is="custom-style" include="backend-ai-styles iron-flex iron-flex-alignment iron-positioning">
+        ul {
+          padding-left: 0;
+        }
+
+        ul li {
+          list-style: none;
+          font-size: 13px;
+        }
+
+        span.indicator {
+          width: 100px;
+        }
+      </style>
+      <paper-toast id="notification" text="" horizontal-align="right"></paper-toast>
+      <paper-material class="item" elevation="1" style="padding-bottom:20px;">
+        <h3 class="paper-material-title">Data</h3>
+        <h4 class="horizontal center layout">
+          <span>Virtual Folders</span>
+          <paper-button id="add-folder" class="fg red">
+            <iron-icon icon="add"></iron-icon>
+            Add new folder
+          </paper-button>
+        </h4>
+
+        <vaadin-grid theme="row-stripes column-borders compact" aria-label="Folder list" items="[[folders]]">
+          <vaadin-grid-column width="40px" flex-grow="0" resizable>
+            <template class="header">#</template>
+            <template>[[_indexFrom1(index)]]</template>
+          </vaadin-grid-column>
+
+          <vaadin-grid-column resizable>
+            <template class="header">Folder Name</template>
+            <template>
+              <div class="indicator">[[item.name]]</div>
+            </template>
+          </vaadin-grid-column>
+
+          <vaadin-grid-column resizable>
+            <template class="header">id</template>
+            <template>
+              <div class="layout vertical">
+                <span>[[item.id]]</span>
+              </div>
+            </template>
+          </vaadin-grid-column>
+
+          <vaadin-grid-column resizable>
+            <template class="header">Host</template>
+            <template>
+              <div class="layout vertical">
+                <span>[[item.host]]</span>
+              </div>
+            </template>
+          </vaadin-grid-column>
+
+
+          <vaadin-grid-column width="80px" flex-grow="0" resizable>
+            <template class="header">Permission</template>
+            <template>
+              <div>
+                <template is="dom-if" if="[[_hasPermission(item, 'r')]]">
+                  <lablup-shields app="" color="green"
+                                  description="Read" ui="flat"></lablup-shields>
+                </template>
+                <template is="dom-if" if="[[_hasPermission(item, 'w')]]">
+                  <lablup-shields app="" color="blue"
+                                  description="Write" ui="flat"></lablup-shields>
+                </template>
+                <template is="dom-if" if="[[_hasPermission(item, 'd')]]">
+                  <lablup-shields app="" color="red"
+                                  description="Delete" ui="flat"></lablup-shields>
+                </template>
+              </div>
+            </template>
+          </vaadin-grid-column>
+          <vaadin-grid-column resizable>
+            <template class="header">Control</template>
+            <template>
+              <div id="controls" class="layout horizontal flex center"
+                   kernel-id="[[item.sess_id]]">
+                <paper-icon-button disabled class="fg"
+                                   icon="assignment"></paper-icon-button>
+                <template is="dom-if" if="[[_hasPermission(item, 'r')]]">
+                </template>
+                <template is="dom-if" if="[[_hasPermission(item, 'w')]]">
+                </template>
+                <template is="dom-if" if="[[_hasPermission(item, 'd')]]">
+                  <paper-icon-button class="fg red controls-running" icon="delete"
+                                     on-tap="_deleteFolder"></paper-icon-button>
+                </template>
+              </div>
+            </template>
+          </vaadin-grid-column>
+        </vaadin-grid>
+
+
+      </paper-material>
+    `;
+  }
+}
+
+customElements.define('backend-ai-data-view', BackendAIData);
