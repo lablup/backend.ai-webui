482375bfe9e60dd9d52cf5e595a3f1be9594cc7d
Jeongkyu Shin <inureyes@gmail.com>
2019-02-23 01:12:15 +0900

    refs #22 : modify - refer configuration for wsproxy

--- src/wsproxy/local_proxy.js
+++ src/wsproxy/local_proxy.js
@@ -1,6 +1,25 @@
-const DEBUG = process.env.DEBUG|| false;
-const port = process.env.PORT|| 5050;
+const DEBUG = process.env.DEBUG || false;
+const port = process.env.PORT || 5050;
 let web = null;
 console.log("unpacked");
 web = require('./web');
-web("0.0.0.0", port, "http://localhost");
+let proxyBaseURL, proxyListenIP;
+
+import( '../../config.js').then((config) => {
+  if (typeof config.proxyBaseURL === "undefined" || config.proxyBaseURL === '') {
+    proxyBaseURL = 'http://localhost';
+  } else {
+    proxyBaseURL = config.proxyURL;
+  }
+  if (typeof config.proxyListenIP === "undefined" || config.proxyListenIP === '') {
+    proxyListenIP = '0.0.0.0';
+  } else {
+    proxyListenIP = config.proxyListenIP;
+  }
+}).catch((err) => {   // Fallback
+  proxyBaseURL = 'http://localhost';
+  proxyListenIP = '0.0.0.0';
+});
+
+
+web(proxyListenIP, port, proxyBaseURL);
