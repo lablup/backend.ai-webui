632745a92a872e95cacb9862199060aaa054acb9
Jeongkyu Shin <inureyes@gmail.com>
2019-02-22 23:15:35 +0900

    refs #22 : bugfix - missing /

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
   //"proxyURL": "https://kcd2019.backend.ai/proxy/",
   //"apiEndpoint": "https://api-beta.backend.ai"
-  "proxyURL": "http://52.78.225.155"
+  "proxyURL": "http://52.78.225.155/"
 }
