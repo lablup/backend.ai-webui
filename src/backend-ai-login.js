8db25b43232fd9a0b8119b82b57e0a255009407a
Jeongkyu Shin <inureyes@gmail.com>
2019-02-22 15:01:15 +0900

    refs #28 : add - config for wsproxy

--- src/backend-ai-login.js
+++ src/backend-ai-login.js
@@ -118,10 +118,11 @@ class BackendAiLogin extends PolymerElement {
       window.backendaiclient.email = this.email;
       window.backendaiclient.is_admin = is_admin;
       window.backendaiclient.resource_policy = resource_policy;
-      if (proxyURL != undefined) {
-        window.backendaiclient._config.proxyURL = proxyURL;
-      } else {
+      if (typeof window.proxyURL === "undefined" || window.proxyURL === '') { // from config
         window.backendaiclient._config.proxyURL = 'http://127.0.0.1:5050/';
+      } else {
+        console.log(window.proxyURL);
+        window.backendaiclient._config.proxyURL = window.proxyURL;
       }
       var event = new CustomEvent("backend-ai-connected", {"detail": this.client});
       document.dispatchEvent(event);
