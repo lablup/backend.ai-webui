7aa8a398bc56bc14f39f80c4e6b2ca8f56892556
Jonghyun Park <adrysn@gmail.com>
2019-03-07 15:47:11 +0900

    Add ingen client for neumann support
    * Client and ClientConfig is inherited by IngenClient and
    IngenClientConfig.
    * IngenClient does not need user's credential information and
    auth-header.

---
+++ src/backend.ai-client-ingen.js
@@ -0,0 +1,68 @@
+'use babel';
+/*
+Backend.AI Node.JS / Javascript ES6 API Proxy Library for Ingen (v19.03b9)
+==========================================================================
+
+(C) Copyright 2016-2019 Lablup Inc.
+Licensed under MIT
+*/
+/* jshint esnext: true */
+
+const clientNode = require('./backend.ai-client-node');
+
+
+class IngenClientConfig extends clientNode.ClientConfig {
+  constructor(accessKey, secretKey, endpoint) {
+    accessKey = secretKey = endpoint = '';
+    super(accessKey, secretKey, endpoint);
+  }
+}
+
+class IngenClient extends clientNode.Client {
+  /* GraphQL requests */
+  gql(q, v) {
+    const query = {'query': q, 'variables': v};
+    const rqst = this.newSignedRequest('POST', '/ingen/graphql', query);
+    return this._wrapWithPromise(rqst);
+  }
+
+  /**
+   * Generate a RequestInfo object that can be passed to fetch() API.
+   * We don't need signing process since it will be dealt in ingen server,
+   * but we keep the method name to follow the parent's interface.
+   *
+   * @param {string} method - the HTTP method
+   * @param {string} queryString - the URI path and GET parameters
+   * @param {string} body - an object that will be encoded as JSON in the request body
+   *
+   * @return {Object} requestInfo - ingen request information
+   */
+  newSignedRequest(method, queryString, body) {
+    const requestBody = !body ? '' : JSON.stringify(body);
+    const requestInfo = {
+      method: method,
+      body: requestBody,
+      uri: queryString // endpoint will be determined in inge
+    };
+    return requestInfo;
+  }
+
+  hello() {
+    const rqst = this.newSignedRequest('GET', '/ingen');
+    return this._wrapWithPromise(rqst);
+  }
+}
+
+
+const backend = {
+  Client: IngenClient,
+  ClientConfig: IngenClientConfig,
+};
+
+module.exports.backend = backend;
+// for classical uses
+module.exports.Client = IngenClient;
+module.exports.ClientConfig = IngenClientConfig;
+// legacy aliases
+module.exports.BackendAIClient = IngenClient;
+module.exports.BackendAIClientConfig = IngenClientConfig;
--- src/compile_node.sh
+++ src/compile_node.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 browserify -s ai -o backend.ai-client-es6.js ./backend.ai-client-node.js
+browserify -s ai -o backend.ai-client-ingen-es6.js ./backend.ai-client-ingen.js
