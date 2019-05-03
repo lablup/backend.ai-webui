47793bd603e3f35d92d0f4f130d592848db72a54
Jeongkyu Shin <inureyes@gmail.com>
2019-05-03 12:31:24 +0900

    refs #55 : update - version number

--- src/components/backend-ai-console.js
+++ src/components/backend-ai-console.js
@@ -347,7 +347,7 @@ class BackendAiConsole extends connect(store)(LitElement) {
                 <div id="sidebar-navbar-footer" class="vertical center center-justified layout">
                   <address>
                     <small class="sidebar-footer">Lablup Inc.</small>
-                    <small class="sidebar-footer" style="font-size:9px;">19.03.1.190429</small>
+                    <small class="sidebar-footer" style="font-size:9px;">19.03.1.190503</small>
                   </address>
                 </div>
               </app-header-layout>
