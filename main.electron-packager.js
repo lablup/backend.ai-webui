a17951530034e94aaa714c4f8ce3f8e8a538675e
Jeongkyu Shin <inureyes@gmail.com>
2019-05-24 10:56:50 +0900

    refs #4 : modify - delete unnecessary lines

--- main.electron-packager.js
+++ main.electron-packager.js
@@ -299,8 +299,6 @@ function createWindow () {
       devTools: true
     }
   });
-  // and load the index.html of the app.
-  //mainWindow.loadFile('build/electron-app/app/index.html')
 
   mainWindow.loadURL(url.format({ // Load HTML into new Window
     pathname: path.join(__dirname, mainIndex),
@@ -311,9 +309,6 @@ function createWindow () {
   //mainWindow.webContents.openDevTools();
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   });
 
@@ -335,9 +330,6 @@ function createWindow () {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
@@ -377,5 +369,3 @@ app.on('web-contents-created', (event, contents) => {
     //}
   })
 });
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
