ef4797ef530c5f35997b4b590a6d441a7091f810
Jeongkyu Shin <inureyes@gmail.com>
2019-06-03 11:04:25 +0900

    refs #4 : modify - disable icon hidden feature for macOS

--- src/components/backend-ai-console.js
+++ src/components/backend-ai-console.js
@@ -124,8 +124,8 @@ class BackendAiConsole extends connect(store)(LitElement) {
 
   firstUpdated() {
     console.log("Electron shell mode: ", window.isElectron);
-    if (window.isElectron) {
-      this.shadowRoot.querySelector('.portrait-canvas').style.visibility = 'hidden';
+    if (window.isElectron) { // For macOS (TODO)
+      //this.shadowRoot.querySelector('.portrait-canvas').style.visibility = 'hidden';
     }
     installRouter((location) => store.dispatch(navigate(decodeURIComponent(location.pathname))));
     installOfflineWatcher((offline) => store.dispatch(updateOffline(offline)));
