a893226a48de01cef6acc096ce4d437c8a4ee4d6
Jeongkyu Shin <inureyes@gmail.com>
2019-06-13 10:34:10 +0900

    refs #4 : update - version tag for 19.06

--- src/components/backend-ai-console.js
+++ src/components/backend-ai-console.js
@@ -505,7 +505,7 @@ class BackendAiConsole extends connect(store)(LitElement) {
             <div id="sidebar-navbar-footer" class="vertical center center-justified layout">
               <address>
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">19.03.5.190611</small>
+                <small class="sidebar-footer" style="font-size:9px;">19.06.0.190613</small>
               </address>
             </div>
           </app-header-layout>
