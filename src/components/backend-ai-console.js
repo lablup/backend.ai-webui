fcec815a0ec111be86aa786a0a39ad4396284de2
Jeongkyu Shin <inureyes@gmail.com>
2019-06-22 21:07:55 +0900

    refs #45 : modify - move offline indicator to lit components

--- src/components/backend-ai-console.js
+++ src/components/backend-ai-console.js
@@ -35,7 +35,7 @@ import 'weightless/select';
 import '../backend.ai-client-es6.js';
 import {BackendAiStyles} from './backend-ai-console-styles';
 import {IronFlex, IronFlexAlignment, IronFlexFactors, IronPositioning} from '../layout/iron-flex-layout-classes';
-import '../backend-ai-offline-indicator.js';
+import './backend-ai-offline-indicator.js';
 import './backend-ai-login.js';
 
 
