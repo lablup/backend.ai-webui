be4903ddf3ffe4820732167cfe5fd3cee3a47fe9
Jeongkyu Shin <inureyes@gmail.com>
2019-07-08 11:32:36 +0900

    refs #82 : update - remove clutter packages

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,13 @@ import resolve from 'rollup-plugin-node-resolve';
 export default {
   // If using any exports from a symlinked project, uncomment the following:
   // preserveSymlinks: true,
-	input: ['src/components/backend-ai-console.js'],
-	output: {
+  input: ['src/components/backend-ai-console.js'],
+  output: {
     dir: 'build/rollup/src/components',
     format: 'es',
-		sourcemap: true
-	},
-	plugins: [
+    sourcemap: false
+  },
+  plugins: [
     resolve()
   ]
 };
