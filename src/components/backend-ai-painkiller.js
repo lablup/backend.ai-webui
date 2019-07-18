84f4370aa85ff1ed7135cf4485cb3234d72df875
pacokwon <haechank@gmail.com>
2019-07-18 11:20:09 +0900

    refs #100: add - painkiller message for resource policy error

--- src/components/backend-ai-painkiller.js
+++ src/components/backend-ai-painkiller.js
@@ -2,7 +2,8 @@ export class BackendAIPainKiller {
     static errorMessageTable = {
         "Cannot read property 'map' of null": "User has no group. Please contact administrator to fix it.",
         "Cannot read property 'split' of undefined": 'Wrong API server address.',
-        "server responded failure: 400 Bad Request - The virtual folder already exists with the same name.": "A virtual folder with the same name already exists. Delete your own folder or decline the invitation."
+        "server responded failure: 400 Bad Request - The virtual folder already exists with the same name.": "A virtual folder with the same name already exists. Delete your own folder or decline the invitation.",
+        "server responded failure: 400 Bad Request - Missing or invalid API parameters. (You cannot create more vfolders.)": "You cannot create more vfolders due to resource policy"
     }
 
     static relieve(msg) {
