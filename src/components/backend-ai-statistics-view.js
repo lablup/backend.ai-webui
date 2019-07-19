33f21c822866b83b60d1e0cbe19a99240fb1f033
pacokwon <haechank@gmail.com>
2019-07-19 15:47:49 +0900

    refs #80: add - statistics view component skeleton

---
+++ src/components/backend-ai-statistics-view.js
@@ -0,0 +1,14 @@
+/**
+ @license
+ Copyright (c) 2015-2019 Lablup Inc. All rights reserved.
+ */
+
+import { css, html, LitElement } from "lit-element";
+
+class BackendAIStatisticsView extends LitElement {
+    static get is() {
+        return 'backend-ai-statistics-view';
+    }
+}
+
+customElements.define(BackendAIStatisticsView.is, BackendAIStatisticsView);
