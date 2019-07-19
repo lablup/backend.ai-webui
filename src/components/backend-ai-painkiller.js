5c95bcf29b8ee41cc306a24fb68df431f57d03aa
pacokwon <haechank@gmail.com>
2019-07-19 15:18:37 +0900

    refs #90: update - regex table for painkiller and applied it to map preset creation error message

--- src/components/backend-ai-painkiller.js
+++ src/components/backend-ai-painkiller.js
@@ -7,6 +7,9 @@ var BackendAIPainKiller = (()=>{
       if (this.errorMessageTable.hasOwnProperty(msg)) {
         return this.errorMessageTable[msg];
       } else {
+        for (const regex of Object.keys(this.regexTable)) {
+          if (RegExp(regex).test(msg)) return this.regexTable[regex];
+        }
         return msg;
       }
     }
@@ -18,7 +21,10 @@ BackendAIPainKiller.errorMessageTable = {
       "Cannot read property 'split' of undefined": 'Wrong API server address.',
       "server responded failure: 400 Bad Request - The virtual folder already exists with the same name.": "A virtual folder with the same name already exists. Delete your own folder or decline the invitation.",
       "server responded failure: 400 Bad Request - Missing or invalid API parameters. (You cannot create more vfolders.)": "You cannot create more vfolders due to resource policy",
-      'integrity error: duplicate key value violates unique constraint "pk_resource_presets"\nDETAIL:  Key (name)=(ss) already exists.\n': 'A resource policy with the same name already exists.'
     };
+
+BackendAIPainKiller.regexTable = {
+  'integrity error: duplicate key value violates unique constraint "pk_resource_presets"[\\n]DETAIL:  Key \\(name\\)=\\([\\w]+\\) already exists.[\\n]': 'A resource policy with the same name already exists.'
+};
 
 export default BackendAIPainKiller;
