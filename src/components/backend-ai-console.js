456120a1fb3a2053b4ed091e0fa3b4ee35f05e4f
Jeongkyu Shin <inureyes@gmail.com>
2019-08-06 00:10:46 +0900

    refs #89 : bugfix - tag for unregistered kernel

--- src/components/backend-ai-console.js
+++ src/components/backend-ai-console.js
@@ -497,7 +497,7 @@ class BackendAiConsole extends connect(store)(LitElement) {
             <div id="sidebar-navbar-footer" class="vertical center center-justified layout">
               <address>
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">19.07.2.190805</small>
+                <small class="sidebar-footer" style="font-size:9px;">19.08.1.190805</small>
               </address>
             </div>
           </app-header-layout>
