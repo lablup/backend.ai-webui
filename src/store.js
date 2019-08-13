edd89e651d29b8d49cfa3a91a0e603f4871b66c4
Jeongkyu Shin <inureyes@gmail.com>
2019-08-13 10:25:01 +0900

    refs #135 : update - summer clean app part (WIP)

--- src/store.js
+++ src/store.js
@@ -1,21 +1,11 @@
 /**
-@license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
+ @license
+ Copyright (c) 2015-2019 Lablup Inc. All rights reserved.
+ */
 
-import {
-  createStore,
-  compose,
-  applyMiddleware,
-  combineReducers
-} from 'redux';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
-import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
+import {lazyReducerEnhancer} from 'pwa-helpers/lazy-reducer-enhancer.js';
 
 import app from './reducers/app.js';
 
