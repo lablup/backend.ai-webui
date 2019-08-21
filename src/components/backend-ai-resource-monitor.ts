f7079dae010bc4ee790fbfe9f44820628ca20132
Jeongkyu Shin <inureyes@gmail.com>
2019-08-22 00:41:27 +0900

    refs #155 : add - scaling group call to resource template refresh

--- src/components/backend-ai-resource-monitor.ts
+++ src/components/backend-ai-resource-monitor.ts
@@ -977,7 +977,20 @@ class BackendAiResourceMonitor extends BackendAIPage {
   }
 
   _refreshResourceTemplate() {
-    window.backendaiclient.resourcePreset.check().then((response) => {
+    let param = null;
+    if (this.enable_scaling_group == true && this.scaling_groups.length > 0) {
+      let scaling_group = 'default';
+      if (this.scaling_group !== '') {
+        scaling_group = this.scaling_group;
+      } else {
+        scaling_group = this.scaling_groups[0]['name'];
+      }
+      param = {
+        'group': window.backendaiclient.current_group,
+        'scaling_group': scaling_group
+      };
+    }
+    window.backendaiclient.resourcePreset.check(param).then((response) => {
       if (response.presets) {
         let presets = response.presets;
         let available_presets = [];
