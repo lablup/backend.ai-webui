38f1e79d14c998cdce45dfb8b7356ffcca25f18f
pacokwon <haechank@gmail.com>
2019-08-23 16:25:10 +0900

    refs #166: add - basic structure with mock data

--- src/components/backend-ai-registry-list.ts
+++ src/components/backend-ai-registry-list.ts
@@ -26,13 +26,19 @@ import './lablup-notification';
 
 class BackendAIRegistryList extends BackendAIPage {
   public notification: any;
+  public active: any;
 
   constructor() {
     super();
+
+    this.active = false;
   }
 
   static get properties() {
     return {
+      active: {
+        type: Boolean
+      },
       notification: {
         type: Object
       }
@@ -43,15 +49,67 @@ class BackendAIRegistryList extends BackendAIPage {
     return 'backend-ai-registry-list'
   }
 
+  static get styles() {
+    return [
+      BackendAiStyles,
+      IronFlex,
+      IronFlexAlignment,
+      css`
+        h4 {
+          font-weight: 200;
+          font-size: 14px;
+          margin: 0px;
+          padding: 5px 15px 5px 20px;
+        }
+
+        wl-button {
+          --button-bg: var(--paper-yellow-50);
+          --button-bg-hover: var(--paper-yellow-100);
+          --button-bg-active: var(--paper-yellow-600);
+        }
+      `
+    ];
+  }
+
   firstUpdated() {
     this.notification = this.shadowRoot.querySelector('#notification');
   }
 
+  async _viewStateChanged(active) {
+    await this.updateComplete;
+    if (active === false) {
+      return;
+    }
+    // If disconnected
+  }
+
   render() {
+    const list = ['a', 'b', 'c'];
     // language=HTML
     return html`
       <lablup-notification id="notification"></lablup-notification>
+      <h4 class="horizontal flex center center-justified layout">
+        <span>Registries</span>
+        <span class="flex"></span>
+        <wl-button
+          class="fg orange"
+          id="add-registry"
+          outlined
+        >
+          <wl-icon>add</wl-icon>
+          Create
+        </wl-button>
+      </h4>
 
+      <vaadin-grid theme="row-stripes column-borders compact" aria-label="Job list" .items="${list}">
+        <vaadin-grid-column flex-grow="1" header="#">
+          <template>
+            <div>
+              [[item]]
+            </div>
+          </template>
+        </vaadin-grid-column>
+      </vaadin-grid>
     `
   }
 }
