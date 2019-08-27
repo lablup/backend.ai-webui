c41f9d7f5a3ac687068a5caaf83d25a1411e1cd0
Jaepil Koh <gofeel@gmail.com>
2019-08-27 23:22:29 +0900

    refs #122 - change event to fix timing issues with handling over the wsproxy manager URL

--- preload.js
+++ preload.js
@@ -1,10 +1,9 @@
 const { ipcRenderer } = require('electron')
 
 process.once('loaded', () => {
-  ipcRenderer.once('proxy-ready', (event, proxy_url) => {
+  ipcRenderer.on('proxy-ready', (event, proxy_url) => {
     window.__local_proxy = proxy_url;
   });
-  ipcRenderer.send('ready');
 
   ipcRenderer.on('app-close-window', _ => {
     let event = new CustomEvent("backend-ai-logout", {"detail": ""});
