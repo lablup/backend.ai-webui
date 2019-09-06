5a9df1511b52de4e7e83ddae3017431fb9579c70
Jeongkyu Shin <inureyes@gmail.com>
2019-09-06 15:02:01 +0900

    refs #188 : update - TypeScript for pwa app setup

--- src/store.ts
+++ src/store.ts
@@ -5,9 +5,16 @@
 
 import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
-import {lazyReducerEnhancer} from 'pwa-helpers/lazy-reducer-enhancer.js';
+import {lazyReducerEnhancer} from 'pwa-helpers/lazy-reducer-enhancer';
 
-import app from './reducers/app.js';
+import app from './reducers/app';
+
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+  }
+}
 
 // Sets up a Chrome extension for time travel debugging.
 // See https://github.com/zalmoxisus/redux-devtools-extension for more information.
@@ -18,7 +25,7 @@ const devCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // that you can dispatch async actions). See the "Redux and state management"
 // section of the wiki for more details:
 // https://github.com/Polymer/pwa-starter-kit/wiki/4.-Redux-and-state-management
-export const store = createStore(
+export const store : any = createStore(
   state => state,
   devCompose(
     lazyReducerEnhancer(combineReducers),
