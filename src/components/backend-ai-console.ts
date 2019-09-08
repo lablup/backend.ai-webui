1188141b8e68018b17e6b523497895be637e5d70
Jeongkyu Shin <inureyes@gmail.com>
2019-09-08 09:51:03 +0900

    refs #4 : update - version autotagging test

--- src/components/backend-ai-console.ts
+++ src/components/backend-ai-console.ts
@@ -538,7 +538,7 @@ export default class BackendAIConsole extends connect(store)(LitElement) {
             <div id="sidebar-navbar-footer" class="vertical center center-justified layout">
               <address>
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">19.09.2.190906</small>
+                <small class="sidebar-footer" style="font-size:9px;">19.09.2.190907</small>
               </address>
             </div>
           </app-header-layout>
