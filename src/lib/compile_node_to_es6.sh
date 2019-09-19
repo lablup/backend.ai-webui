3f994ab507196d6412069b46ae73e759ca0c070a
Jeongkyu Shin <inureyes@gmail.com>
2019-09-19 11:30:10 +0900

    refs #161 : modify - update error parser

--- src/lib/compile_node_to_es6.sh
+++ src/lib/compile_node_to_es6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tsc --target ES2017 backend.ai-client-node.ts
+../../node_modules/typescript/bin/tsc --target ES2017 backend.ai-client-node.ts
 grep -v "Exclude for ES6" ./backend.ai-client-node.js > ./backend.ai-client-node-to-es6.js
 ../../node_modules/browserify/bin/cmd.js -p tinyify -s ai -o backend.ai-client-es6.js ./backend.ai-client-node-to-es6.js
 ../../node_modules/browserify/bin/cmd.js -s ai -o backend.ai-client-ingen-es6.js ./backend.ai-client-ingen.js
