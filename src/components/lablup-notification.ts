90818b92f02c88d98404b51c9ec64e989fc27123
Jeongkyu Shin <inureyes@gmail.com>
2019-10-02 19:58:25 +0900

    refs #213 : add - more information query feature to snackbar

--- src/components/lablup-notification.ts
+++ src/components/lablup-notification.ts
@@ -14,6 +14,7 @@ export default class LablupNotification extends LitElement {
   public updateComplete: any;
 
   @property({type: String}) text = '';
+  @property({type: String}) detail = '';
   @property({type: String}) message = '';
   @property({type: Object}) indicator;
   @property({type: Array}) notifications = Array();
@@ -44,7 +45,12 @@ export default class LablupNotification extends LitElement {
         }
 
         wl-button {
-            --button-font-size: 12px;
+          --button-font-size: 12px;
+          --button-fab-size: 12px;
+        }
+
+        wl-icon {
+          --icon-size: 10px;
         }
       `];
   }
@@ -97,15 +103,38 @@ export default class LablupNotification extends LitElement {
     dialog.hide();
   }
 
+  _moreNotification(e) {
+    const noti = e.target.closest('wl-snackbar');
+    const button = e.target.closest('wl-button');
+    if (noti.querySelector('div') !== null) {
+      noti.querySelector('div').style.display = 'block';
+    }
+    button.style.display = 'none';
+  }
+
   async show(persistent: boolean = false, message: string = '') {
     this.gc();
     let notification = document.createElement('wl-snackbar');
     if (message === '') {
-      notification.innerHTML = this.text;
+      notification.innerHTML = '<span style="overflow-x:hidden">' + this.text + '</span>';
+      if (this.detail != '') {
+        notification.innerHTML = notification.innerHTML + '<div style="display:none;"> : ' + this.detail + '</div>';
+        this.detail = '';
+      }
     } else {
-      notification.innerHTML = message;
+      notification.innerHTML = '<span style="overflow-x:hidden">' + message + '</span>';
       this.text = message;
     }
+    if (this.detail != '' && persistent) {
+      let more_button = document.createElement('wl-button');
+      more_button.setAttribute('slot', "action");
+      more_button.setAttribute('flat', "");
+      more_button.setAttribute('fab', "");
+      more_button.addEventListener('click', this._moreNotification.bind(this));
+      more_button.innerHTML = "<wl-icon>expand_more</wl-icon>";
+      notification.appendChild(more_button);
+    }
+    this.detail = ''; // Reset the temporary detail scripts
     if (persistent === false) {
       notification.setAttribute('hideDelay', '3000');
     } else {
@@ -113,13 +142,15 @@ export default class LablupNotification extends LitElement {
       let button = document.createElement('wl-button');
       button.setAttribute('slot', "action");
       button.setAttribute('flat', "");
+      button.setAttribute('fab', "");
       button.addEventListener('click', this._hideNotification.bind(this));
       button.innerHTML = "<wl-icon>close</wl-icon>";
       notification.appendChild(button);
     }
     notification.setAttribute('backdrop', '');
-    notification.style.bottom = (20 + 45 * this.step) + 'px';
+    notification.style.bottom = (20 + 55 * this.step) + 'px';
     notification.style.position = 'fixed';
+    (notification.querySelector('span') as any).style.overflowX = 'hidden';
     notification.style.right = '20px';
     notification.style.fontSize = '16px';
     notification.style.fontWeight = '400';
@@ -129,7 +160,7 @@ export default class LablupNotification extends LitElement {
     this.notifications.push(notification);
     await this.updateComplete;
     notification.show();
-    this._spawnDesktopNotification(notification.innerHTML, '', '');
+    this._spawnDesktopNotification(this.text, '', '');
   }
 
   _spawnDesktopNotification(title, body, icon) {
