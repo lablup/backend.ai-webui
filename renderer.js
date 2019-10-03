ae4e13dd799f87b4c6ea5b335015639e54c985c9
Jeongkyu Shin <inureyes@gmail.com>
2019-10-03 16:55:20 +0900

    refs #185 : modify - dig into bugs on tab impl. on electron-tabs

--- renderer.js
+++ renderer.js
@@ -25,18 +25,23 @@ let tab1 = tabGroup.addTab({
     active: true,
     webviewAttributes: {
       nodeintegration: false,
-      allowpopups: true,
+      allowpopups: 'on',
+      autosize: true,
+      blinkfeatures: '',
+      webpreferences:"nativeWindowOpen=yes"
       //preload: path.join(electronPath, 'preload.js'),
     }
 });
 let mainView = tab1.webview;
 mainView.addEventListener('dom-ready', () =>{
   mainView.executeJavaScript('window.__local_proxy="'+window.__local_proxy+'"');
-  mainView.openDevTools();
+  //mainView.openDevTools();
   let mainViewEvent = mainView.getWebContents();
   console.log(mainViewEvent);
   mainViewEvent.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
     event.preventDefault();
+    //console.log(disposition);
+    //event.preventDefault();
     console.log('new window requested');
     newPopupWindow(event, url, frameName, disposition, options, additionalFeatures);
   });
@@ -46,33 +51,51 @@ console.log(mainView);
 //console.log(tab1.webContents);
 
 function newPopupWindow(event, url, frameName, disposition, options, additionalFeatures) {
-    event.preventDefault();
   Object.assign(options, {
     visible: true,
     backgroundColor: '#EFEFEF',
     //parent: win,
     closable: true,
+    //url: url,
     width: windowWidth,
     height: windowHeight,
     webviewAttributes: {
-      allowpopups: true,
+      nodeintegration: false,
+      allowpopups: 'on',
       autosize: true,
+      blinkfeatures: '',
+      webpreferences:"nativeWindowOpen=yes"
     }
   });
   if (frameName === 'modal') {
     options.modal = true;
   }
   console.log(url);
+  let option = {    visible: true,
+    backgroundColor: '#EFEFEF',
+    //parent: win,
+    closable: true,
+    //url: url,
+    width: windowWidth,
+    height: windowHeight,
+    webviewAttributes: {
+      nodeintegration: false,
 
-  let newTab = tabGroup.addTab(options);
+      allowpopups: 'on',
+      autosize: true,
+      blinkfeatures: '',
+      webpreferences:"nativeWindowOpen=yes"
+    }
+  };
+  let newTab = tabGroup.addTab(option);
   newTab.webview.addEventListener('dom-ready', () => {
+    let newTabContents = newTab.webview.getWebContents();
+    newTabContents.on('new-window',(event, url, frameName, disposition, options, additionalFeatures) => {
+      newPopupWindow(event, url, frameName, disposition, options, additionalFeatures);
+    });
     newTab.webview.loadURL(url);
   });
-  newTab.once('ready-to-show', () => {
-    //newTab.webview.loadURL(url);
-    console.log(newTab);
-    //newTab.show()
-  });
+
   //event.newGuest.webContents.on('new-window',(event, url, frameName, disposition, options, additionalFeatures) => {
   //  newPopupWindow(event, url, frameName, disposition, options, additionalFeatures, event.newGuest);
   //});
