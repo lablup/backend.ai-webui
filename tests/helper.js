f9fcf070cc8d28257b371ee3b0c99086112da2ef
Jonghyun Park <adrysn@gmail.com>
2019-11-11 22:26:57 +0900

    Add vfolder creation/deletion test cases

--- tests/helper.js
+++ tests/helper.js
@@ -1,4 +1,4 @@
-import {Selector} from 'testcafe';
+import {ClientFunction, Selector} from 'testcafe';
 
 
 export const ShadowSelector = Selector(
@@ -12,3 +12,20 @@ export const ShadowSelector = Selector(
       }
       return element;
     });
+
+export const generateRandomString = (length=6, chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') => {
+  let result = '';
+  for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+};
+
+class _JSActions {
+  constructor() {
+    // Some buttons are not clickable by TestCafe's click.
+    this.click = ClientFunction((elm) => {
+      elm().click();
+      return true;
+    });
+  }
+}
+export const JSAction = new _JSActions();
