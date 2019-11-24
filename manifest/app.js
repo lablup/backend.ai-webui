c7f4c2fc40a2a77217fdbaec5fa0acce886f330d
Jeongkyu Shin <inureyes@gmail.com>
2019-11-24 22:14:35 +0800

    refs #296 : remove - notificationDialog features

--- manifest/app.js
+++ manifest/app.js
@@ -1,31 +1,5 @@
-function notificationDialog(message) {
-  var dialog = document.querySelector('#notificationDialog');
-  dialog.querySelector('#notificationDialog-message').innerHTML = message;
-  dialog.open();
-}
-
 function sendData(id) {
   document.getElementById(id).submit();
-}
-
-function toggleDisplayElement(e) {
-  var o = document.getElementById(e);
-  if (o.style.display == '' || o.style.display == undefined || o.style.display != 'block') {
-    o.style.display = 'block';
-  } else {
-    o.style.display = 'none';
-  }
-}
-
-function toggleVisibilityElement(e) {
-  var o = document.getElementById(e);
-  if (o.classList.contains('visible')) {
-    o.classList.remove('visible');
-    o.classList.add("invisible");
-  } else {
-    o.classList.remove("invisible");
-    o.classList.add('visible');
-  }
 }
 
 var forEach = function (array, callback, scope) {
