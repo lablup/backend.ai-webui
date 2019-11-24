2570e70e9b2a13571bca9d777fcd09e2e70f2b99
Jeongkyu Shin <inureyes@gmail.com>
2019-11-24 22:10:19 +0800

    refs #296 : update - reduce app.js

--- manifest/app.js
+++ manifest/app.js
@@ -1,71 +1,6 @@
-function isAuthenticated() {
-  const api_key = localStorage.getItem('backendaiconsole.api_key');
-  const secret_key = localStorage.getItem('backendaiconsole.secret_key');
-  const api_endpoint = localStorage.getItem('backendaiconsole.api_endpoint');
-  let authenticated = false;
-  if (api_key && secret_key && api_endpoint &&
-    api_key !== 'null' && secret_key !== 'null' && api_endpoint !== 'null') {
-    authenticated = true;
-  }
-  return authenticated;
-}
-
-function backToPreviousPage(e) {
-  window.history.back();
-}
-
-function setNotification(text) {
-  document.querySelector('#lablup-notification').text = text;
-  document.querySelector('#lablup-notification').show();
-}
-
-function setNotificationNavbar(text, timeout) {
-  document.querySelector('#lablup-notification-navbar').innerText = text;
-  if (timeout != 0) {
-    setTimeout(function () {
-      document.querySelector('#lablup-notification-navbar').innerText = '';
-    }, timeout);
-  }
-}
-
-function closeNotification() {
-  document.querySelector('#lablup-notification').visible = false;
-}
-
-function popAlert(html) {
-  var alert = document.querySelector('lablup-alert');
-  alert.text = html;
-  alert.show();
-}
-
-function closeAlert() {
-  document.getElementById('lablup-alert').close();
-}
-
-function activateSpinner() {
-  var spinner = document.querySelector('#lablup-loading-spinner');
-  spinner.style.display = 'block';
-  spinner.active = true;
-}
-
-function deactivateSpinner() {
-  var spinner = document.querySelector('#lablup-loading-spinner');
-  setTimeout(function () {
-    spinner.style.display = 'none';
-  }, 500);
-  spinner.active = false;
-}
-
 function toggleSpinner() {
   var spinner = document.querySelector('#lablup-loading-spinner');
   spinner.active = !spinner.active;
-}
-
-function confirmHrefAction(link, message) {
-  var dialog = document.querySelector('#confirmDialog');
-  dialog.querySelector('#confirmDialog-message').innerHTML = message;
-  dialog.querySelector('#confirmDialog-confirm').href = link;
-  dialog.open();
 }
 
 function confirmJSAction(js, message) {
@@ -79,11 +14,6 @@ function notificationDialog(message) {
   var dialog = document.querySelector('#notificationDialog');
   dialog.querySelector('#notificationDialog-message').innerHTML = message;
   dialog.open();
-}
-
-function resetInputForm(event) {
-  Polymer.dom(event).localTarget.errorMessage = '';
-  Polymer.dom(event).localTarget.invalid = false;
 }
 
 function warnInputForm(response) {
@@ -105,19 +35,6 @@ function warnInputForm(response) {
 
 function sendData(id) {
   document.getElementById(id).submit();
-}
-
-function mapToggleButtonValue(e) {
-  if (Polymer.dom(e).localTarget.checked) {
-    Polymer.dom(e).localTarget.value = "checked";
-  } else {
-    Polymer.dom(e).localTarget.value = undefined;
-  }
-}
-
-function moveTo(url) {
-  activateSpinner();
-  window.location.href = url;
 }
 
 function toggleDisplayElement(e) {
