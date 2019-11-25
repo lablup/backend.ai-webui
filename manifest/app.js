7f3db302a71eba44509809f80856cb94ac47454e
Jeongkyu Shin <inureyes@gmail.com>
2019-11-25 13:00:57 +0800

    refs #296 : remove - sendData function

--- manifest/app.js
+++ manifest/app.js
@@ -1,7 +1,3 @@
-function sendData(id) {
-  document.getElementById(id).submit();
-}
-
 var forEach = function (array, callback, scope) {
   for (var i = 0; i < array.length; i++) {
     callback.call(scope, i, array[i]);
