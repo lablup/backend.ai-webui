a370d713b903896f9908c63564b01931d98a9c6e
Jeongkyu Shin <inureyes@gmail.com>
2020-01-06 22:38:56 +0900

    refs #208 : update - year mark on copyright lines

--- src/plastics/lablup-piechart/lablup-piechart.js
+++ src/plastics/lablup-piechart/lablup-piechart.js
@@ -1,6 +1,6 @@
 /**
  @license
- Copyright (c) 2015-2019 Lablup Inc. All rights reserved.
+ Copyright (c) 2015-2020 Lablup Inc. All rights reserved.
  */
 import {css, html, LitElement} from "lit-element";
 import '@polymer/iron-icon/iron-icon';
