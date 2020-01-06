2ce90b37987711fa29b29db3739431099460c90c
Jeongkyu Shin <inureyes@gmail.com>
2020-01-06 15:49:09 +0900

    refs #311 : add - layout component  * bugfix - attribute / property of lit-element

--- src/components/lablup-slider.ts
+++ src/components/lablup-slider.ts
@@ -3,26 +3,37 @@
  Copyright (c) 2015-2020 Lablup Inc. All rights reserved.
  */
 
-
 import {css, customElement, html, LitElement, property} from "lit-element";
 import '@material/mwc-slider';
 import '@material/mwc-textfield';
+import {
+  IronFlex,
+  IronFlexAlignment,
+  IronFlexFactors,
+  IronPositioning
+} from "../plastics/layout/iron-flex-layout-classes";
+import {BackendAiStyles} from "./backend-ai-console-styles";
 
 @customElement("lablup-slider")
 export default class LablupSlider extends LitElement {
   public shadowRoot: any; // ShadowRoot
 
-  @property({type: Number}) step = 0;
+  @property({type: Number}) step = 1;
   @property({type: Number}) value = 0;
   @property({type: Number}) max = 0;
   @property({type: Number}) min = 0;
-  @property({type: Boolean}) editable = false;
-  @property({type: Boolean}) pin = false;
-  @property({type: Boolean}) markers = false;
+  @property({type: Boolean}) editable = null;
+  @property({type: Boolean}) pin = null;
+  @property({type: Boolean}) markers = null;
   @property({type: Object}) slider;
 
   static get styles() {
     return [
+      BackendAiStyles,
+      IronFlex,
+      IronFlexAlignment,
+      IronFlexFactors,
+      IronPositioning,
       // language=CSS
       css`
       `];
@@ -31,11 +42,12 @@ export default class LablupSlider extends LitElement {
   render() {
     // language=HTML
     return html`
-      <mwc-slider id="slider" value="${this.value}" min="${this.min}" max="${this.max}" ${this.step != 0 ? html`step="${this.step}"` : html``}
-        ${this.pin ? `pin` : ``}
-        ${this.markers ? `markers` : ``}
+      <mwc-slider id="slider" value="${this.value}" min="${this.min}" max="${this.max}"
+        step="${this.step}"
+        ?pin="${this.pin}"
+        ?markers="${this.markers}"
       ></mwc-slider>
-      ${this.editable ? html`<mwc-textfield></mwc-textfield>` : html``}
+      ${this.editable ? html`<mwc-textfield>${this.value}</mwc-textfield>` : html``}
     `;
   }
 
@@ -51,4 +63,10 @@ export default class LablupSlider extends LitElement {
     super.disconnectedCallback();
   }
 
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "lablup-slider": LablupSlider;
+  }
 }
