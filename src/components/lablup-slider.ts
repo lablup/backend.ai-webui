3533e2868628279b7200f88dcfe21d0c2e6c716f
Jeongkyu Shin <inureyes@gmail.com>
2020-01-06 13:50:20 +0900

    refs #311 : add - basic methods / properties

--- src/components/lablup-slider.ts
+++ src/components/lablup-slider.ts
@@ -9,4 +9,30 @@ import '@material/mwc-slider';
 
 @customElement("lablup-slider")
 export default class LablupSlider extends LitElement {
+  @property({type: Number}) step = 0;
+  @property({type: Number}) value = 0;
+
+  static get styles() {
+    return [
+      // language=CSS
+      css`
+      `];
+  }
+
+  render() {
+    // language=HTML
+    return html``;
+  }
+
+  firstUpdated() {
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+  }
+
 }
