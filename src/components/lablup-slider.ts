6a571927942c5027f0970f07062f463af5a90ed4
Jeongkyu Shin <inureyes@gmail.com>
2020-01-06 13:36:12 +0900

    refs #311 : add - lablup-slider skeleton

---
+++ src/components/lablup-slider.ts
@@ -0,0 +1,12 @@
+/**
+ @license
+ Copyright (c) 2015-2020 Lablup Inc. All rights reserved.
+ */
+
+
+import {css, customElement, html, LitElement, property} from "lit-element";
+import '@material/mwc-slider';
+
+@customElement("lablup-slider")
+export default class LablupSlider extends LitElement {
+}
