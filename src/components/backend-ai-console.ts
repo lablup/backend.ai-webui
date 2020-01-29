ee869f8e4c6dba3876b58c6d1062b74793bb08d1
Jeongkyu Shin <inureyes@gmail.com>
2020-01-29 16:10:33 +0900

    refs #208 : modify - solving merge problem

--- src/components/backend-ai-console.ts
+++ src/components/backend-ai-console.ts
@@ -709,7 +709,7 @@ export default class BackendAIConsole extends connect(store)(LitElement) {
             <div id="sidebar-navbar-footer" class="vertical center center-justified layout">
               <address>
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">20.01.5.200128</small>
+                <small class="sidebar-footer" style="font-size:9px;">20.01.5.200129</small>
               </address>
             </div>
         </div>
