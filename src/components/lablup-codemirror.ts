5b06f72568626e3cadee657fbad9697f5bea9336
Jeongkyu Shin <inureyes@gmail.com>
2020-02-17 10:04:15 +0900

    refs #70 : bugfix - resolve conflict during merge

--- src/components/lablup-codemirror.ts
+++ src/components/lablup-codemirror.ts
@@ -6,16 +6,10 @@ import {css, customElement, html, LitElement, property} from "lit-element";
 
 import {IronFlex, IronFlexAlignment} from '../plastics/layout/iron-flex-layout-classes';
 
-<<<<<<< HEAD
-import {CodemirrorStyle} from '../lib/codemirror/lib/codemirror.css.js';
-import {CodemirrorThemeMonokai} from '../lib/codemirror/theme/monokai.css.js';
-import '../lib/codemirror/mode/python/python.js';
-=======
 import '@vanillawc/wc-codemirror/index';
 import '@vanillawc/wc-codemirror/mode/python/python';
 import '@vanillawc/wc-codemirror/mode/shell/shell';
 import {CodemirrorThemeMonokai} from '../lib/codemirror/theme/monokai.css.js';
->>>>>>> master
 
 declare const window: any;
 
@@ -25,25 +19,16 @@ export default class LablupCodemirror extends LitElement {
   public shadowRoot: any; // ShadowRoot
   public editor: any;
 
-<<<<<<< HEAD
-  @property({type: Object}) config = {};
-=======
   @property({type: Object}) config = Object();
   @property({type: String}) mode = 'shell';
   @property({type: String}) theme = 'monokai';
   @property({type: String}) src = '';
->>>>>>> master
 
   constructor() {
     super();
     this.config = {
-<<<<<<< HEAD
-      mode: 'python',
-      theme: 'monokai',
-=======
       // mode: this.mode,
       // theme: 'monokai',
->>>>>>> master
       tabSize: 2,
       indentUnit: 2,
       cursorScrollMargin: 50,
@@ -62,10 +47,6 @@ export default class LablupCodemirror extends LitElement {
   }
 
   _initEditor() {
-<<<<<<< HEAD
-    const textarea: HTMLTextAreaElement = this.shadowRoot.querySelector('textarea') as HTMLTextAreaElement;
-    this.editor = window.CodeMirror.fromTextArea(textarea, this.config);
-=======
     const cm = this.shadowRoot.querySelector('#codemirror-editor');
     if (!cm.__initialized) {
       window.setTimeout(this._initEditor.bind(this), 100);
@@ -73,16 +54,11 @@ export default class LablupCodemirror extends LitElement {
     }
     this.editor = cm.__editor;
     Object.assign(this.editor.options, this.config);
->>>>>>> master
     this.refresh();
   }
 
   refresh() {
-<<<<<<< HEAD
-    window.setTimeout(() => this.editor.refresh(), 0);
-=======
     window.setTimeout(() => this.editor.refresh(), 100);
->>>>>>> master
   }
 
   getValue() {
@@ -98,32 +74,19 @@ export default class LablupCodemirror extends LitElement {
     return [
       IronFlex,
       IronFlexAlignment,
-<<<<<<< HEAD
-      CodemirrorStyle,
-      CodemirrorThemeMonokai,
-      css`
-      .CodeMirror {
-        /* font-size: 15px; */
-      }
-=======
       CodemirrorThemeMonokai,
       css`
         .CodeMirror {
           height: auto;
           font-size: 15px;
         }
->>>>>>> master
       `,
     ];
   }
 
   render() {
     return html`
-<<<<<<< HEAD
-      <textarea></textarea>
-=======
       <wc-codemirror id="codemirror-editor" mode="${this.mode}" theme="monokai" src="${this.src}"></wc-codemirror>
->>>>>>> master
     `;
   }
 }
