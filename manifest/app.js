b30ce52a27904a39aa92b8f022e32e800e175570
Jeongkyu Shin <inureyes@gmail.com>
2020-04-09 13:47:33 +0900

    fix: environment script location

--- manifest/app.js
+++ manifest/app.js
@@ -3,3 +3,26 @@ var forEach = function (array, callback, scope) {
     callback.call(scope, i, array[i]);
   }
 };
+
+function isElectron() {
+  // Renderer process
+  if (typeof window !== 'undefined' && typeof window.process === 'object' &&
+    window.process.type === 'renderer') {
+    return true;
+  }
+  // Main process
+  if (typeof process !== 'undefined' && typeof process.versions === 'object' &&
+    Boolean(process.versions.electron)) {
+    return true;
+  }
+  // Detect the user agent when the `nodeIntegration` option is set to true
+  if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' &&
+    navigator.userAgent.indexOf('Electron') >= 0) {
+    return true;
+  }
+  return false;
+}
+
+function isIE() {
+  return globalThis.navigator.userAgent.match(/(MSIE|Trident)/);
+}
