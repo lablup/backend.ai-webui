52472cced8bbb31308d76291ee4fd23db8564856
Jonghyun Park <adrysn@gmail.com>
2020-05-16 20:28:16 +0900

    feat: add token verification steps
    * Call verification API by parsing verification_code from URL query strig.
    * Add verification success / fail dialog, and show them depending on the
      result of verification.

--- src/components/backend-ai-email-verification-view.ts
+++ src/components/backend-ai-email-verification-view.ts
@@ -5,6 +5,7 @@
 import {get as _text, translate as _t} from "lit-translate";
 import {css, customElement, html, property} from "lit-element";
 
+import '@material/mwc-textfield/mwc-textfield';
 import 'weightless/card';
 import 'weightless/dialog';
 
@@ -25,7 +26,7 @@ import {
 
 @customElement("backend-ai-email-verification-view")
 export default class BackendAIEmailVerificationView extends BackendAIPage {
-  // @property({type: Object}) _lists = Object();
+  // @property({type: Object}) client = Object();
 
   static get styles() {
     return [
@@ -36,44 +37,98 @@ export default class BackendAIEmailVerificationView extends BackendAIPage {
       IronPositioning,
       // language=CSS
       css`
+        mwc-textfield {
+          width: 100%;
+        }
       `;
     ];
   }
 
-  firstUpdated() {
-    console.log('email-verification-view firstUpdated')
-  }
+  _initClient(apiEndpoint: string) {
+    const consoleShell = document.querySelector('#console-shell');
+    consoleShell.appBody.style.visibility = 'visible';
+    this.notification = consoleShell.notification;
+    this.successDialog = this.shadowRoot.querySelector('#verification-success-dialog');
+    this.failDialog = this.shadowRoot.querySelector('#verification-fail-dialog');
 
-  async verify(apiEndpoint) {
-    console.log('apiEndpoint:', apiEndpoint)
     this.clientConfig = new ai.backend.ClientConfig('', '', apiEndpoint, 'SESSION');
     this.client = new ai.backend.Client(
       this.clientConfig,
       'Backend.AI Console.',
     );
-    const resp = await this.client.cloud.ping()
-    console.log('ping resp:', resp)
+  }
 
-    // this.verificationSuccessDialog = this.shadowRoot.querySelector('#verification-success-dialog');
-    // console.log(this.verificationSuccessDialog)
-    // this.verificationSuccessDialog.show();
+  _redirectToLoginPage() {
+    window.location.href = '/';
+  }
+
+  async verify(apiEndpoint: string) {
+    const queryParams = new URLSearchParams(window.location.search);
+    const token = queryParams.get('verification_code');
+
+    this._initClient(apiEndpoint);
+
+    let dialog;
+    if (token) {
+      try {
+        const resp = await this.client.cloud.verify_email(token);
+        console.log('- ', resp)
+        resp.success ? this.successDialog.show() : this.failDialog.show();
+      } catch (e) {
+        console.error(e);
+        this.notification.text = e.message || 'Verification Error';
+        this.notification.show();
+        window.setTimeout(() => this.failDialog.show(), 1000);
+      }
+    } else {
+      this.failDialog.show();
+    }
+  }
+
+  async sendVerificationCode() {
+    const emailEl = this.shadowRoot.querySelector('#email');
+    console.log('email:', emailEl.value)
+    console.log('validity:', emailEl.validity)
+    if (!emailEl.value || !emailEl.validity.valid) return;
+
+    const resp = await this.client.cloud.send_verification_email(emailEl.value);
+    console.log('- resp:', resp);
   }
 
   render() {
     // language=HTML
     return html `
-      <div>This is email verification view!</div>
       <wl-dialog id="verification-success-dialog" fixed backdrop blockscrolling persistent style="padding:0;">
         <wl-card class="login-panel intro centered" style="margin: 0;">
           <h3 class="horizontal center layout">
-            <span style="font-size:16px;">Email verified!</span>
-            <div class="flex"></div>
-            <mwc-icon-button icon="close" class="blue close-button"
-              @click="${(e) => this._hideDialog(e)}">
-            </mwc-icon-button>
+            <span>Email verified!</span>
           </h3>
-          <div class="horizontal layout center" style="margin:5px;">
-            <p style="font-size:14px;width:256px;">Your email is verified! Now you can login with your account.</p>
+          <div class="horizontal layout center" style="margin:10px;">
+            <p style="width:256px;">Your email is verified! Now you can login with your account.</p>
+          </div>
+          <div class="horizontal layout center" style="margin:20px;">
+            <wl-button outlined flat class="fg green mini flex" type="button" @click="${(e) => this._redirectToLoginPage(e)}">${_t("login.Login")}</wl-button>
+          </div>
+        </wl-card>
+      </wl-dialog>
+
+      <wl-dialog id="verification-fail-dialog" fixed backdrop blockscrolling persistent style="padding:0;">
+        <wl-card class="login-panel intro centered" style="margin: 0;">
+          <h3 class="horizontal center layout">
+            <span>Email verification failed!</span>
+          </h3>
+          <div class="horizontal layout center" style="margin:10px;">
+            <p style="width:256px;">
+              We're unable to verify your email. Your token may be invalid or expired.<br /><br />
+              Write your email address and click send button to get new verification code.
+            </p>
+          </div>
+          <div style="margin:20px;">
+            <mwc-textfield id="email" label="${_t('data.explorer.EnterEmailAddress')}"
+                autofocus auto-validate validationMessage="Invalid email address"
+                pattern="^[A-Z0-9a-z#-_]+@.+\\..+$"></mwc-textfield>
+            <div style="height:1em"></div>
+            <wl-button outlined flat class="fg red mini flex" type="button" @click="${(e) => this.sendVerificationCode(e)}">Send verification email</wl-button>
           </div>
         </wl-card>
       </wl-dialog>
