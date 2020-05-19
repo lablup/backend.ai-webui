161d446b93697e206ca1735144611ff093b6290d
Jeongkyu Shin <inureyes@gmail.com>
2020-05-19 15:47:31 +0900

    feat: feature icons on mini sidebar mode (#518)

--- src/components/backend-ai-console.ts
+++ src/components/backend-ai-console.ts
@@ -791,14 +791,24 @@ export default class BackendAIConsole extends connect(store)(LitElement) {
           </wl-popover>
           <wl-popover anchor="#usersettings-menu-icon" .anchorOpenEvents="${["mouseover"]}" .anchorCloseEvents="${["mouseout"]}" fixed disablefocustrap
              anchororiginx="right" anchororiginy="center" transformoriginx="left" transformOriginY="center">
-             <wl-popover-card><div style="padding:5px">${_t("console.menu.Settings")}</div></wl-popover-card>
+            <wl-popover-card><div style="padding:5px">${_t("console.menu.Settings")}</div></wl-popover-card>
+          </wl-popover>
+          <wl-popover anchor="#mini-ui-toggle-button" .anchorOpenEvents="${["mouseover"]}" fixed disablefocustrap
+             anchororiginx="right" anchororiginy="center" transformoriginx="left" transformOriginY="center">
+            <wl-popover-card>
+              <div style="padding:5px">
+                <mwc-icon-button disabled class=" side-menu fg white" id="feedback-icon-popover" icon="question_answer" slot="graphic"></mwc-icon-button>
+                <mwc-icon-button disabled class=" side-menu fg white" id="notification-icon-popover" icon="notification_important" slot="graphic"></mwc-icon-button>
+                <mwc-icon-button disabled class=" side-menu fg white" id="task-icon-popover" icon="ballot" slot="graphic"></mwc-icon-button>
+              </div>
+            </wl-popover-card>
           </wl-popover>
           ${this.is_admin ? html`
             <wl-popover anchor="#user-menu-icon" .anchorOpenEvents="${["mouseover"]}" .anchorCloseEvents="${["mouseout"]}" fixed disablefocustrap
                anchororiginx="right" anchororiginy="center" transformoriginx="left" transformOriginY="center">
                <wl-popover-card><div style="padding:5px">${_t("console.menu.Users")}</div></wl-popover-card>
             </wl-popover>
-            `: html``}
+            ` : html``}
           ${this.is_superadmin ? html`
             <wl-popover anchor="#resources-menu-icon" .anchorOpenEvents="${["mouseover"]}" .anchorCloseEvents="${["mouseout"]}" fixed disablefocustrap
                anchororiginx="right" anchororiginy="center" transformoriginx="left" transformOriginY="center">
