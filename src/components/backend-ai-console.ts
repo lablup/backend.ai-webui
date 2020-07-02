e3357679d9d4a256394ac87e6b75b38acb612524
Jeongkyu Shin <inureyes@gmail.com>
2020-07-02 13:45:43 +0900

    up: version index (#208)

--- src/components/backend-ai-console.ts
+++ src/components/backend-ai-console.ts
@@ -883,7 +883,7 @@ export default class BackendAIConsole extends connect(store)(LitElement) {
             </div>
             <address>
               <small class="sidebar-footer">Lablup Inc.</small>
-              <small class="sidebar-footer" style="font-size:9px;">20.07.0.200701</small>
+              <small class="sidebar-footer" style="font-size:9px;">20.07.0.200702</small>
             </address>
           </footer>
           <div id="sidebar-navbar-footer" class="vertical start end-justified layout">
