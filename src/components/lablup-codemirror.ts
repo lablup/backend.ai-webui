3be229b167980241ebffea626efcedbf49484cb6
Jonghyun Park <adrysn@gmail.com>
2020-07-19 02:12:18 +0900

    fix: downgrade wc-codemirror version to 1.5.3
    * wc-codemirror has upstream issue from >=1.6.0, which breaks codemirror UI.
      - From 1.6.0, basic codemirror CSS styles are moved from
        wc-codemirror's own template to document.body. lablup-codemirror is
        a custom element, so it does not automatically inherit styles under
        document.body.
      - We can workaround this issue by manually attach the styles into
        lablup-codemirror, but let's see if any updates are coming from
        upstream.
      - https://github.com/vanillawc/wc-codemirror/compare/v1.5.3...v1.6.0#diff-9de5e984ff98ce83bed512dd2a5f2d21R140
    * lablup-codemirror now internally uses nosrc-wc-codemirror, which does
      not always fetch external sources specified by "src" attribute.

--- src/components/lablup-codemirror.ts
+++ src/components/lablup-codemirror.ts
@@ -6,12 +6,30 @@ import {css, customElement, html, LitElement, property} from "lit-element";
 
 import {IronFlex, IronFlexAlignment} from '../plastics/layout/iron-flex-layout-classes';
 
+import {WCCodeMirror} from '@vanillawc/wc-codemirror/index';
 import '@vanillawc/wc-codemirror/index';
 import '@vanillawc/wc-codemirror/mode/python/python';
 import '@vanillawc/wc-codemirror/mode/shell/shell';
 import {CodemirrorThemeMonokai} from '../lib/codemirror/theme/monokai.css.js';
 
 declare const window: any;
+
+
+/**
+ * Extension of WCCodeMirror that does not fetch external sources.
+ *
+ * WCCodeMirror always tries to fetch the external file specified in `src`
+ * attribute during initialization. To prevent this, we define a custom element
+ * with the `srcFetch` method patched.
+ */
+class nosrcWCCodeMirror extends WCCodeMirror {
+  async fetchSrc(src) {
+    // const response = await fetch(src);
+    // return response.text();
+    return '';
+  }
+}
+customElements.define('nosrc-wc-codemirror', <CustomElementConstructor><unknown>nosrcWCCodeMirror);
 
 
 @customElement('lablup-codemirror')
@@ -49,12 +67,10 @@ export default class LablupCodemirror extends LitElement {
   _initEditor() {
     const cm = this.shadowRoot.querySelector('#codemirror-editor');
     if (!cm.__initialized) {
-      setTimeout(() => {
-        this._initEditor()
-      }, 500);
+      setTimeout(this._initEditor.bind(this), 100);
       return;
     }
-    this.editor = cm.editor;
+    this.editor = cm.__editor;
     Object.assign(this.editor.options, this.config);
     this.refresh();
   }
@@ -88,7 +104,7 @@ export default class LablupCodemirror extends LitElement {
 
   render() {
     return html`
-      <wc-codemirror id="codemirror-editor" src="" mode="${this.mode}" theme="monokai"></wc-codemirror>
+      <nosrc-wc-codemirror id="codemirror-editor" mode="${this.mode}" theme="monokai"></nosrc-wc-codemirror>
     `;
   }
 }
