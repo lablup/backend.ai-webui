71997b6058a6f623a025b1e27a5d8cf1d471f5b5
jihyunkang <46954439+lizable@users.noreply.github.com>
2020-08-18 13:43:25 +0900

    feat: toggle password visibility (#666)
    * add: toggle password visibility with material icon (#665)
    
    * change: variable name to be more understandable (#665)
    
    * fix minor typo
    
    Co-authored-by: Jonghyun Park <adrysn@gmail.com>

--- src/components/backend-ai-signup.ts
+++ src/components/backend-ai-signup.ts
@@ -8,6 +8,7 @@ import 'weightless/button';
 import 'weightless/icon';
 import 'weightless/card';
 import '@material/mwc-textfield';
+import '@material/mwc-icon-button-toggle';
 import './lablup-terms-of-service';
 import './backend-ai-dialog';
 
@@ -106,6 +107,7 @@ export default class BackendAiSignup extends BackendAIPage {
               --button-bg-hover: var(--paper-green-300);
               --button-bg-active: var(--paper-green-300);
           }
+
           mwc-textfield {
             width: 100%;
             --mdc-text-field-fill-color: transparent;
@@ -304,6 +306,12 @@ export default class BackendAiSignup extends BackendAIPage {
     return err;
   }
 
+  _togglePasswordVisibility(element) {
+    const isVisible = element.__on;
+    const password = element.closest('div').querySelector('mwc-textfield');
+    isVisible ? password.setAttribute('type', 'text') : password.setAttribute('type', 'password');
+  }
+  
   _validateEmail() {
     const emailInput = this.shadowRoot.querySelector('#id_user_email');
     emailInput.validityTransform = (newValue, nativeValidity) => {
@@ -418,18 +426,28 @@ export default class BackendAiSignup extends BackendAIPage {
           <mwc-textfield type="text" name="token" id="id_token" maxlength="50"
                        label="${_t("signup.InvitationToken")}"
                        validationMessage="${_t("signup.TokenInputRequired")}" required></mwc-textfield>
-          <mwc-textfield type="password" name="password1" id="id_password1"
-                       label="${_t("signup.Password")}" minlength="8"
-                       pattern="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"
-                       validationMessage="${_t("signup.PasswordInputRequired")}"
-                       @change="${this._validatePassword}"
-                       value="" required></mwc-textfield>
-          <mwc-textfield type="password" name="password2" id="id_password2"
-                       label="${_t("signup.PasswordAgain")}" minlength="8"
-                       pattern="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"
-                       validationMessage="${_t("signup.PasswordInputRequired")}"
-                       @change="${this._validatePassword}"
-                       value="" required></mwc-textfield>
+          <div class="horizontal flex layout">
+            <mwc-textfield type="password" name="password1" id="id_password1"
+                        label="${_t("signup.Password")}" minlength="8"
+                        pattern="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"
+                        validationMessage="${_t("signup.PasswordInputRequired")}"
+                        @change="${this._validatePassword}"
+                        value="" required></mwc-textfield>
+            <mwc-icon-button-toggle off onIcon="visibility" offIcon="visibility_off"
+                                    @click="${(e) => this._togglePasswordVisibility(e.target)}">
+            </mwc-icon-button-toggle>             
+          </div>
+          <div class="horizontal flex layout">
+            <mwc-textfield type="password" name="password2" id="id_password2"
+                        label="${_t("signup.PasswordAgain")}" minlength="8"
+                        pattern="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"
+                        validationMessage="${_t("signup.PasswordInputRequired")}"
+                        @change="${this._validatePassword}"
+                        value="" required></mwc-textfield>
+            <mwc-icon-button-toggle off onIcon="visibility" offIcon="visibility_off"
+                                    @click="${(e) => this._togglePasswordVisibility(e.target)}">
+            </mwc-icon-button-toggle>              
+          </div>
           <div style="margin-top:10px;">
             <wl-checkbox id="approve-terms-of-service">
             </wl-checkbox>
