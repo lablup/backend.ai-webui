19578ccb4452bc8734d5aa7e9f2042dfc50e40e6
Jeongkyu Shin <inureyes@gmail.com>
2020-10-22 12:45:47 +0900

    fix: registry deletion confirmation button (#742)

--- src/components/backend-ai-registry-list.ts
+++ src/components/backend-ai-registry-list.ts
@@ -626,9 +626,9 @@ class BackendAIRegistryList extends BackendAIPage {
             label="${_t("registry.TypeRegistryNameToDelete")}"
           ></wl-textfield>
         </div>
-        <div slot="footer" class="horizontal center-justified flex layout">
-          <mwc-button raised icon="delete" label="${_t("button.Delete")}"
-              @click=${this._deleteRegistry} style="width:100%;"></mwc-button>
+        <div slot="footer" class="horizontal end-justified flex layout">
+          <mwc-button unelevated icon="delete" label="${_t("button.Delete")}"
+              @click=${this._deleteRegistry}></mwc-button>
         </div>
       </backend-ai-dialog>
     `
