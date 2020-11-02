0bef1893774c8caee76e3e166e670ccb2f2da000
Jeongkyu Shin <inureyes@gmail.com>
2020-11-02 20:43:52 +0900

    feat: height on log page (#766)

--- src/components/backend-ai-error-log-list.ts
+++ src/components/backend-ai-error-log-list.ts
@@ -71,7 +71,7 @@ export default class BackendAiErrorLogList extends BackendAIPage {
           width: 100%;
           border: 0;
           font-size: 12px;
-          height: calc(100vh - 275px);
+          height: calc(100vh - 305px);
         }
 
         vaadin-grid-cell {
