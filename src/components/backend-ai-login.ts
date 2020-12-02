29873aff9581bf26ffa3352c9e889119bd0918f4
jihyunkang <lizable.me@gmail.com>
2020-12-02 22:42:58 +0900

    feat: auto logout by no actions in 3 minutes as a default

--- src/components/backend-ai-login.ts
+++ src/components/backend-ai-login.ts
@@ -86,6 +86,8 @@ export default class BackendAILogin extends BackendAIPage {
   @property({type: Boolean}) maxCUDADevicesPerSession = 16;
   @property({type: Boolean}) maxShmPerSession = 2;
   @property({type: Array}) endpoints;
+  @property({type: Object}) logoutTimerBeforeOneMin;
+  @property({type: Object}) logoutTimer;
 
   constructor() {
     super();
@@ -313,6 +315,18 @@ export default class BackendAILogin extends BackendAIPage {
     this.blockPanel = this.shadowRoot.querySelector('#block-panel');
     this.notification = globalThis.lablupNotification;
     this.endpoints = globalThis.backendaioptions.get("endpoints", []);
+    if (globalThis.backendaioptions.get('auto_logout')) {    
+      document.onkeypress = () => this.setLogoutTimer();
+      document.onmousemove = () => this.setLogoutTimer();
+    }
+    document.addEventListener('backend-ai-auto-logout', (e: any) => {
+      if (e.detail) {
+        this.setLogoutTimer();
+      } else {
+        clearTimeout(this.logoutTimerBeforeOneMin);
+        clearTimeout(this.logoutTimer);
+      }
+    });
   }
 
   /**
@@ -644,6 +658,10 @@ export default class BackendAILogin extends BackendAIPage {
       globalThis.backendaioptions.set('login_attempt', loginAttempt + 1, 'general');
     }
 
+    if (globalThis.backendaioptions.get('auto_logout')) {
+      this.setLogoutTimer();
+    }
+
     this.api_endpoint = (this.shadowRoot.querySelector('#id_api_endpoint') as any).value;
     this.api_endpoint = this.api_endpoint.replace(/\/+$/, "");
     if (this.api_endpoint === '') {
@@ -961,6 +979,28 @@ export default class BackendAILogin extends BackendAIPage {
     this.shadowRoot.querySelector('#id_user_id').disabled = false;
     this.shadowRoot.querySelector('#id_password').disabled = false;
     this.shadowRoot.querySelector('#loading-animator').style.display = 'none';
+  }
+
+  /**
+   * 
+   * @param msec - maximum minute to logout automatically. default value is 3.
+   */
+  setLogoutTimer(msec = 3) {
+    const msecToMinute = 60000;
+    let minute = msec * msecToMinute;
+
+    clearTimeout(this.logoutTimerBeforeOneMin);
+    clearTimeout(this.logoutTimer);
+
+    if (globalThis.backendaioptions.get('auto_logout')) {
+      this.logoutTimerBeforeOneMin = setTimeout( () => {
+        alert(_text("usersettings.OneMinuteLeftAlert"));
+      } , minute - msecToMinute);
+      this.logoutTimer = setTimeout( async () => {
+        let event = new CustomEvent("backend-ai-logout", {"detail": ""});
+        document.dispatchEvent(event);
+      } , minute);
+    }
   }
 
   render() {
