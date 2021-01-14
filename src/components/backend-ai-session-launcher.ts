44a0ba02fd0a3bfbec184011ea8ed69e08de921e
Jeongkyu Shin <inureyes@gmail.com>
2021-01-14 22:50:44 +0900

    feat: add triton inference server to support app list (#890)

--- src/components/backend-ai-session-launcher.ts
+++ src/components/backend-ai-session-launcher.ts
@@ -1104,6 +1104,7 @@ export default class BackendAiSessionLauncher extends BackendAIPage {
       'base': 'Base',
       'cntk': 'CNTK',
       'h2o': 'H2O.AI',
+      'triton-server': 'Triton Server',
       'digits': 'DIGITS',
       'ubuntu-linux': 'Ubuntu Linux',
       'tf1': 'TensorFlow 1',
