c772cb3bab0970a34a02e49f532df7cfdf2e2a3d
jihyunkang <lizable.me@gmail.com>
2021-01-21 11:06:41 +0900

    fix: Update broken URL links to proper documentation pages.

--- src/components/backend-ai-help-button.ts
+++ src/components/backend-ai-help-button.ts
@@ -29,11 +29,12 @@ export default class BackendAiHelpButton extends BackendAIPage {
     '': 'summary/summary.html',
     'summary': 'summary/summary.html',
     'data': 'vfolder/vfolder.html',
-    'job': 'session_use/session_use.html',
+    'job': 'sessions_all/sessions_all.html',
+    'statistics': 'statistics/statistics.html',
     'credential': 'admin_user_keypair_management/admin_user_keypair_management.html',
     'environment': 'admin_user_keypair_management/admin_user_keypair_management.html#manage-images',
     'agent': 'admin_user_keypair_management/admin_user_keypair_management.html#query-agent-nodes',
-    'settings': 'admin_user_keypair_management/admin_user_keypair_management.html#system-settings',
+    'mainetenance': 'admin_user_keypair_management/admin_user_keypair_management.html#system-settings',
     'usersettings': 'user_settings/user_settings.html'
   };
 
