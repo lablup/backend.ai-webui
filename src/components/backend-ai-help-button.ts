7dcc27415f882f46179de5138c13afb21d8c4882
jihyunkang <lizable.me@gmail.com>
2021-01-22 18:06:27 +0900

    fix: Update documentation link for each menu in GUI Console

--- src/components/backend-ai-help-button.ts
+++ src/components/backend-ai-help-button.ts
@@ -28,13 +28,16 @@ export default class BackendAiHelpButton extends BackendAIPage {
   @property({type: Object}) URLmatchingTable = {
     '': 'summary/summary.html',
     'summary': 'summary/summary.html',
-    'data': 'vfolder/vfolder.html',
     'job': 'sessions_all/sessions_all.html',
+    'import': 'import_run/import_run.html',
+    'data': 'vfolder/vfolder.html',
     'statistics': 'statistics/statistics.html',
-    'credential': 'admin_user_keypair_management/admin_user_keypair_management.html',
-    'environment': 'admin_user_keypair_management/admin_user_keypair_management.html#manage-images',
-    'agent': 'admin_user_keypair_management/admin_user_keypair_management.html#query-agent-nodes',
-    'mainetenance': 'admin_user_keypair_management/admin_user_keypair_management.html#system-settings',
+    'credential': 'admin_menu/admin_menu.html',
+    'environment': 'admin_menu/admin_menu.html#manage-images',
+    'agent': 'admin_menu/admin_menu.html#query-agent-nodes',
+    'settings': 'admin_menu/admin_menu.html#system-settings',
+    'mainetenance': 'admin_menu/admin_menu.html#server-management',
+    'information': 'admin_menu/admin_menu.html#detailed-information',
     'usersettings': 'user_settings/user_settings.html'
   };
 
