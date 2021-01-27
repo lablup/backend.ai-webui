1e099529ff106d7f534afbaac99a77f6e9857b60
Jeongkyu Shin <inureyes@gmail.com>
2021-01-27 14:42:26 +0900

    fix: update year-end mark on license description (#896)

--- src/components/backend-ai-resource-broker.ts
+++ src/components/backend-ai-resource-broker.ts
@@ -1,6 +1,6 @@
 /**
  @license
- Copyright (c) 2015-2020 Lablup Inc. All rights reserved.
+ Copyright (c) 2015-2021 Lablup Inc. All rights reserved.
  */
 import {customElement, html, property} from "lit-element";
 import {BackendAIPage} from './backend-ai-page';
