dd718c45d2866a6f177548c684929e9242b81525
Jeongkyu Shin <inureyes@gmail.com>
2021-02-03 20:32:36 +0900

    update: version index (#208)

--- src/components/backend-ai-console.ts
+++ src/components/backend-ai-console.ts
@@ -1346,7 +1346,7 @@ export default class BackendAIConsole extends connect(store)(LitElement) {
               </div>
               <address class="full-menu">
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">21.01.2.210125</small>
+                <small class="sidebar-footer" style="font-size:9px;">21.02.0.210203</small>
               </address>
               <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
                 <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
@@ -1370,7 +1370,7 @@ export default class BackendAIConsole extends connect(store)(LitElement) {
             </div>
             <address class="full-menu">
               <small class="sidebar-footer">Lablup Inc.</small>
-              <small class="sidebar-footer" style="font-size:9px;">21.01.2.210125</small>
+              <small class="sidebar-footer" style="font-size:9px;">21.02.0.210203</small>
             </address>
             <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
               <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
