4e4cd5ed0f795d3c238858dd4ac5c54ca60e072f
Jeongkyu Shin <inureyes@gmail.com>
2021-04-19 10:17:28 +0900

    feat: cleanup dependency tree (#940)

--- src/components/backend-ai-webui.ts
+++ src/components/backend-ai-webui.ts
@@ -1360,7 +1360,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
               </div>
               <address class="full-menu">
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">21.03.3.210418</small>
+                <small class="sidebar-footer" style="font-size:9px;">21.03.3.210419</small>
               </address>
               <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
                 <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
@@ -1384,7 +1384,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
             </div>
             <address class="full-menu">
               <small class="sidebar-footer">Lablup Inc.</small>
-              <small class="sidebar-footer" style="font-size:9px;">21.03.3.210418</small>
+              <small class="sidebar-footer" style="font-size:9px;">21.03.3.210419</small>
             </address>
             <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
               <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
