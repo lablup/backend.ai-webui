2d43a149854140d3fdc74f114292dd233608a28b
Doeun Sim <sdeun24@postech.ac.kr>
2021-08-05 17:48:08 +0900

    8/5 draft PR

--- src/components/lablup-loading-dots.ts
+++ src/components/lablup-loading-dots.ts
@@ -36,6 +36,7 @@ export default class LablupLoadingdots extends LitElement {
           width: 100px;
           background-color: transparent;
         }
+        
 
         .pulse-container {
           width: 100px;
