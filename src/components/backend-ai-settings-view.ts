2a77b8df3237b44330a94f17fdf82deeaa527448
YEEUN LEE <82070770+euni00@users.noreply.github.com>
2021-10-05 17:27:12 +0900

    Fix: Make the text of the config button horizontally (#1114)

--- src/components/backend-ai-settings-view.ts
+++ src/components/backend-ai-settings-view.ts
@@ -154,6 +154,7 @@ export default class BackendAiSettingsView extends BackendAIPage {
         .setting-button {
           float: right;
           width: 35px;
+          white-space: nowrap;
         }
 
         .setting-desc-pulldown {
