b9be61131a861e5705d75a8f41878fcecf682422
Jihyun Kang <lizable.me@gmail.com>
2021-10-15 14:49:31 +0900

    add: _gBToByte function for sending in byte, not GB as storage quota

--- src/components/backend-ai-credential-view.ts
+++ src/components/backend-ai-credential-view.ts
@@ -491,7 +491,7 @@ export default class BackendAICredentialView extends BackendAIPage {
     this.concurrency_limit['value'] = this.concurrency_limit['value'] === '' ? 0 : parseInt(this.concurrency_limit['value']);
     this.idle_timeout['value'] = this.idle_timeout['value'] === '' ? 0 : parseInt(this.idle_timeout['value']);
     this.container_per_session_limit['value'] = this.container_per_session_limit['value'] === '' ? 0 : parseInt(this.container_per_session_limit['value']);
-    this.vfolder_capacity['value'] = this.vfolder_capacity['value'] === '' ? 0 : parseInt(this.vfolder_capacity['value']);
+    this.vfolder_capacity['value'] = this.vfolder_capacity['value'] === '' ? 0 : parseFloat(this.vfolder_capacity['value']);
     this.vfolder_max_limit['value'] = this.vfolder_max_limit['value'] === '' ? 0 : parseInt(this.vfolder_max_limit['value']);
 
     Object.keys(total_resource_slots).map((resource) => {
@@ -507,7 +507,7 @@ export default class BackendAICredentialView extends BackendAIPage {
       'max_containers_per_session': this.container_per_session_limit['value'],
       'idle_timeout': this.idle_timeout['value'],
       'max_vfolder_count': this.vfolder_max_limit['value'],
-      'max_vfolder_size': this.vfolder_capacity['value'],
+      'max_vfolder_size': this._gBToByte(this.vfolder_capacity['value']),
       'allowed_vfolder_hosts': vfolder_hosts
     };
     return input;
@@ -989,6 +989,11 @@ export default class BackendAICredentialView extends BackendAIPage {
     if (itemCount > 0) {
       this.shadowRoot.querySelector('#dropdown-area').style.height = (actualHeight + itemCount * 14) +'px';
     }
+  }
+
+  _gBToByte(value: number = 0) {
+    const gigabyte = Math.pow(2, 30);
+    return Math.round(gigabyte * value);
   }
 
   render() {
