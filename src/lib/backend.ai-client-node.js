991e1d52761579bec1e4a74d935798eeddcc9846
jihyunkang <46954439+lizable@users.noreply.github.com>
2021-12-23 15:02:19 +0900

    feat: provide a dialog used for detailed status data (#1174)
    * add: KO/EN translation for session status data dialog
    
    * add: receive status_data from 20.09 version of the manager
    
    * add: dialog for detailed status data when session is in PENDING, TERMINATED, CANCELLED status

--- src/lib/backend.ai-client-node.js
+++ src/lib/backend.ai-client-node.js
@@ -471,6 +471,7 @@ class Client {
         }
         if (this.isManagerVersionCompatibleWith('20.09.16')) {
             this._features['avoid-hol-blocking'] = true;
+            this._features['session-detail-status'] = true;
         }
     }
     /**
