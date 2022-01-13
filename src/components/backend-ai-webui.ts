a9bb7ce1a7a9e2feb19ead0cac45ffde0c37616e
Jeongkyu Shin <inureyes@gmail.com>
2022-01-13 19:17:58 +0900

    fix: downgrade Electron to keep login session (#208)

--- src/components/backend-ai-webui.ts
+++ src/components/backend-ai-webui.ts
@@ -1360,7 +1360,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
               </div>
               <address class="full-menu">
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">21.09.2.211109</small>
+                <small class="sidebar-footer" style="font-size:9px;">21.09.3.220113</small>
               </address>
               <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
                 <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
@@ -1384,7 +1384,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
             </div>
             <address class="full-menu">
               <small class="sidebar-footer">Lablup Inc.</small>
-              <small class="sidebar-footer" style="font-size:9px;">21.09.2.211109</small>
+              <small class="sidebar-footer" style="font-size:9px;">21.09.3.220113</small>
             </address>
             <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
               <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
