c8f531766ac55508d186e9d7f50a1e62615a2dac
Jihyun Kang <jihyunkang@lablup.com>
2022-02-07 02:05:27 +0900

    add: draft node add/delete feature with zoom/lock option

--- src/pipeline/components/pipeline-template-view.ts
+++ src/pipeline/components/pipeline-template-view.ts
@@ -2,8 +2,23 @@
  @license
  Copyright (c) 2015-2022 Lablup Inc. All rights reserved.
  */
- import {LitElement} from 'lit';
- import {customElement} from 'lit/decorators.js';
+import {css, CSSResultGroup, html, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+import '../../components/lablup-activity-panel';
+import {BackendAiStyles} from '../../components/backend-ai-general-styles';
+import {
+  IronFlex,
+  IronFlexAlignment,
+  IronFlexFactors,
+  IronPositioning
+} from '../../plastics/layout/iron-flex-layout-classes';
+import '@material/mwc-tab-bar/mwc-tab-bar';
+import '@material/mwc-tab/mwc-tab';
+import '@material/mwc-button';
+import '../lib/pipeline-flow';
+import './pipeline-template-list';
+import { DrawflowNode } from 'drawflow';
 
 /**
  Pipeline Template View
@@ -19,16 +34,169 @@
 @group Backend.AI pipeline
  @element pipeline-template-view
 */
+
 @customElement('pipeline-template-view')
 export default class PipelineTemplateView extends LitElement {
   public shadowRoot: any; // ShadowRoot
+  @property({type: String}) _activeTab = 'template-list';
+  @property({type: Boolean}) isNodeSelected = false;
+  @property({type: Object}) selectedNode;
 
   constructor() {
     super();
   }
 
+  static get styles(): CSSResultGroup | undefined {
+    return [
+      BackendAiStyles,
+      IronFlex,
+      IronFlexAlignment,
+      IronFlexFactors,
+      IronPositioning,
+      // language=CSS
+      css`
+        h3.tab {
+          background-color: var(--general-tabbar-background-color);
+          border-radius: 5px 5px 0px 0px;
+          margin: 0px auto;
+        }
+
+        mwc-tab-bar {
+          --mdc-theme-primary: var(--general-sidebar-selected-color);
+          --mdc-text-transform: none;
+          --mdc-tab-color-default: var(--general-tabbar-background-color);
+          --mdc-tab-text-label-color-default: var(--general-tabbar-tab-disabled-color);
+        }
+
+        mwc-button {
+          margin: 10px;
+        }
+      `
+    ];
+  }
+
+  firstUpdated() {
+    document.addEventListener('node-selected', (e: any) => {
+      if (e.detail) {
+        this.selectedNode = e.detail;
+        this.isNodeSelected = true;
+      }
+    });
+    document.addEventListener('node-unselected', (e: any) => {
+      this.selectedNode = {};
+      this.isNodeSelected = false;
+    });
+  }
+
+  _showTab(tab) {
+    const els = this.shadowRoot.querySelectorAll('.tab-content');
+    for (const obj of els) {
+      obj.style.display = 'none';
+    }
+    this._activeTab = tab.title;
+    this.shadowRoot.querySelector('#' + tab.title).style.display = 'block';
+  }
+
+  /**
+   * Create a task in pipeline template
+   */
+  _createTask() {
+    /**
+     * TODO: Add task with config launcher
+     * 
+     * procedure:
+     *    step 1. Select task type from 3 options: from URL / from YAML / custom task
+     *    step 2. Show task configuration dialog
+     *    step 3. Trigger custom event to add corresponding node into pipeline-flow pane.
+     */
+    
+    // step 1. Select task type from 3 options: from URL / from YAML / custom task
+
+    // step 2. Show task configuration dialog
+
+    // step 3. Trigger custom event to add corresponding node into pipeline-flow pane.
+    // detail: {name, inputs #, outputs #, pos_x, pos_y, class, data, html, typenode}
+    const dummyNodeInfo = {
+      name: 'dummy',
+      inputs: 1,
+      outputs: 1,
+      pos_x: 100,
+      pos_y: 200,
+      class: 'new-task',
+      data: {},
+      html: 'dummy', // put raw html code
+    };
+    const addTaskEvent = new CustomEvent("add-task", {'detail': dummyNodeInfo});
+    document.dispatchEvent(addTaskEvent);
+  }
+
+  /**
+   * Edit the selected task in pipeline template
+   */
+  _editTask() {
+    /**
+     * TODO: Edit task in config launcher
+     * 
+     * * NOTE *
+     * For now, when task is created, the user cannot change the type of task.
+     * 
+     * procedure:
+     *    step 1. Show task configuration dialog
+     *    step 2. Update the task
+     */
+
+    // step 1. Show task configuration dialog
+
+    // step 2. Update the task
+    console.log('_editTask function Called!');
+  }
+
+  /**
+   * Remove the selected task in pipeline template
+   */
+  _removeTask() {
+    /**
+     * TODO: Remove task with confirmation dialog
+     * 
+     * procedure:
+     *    step 1. Show remove task confirmation dialog
+     *    step 2. If confirmation dialog returns true, then dispatchEvent to remove the node.
+     */
+    
+    // step 1. Show remove task confirmation dialog
+
+    // step 2. If confirmation dialog returns true, then dispatchEvent to remove the node.
+    const removeTaskEvent = new CustomEvent("remove-task", {'detail': this.selectedNode.id});
+    document.dispatchEvent(removeTaskEvent);
+    console.log('_removeTask function Called!');
+  }
+
   render() {
     // language=HTML
+    return html`
+      <lablup-activity-panel noheader narrow autowidth>
+        <div slot="message">
+          <h3 class="tab horizontal center layout">
+            <mwc-tab-bar>
+              <mwc-tab title="template-list" label="Template List" @click="${(e) => this._showTab(e.target)}"></mwc-tab>
+              <mwc-tab title="template-view" label="Template View" @click="${(e) => this._showTab(e.target)}"></mwc-tab>
+            </mwc-tab-bar>
+          </h3>
+        <div id="template-list" class="tab-content">
+          <pipeline-template-list></pipeline-template-list>
+        </div>
+        <div id="template-view" class="tab-content" style="display:none;">
+          <div class="horizontal layout flex center end-justified">
+            ${this.isNodeSelected ? html`
+              <mwc-button outlined icon="delete" label="Remove Task" @click="${() => this._removeTask()}"></mwc-button>
+              <mwc-button outlined icon="edit" label="Edit Task" @click="${() => this._editTask()}"></mwc-button>
+            ` : html``}
+              <mwc-button unelevated icon="add" label="New Task" @click="${() => this._createTask()}"></mwc-button>
+          </div>
+          <pipeline-flow isEditable></pipeline-flow>
+        </div>
+      </lablup-activity-panel>
+    `;
   }
 }
 
--- src/pipeline/lib/pipeline-flow.ts
+++ src/pipeline/lib/pipeline-flow.ts
@@ -2,8 +2,22 @@
  @license
  Copyright (c) 2015-2022 Lablup Inc. All rights reserved.
  */
- import {css, CSSResultGroup, html, LitElement} from 'lit';
- import {customElement, property, query} from 'lit/decorators.js';
+import {css, CSSResultGroup, html, LitElement} from 'lit';
+import {customElement, property, query} from 'lit/decorators.js';
+import '@material/mwc-button';
+import '@material/mwc-icon-button';
+import '@material/mwc-icon-button-toggle';
+import 'drawflow';
+import { style } from 'drawflow/dist/drawflow.style';
+import { DrawflowBaseStyle } from './drawflow-base-style.css';
+import 'drawflow/dist/drawflow.min';
+import { DrawflowNode } from 'drawflow';
+import {
+  IronFlex,
+  IronFlexAlignment,
+  IronFlexFactors,
+  IronPositioning
+} from '../../plastics/layout/iron-flex-layout-classes';
 
 /**
  Pipeline Flow
@@ -23,13 +37,269 @@
 @customElement('pipeline-flow')
 export default class PipelineFlow extends LitElement {
   public shadowRoot: any; // ShadowRoot
+  @property({type: Object}) editor;
+  @property({type: Object}) data;
+  @property({type: Boolean}) isEditable = false;
+
+  static get styles() {
+    return [
+      style,
+      DrawflowBaseStyle,
+      IronFlex,
+      IronFlexAlignment,
+      IronFlexFactors,
+      IronPositioning,
+      css`
+        #drawflow {
+          display: inline-flex;
+          position: relative;
+          width: 100%;
+          height: 500px;
+          background: var(--dfBackgroundColor);
+          background-size: var(--dfBackgroundSize) var(--dfBackgroundSize);
+          background-image: var(--dfBackgroundImage);
+        }
+        
+        .drawflow .drawflow-node {
+          display: var(--dfNodeType);
+          background: var(--dfNodeBackgroundColor);
+          color: var(--dfNodeTextColor);
+          border: var(--dfNodeBorderSize)  solid var(--dfNodeBorderColor);
+          border-radius: var(--dfNodeBorderRadius);
+          min-height: var(--dfNodeMinHeight);
+          width: auto;
+          min-width: var(--dfNodeMinWidth);
+          padding-top: var(--dfNodePaddingTop);
+          padding-bottom: var(--dfNodePaddingBottom);
+          -webkit-box-shadow: var(--dfNodeBoxShadowHL) var(--dfNodeBoxShadowVL) var(--dfNodeBoxShadowBR) var(--dfNodeBoxShadowS) var(--dfNodeBoxShadowColor);
+          box-shadow:  var(--dfNodeBoxShadowHL) var(--dfNodeBoxShadowVL) var(--dfNodeBoxShadowBR) var(--dfNodeBoxShadowS) var(--dfNodeBoxShadowColor);
+        }
+        
+        .drawflow .drawflow-node:hover {
+          background: var(--dfNodeHoverBackgroundColor);
+          color: var(--dfNodeHoverTextColor);
+          border: var(--dfNodeHoverBorderSize)  solid var(--dfNodeHoverBorderColor);
+          border-radius: var(--dfNodeHoverBorderRadius);
+          -webkit-box-shadow: var(--dfNodeHoverBoxShadowHL) var(--dfNodeHoverBoxShadowVL) var(--dfNodeHoverBoxShadowBR) var(--dfNodeHoverBoxShadowS) var(--dfNodeHoverBoxShadowColor);
+          box-shadow:  var(--dfNodeHoverBoxShadowHL) var(--dfNodeHoverBoxShadowVL) var(--dfNodeHoverBoxShadowBR) var(--dfNodeHoverBoxShadowS) var(--dfNodeHoverBoxShadowColor);
+        }
+        
+        .drawflow .drawflow-node.selected {
+          background: var(--dfNodeSelectedBackgroundColor);
+          color: var(--dfNodeSelectedTextColor);
+          border: var(--dfNodeSelectedBorderSize)  solid var(--dfNodeSelectedBorderColor);
+          border-radius: var(--dfNodeSelectedBorderRadius);
+          -webkit-box-shadow: var(--dfNodeSelectedBoxShadowHL) var(--dfNodeSelectedBoxShadowVL) var(--dfNodeSelectedBoxShadowBR) var(--dfNodeSelectedBoxShadowS) var(--dfNodeSelectedBoxShadowColor);
+          box-shadow:  var(--dfNodeSelectedBoxShadowHL) var(--dfNodeSelectedBoxShadowVL) var(--dfNodeSelectedBoxShadowBR) var(--dfNodeSelectedBoxShadowS) var(--dfNodeSelectedBoxShadowColor);
+        }
+        
+        .drawflow .drawflow-node .input {
+          left: var(--dfInputLeft);
+          background: var(--dfInputBackgroundColor);
+          border: var(--dfInputBorderSize)  solid var(--dfInputBorderColor);
+          border-radius: var(--dfInputBorderRadius);
+          height: var(--dfInputHeight);
+          width: var(--dfInputWidth);
+        }
+        
+        .drawflow .drawflow-node .input:hover {
+          background: var(--dfInputHoverBackgroundColor);
+          border: var(--dfInputHoverBorderSize)  solid var(--dfInputHoverBorderColor);
+          border-radius: var(--dfInputHoverBorderRadius);
+        }
+        
+        .drawflow .drawflow-node .outputs {
+          float: var(--dfNodeTypeFloat);
+        }
+        
+        .drawflow .drawflow-node .output {
+          right: var(--dfOutputRight);
+          background: var(--dfOutputBackgroundColor);
+          border: var(--dfOutputBorderSize)  solid var(--dfOutputBorderColor);
+          border-radius: var(--dfOutputBorderRadius);
+          height: var(--dfOutputHeight);
+          width: var(--dfOutputWidth);
+        }
+        
+        .drawflow .drawflow-node .output:hover {
+          background: var(--dfOutputHoverBackgroundColor);
+          border: var(--dfOutputHoverBorderSize)  solid var(--dfOutputHoverBorderColor);
+          border-radius: var(--dfOutputHoverBorderRadius);
+        }
+        
+        .drawflow .connection .main-path {
+          stroke-width: var(--dfLineWidth);
+          stroke: var(--dfLineColor);
+        }
+        
+        .drawflow .connection .main-path:hover {
+          stroke: var(--dfLineHoverColor);
+        }
+        
+        .drawflow .connection .main-path.selected {
+          stroke: var(--dfLineSelectedColor);
+        }
+        
+        .drawflow .connection .point {
+          stroke: var(--dfRerouteBorderColor);
+          stroke-width: var(--dfRerouteBorderWidth);
+          fill: var(--dfRerouteBackgroundColor);
+        }
+        
+        .drawflow .connection .point:hover {
+          stroke: var(--dfRerouteHoverBorderColor);
+          stroke-width: var(--dfRerouteHoverBorderWidth);
+          fill: var(--dfRerouteHoverBackgroundColor);
+        }
+        
+        .drawflow-delete {
+          display: var(--dfDeleteDisplay);
+          color: var(--dfDeleteColor);
+          background: var(--dfDeleteBackgroundColor);
+          border: var(--dfDeleteBorderSize) solid var(--dfDeleteBorderColor);
+          border-radius: var(--dfDeleteBorderRadius);
+        }
+        
+        .parent-node .drawflow-delete {
+          top: var(--dfDeleteTop);
+        }
+        
+        .drawflow-delete:hover {
+          color: var(--dfDeleteHoverColor);
+          background: var(--dfDeleteHoverBackgroundColor);
+          border: var(--dfDeleteHoverBorderSize) solid var(--dfDeleteHoverBorderColor);
+          border-radius: var(--dfDeleteHoverBorderRadius);
+        }
+
+        mwc-icon-button,
+        mwc-icon-button-toggle {
+          color: white;
+          --mdc-icon-size: 36px;
+        }
+
+        .pane-options {
+          margin: 5px 10px;
+          background: #555555;
+          border-radius: 10px;
+          border-right: 1px solid var(--border-color);
+          z-index: 5;
+        }
+      `
+    ];
+  }
 
   constructor() {
     super();
   }
 
+  firstUpdated() {
+    const element: HTMLElement = this.shadowRoot?.getElementById('drawflow')!;
+    this.editor = new Drawflow(element);
+    this.editor.start();
+
+    this.data = {};
+
+    document.addEventListener('add-task', (e: any) => {
+      const isEditorMode = this.editor.editor_mode === 'edit';
+      if (e.detail && isEditorMode) {
+        this._addNode(e.detail);
+      }
+    });
+
+    document.addEventListener('remove-task', (e: any) => {
+      if (e.detail) {
+        this._removeNode(e.detail);
+      }
+    });
+
+    this.editor.on('nodeSelected', (nodeId: any) => {
+      const nodeInfo: DrawflowNode = this.editor.getNodeFromId(nodeId);
+      const nodeSelectedEvent = new CustomEvent('node-selected', {'detail': nodeInfo});
+      document.dispatchEvent(nodeSelectedEvent);
+    });
+
+    this.editor.on('nodeUnselected', () => {
+      const nodeUnselectedEvent = new CustomEvent('node-unselected', {'detail': false});
+      document.dispatchEvent(nodeUnselectedEvent);
+    })
+
+    // Example data
+    // this.editor.addNode('MNIST', 1, 1, 100, 200, 'foo', this.data, 'MNIST');
+    // this.editor.addNode('CNN_3conv2_FC_flatten_hidden_Adam', 1, 1, 400, 100, 'bar', this.data, 'CNN_3conv2_FC_flatten_hidden_Adam');
+    // this.editor.addNode('CNN_1conv_5x5_Adam', 1, 1, 400, 300, 'bar', this.data, 'CNN_1conv_5x5_Adam');
+
+    // this.editor.addConnection(1, 2, "output_1", "input_1");
+    // this.editor.addConnection(1, 3, "output_1", "input_1");
+  }
+
   render() {
     // language=HTML
+    return html`
+      <div class="vertical layout center flex">
+        <div id="drawflow"></div>
+      </div>
+      <div class="horizontal layout center end-justified flex">
+        <div class="pane-options">
+          <mwc-icon-button-toggle ?on="${this.isEditable}" onIcon="lock_open" offIcon="lock" @click="${this._togglePaneMode}"></mwc-icon-button-toggle>
+        </div>
+        <div class="pane-options">
+          <mwc-icon-button icon="zoom_out" @click="${this._zoomOutPane}}"></mwc-icon-button>
+          <mwc-icon-button icon="search" @click="${this._resetZoom}"></mwc-icon-button>
+          <mwc-icon-button icon="zoom_in" @click="${this._zoomInPane}"></mwc-icon-button>
+        </div>
+      </div>
+    `;
+  }
+
+
+  /**
+   * Add node into pane 
+   */
+  _addNode(nodeInfo: DrawflowNode) {
+    this.editor.addNode(nodeInfo.name, nodeInfo.inputs, nodeInfo.outputs, 
+                        nodeInfo.pos_x, nodeInfo.pos_y, nodeInfo.class, nodeInfo.data, nodeInfo.html);
+  }
+
+  /**
+   * Remove node from pane
+   * 
+   * @param id 
+   */
+  _removeNode(id: number) {
+    // NOTE: every node element id starts with 'node-'
+    const nodeId = 'node-' + id;
+    this.editor.removeNodeId(nodeId);
+  }
+
+  /**
+   * Zoom in 10% from current pane
+   * 
+   */
+  _zoomInPane() {
+    this.editor.zoom_in();
+  }
+
+  /**
+   * Zoom out 10% from current pane
+   */
+  _zoomOutPane() {
+    this.editor.zoom_out();
+  }
+
+  /**
+   * Reset Zoom amount to default status
+   */
+  _resetZoom() {
+    this.editor.zoom_reset();
+  }
+
+  /**
+   * Toggle Pane Mode into two option
+   */
+  _togglePaneMode() {
+    if (this.isEditable) {
+      this.editor.editor_mode = this.editor.editor_mode === 'edit' ? 'fixed' : 'edit';
+    }
   }
 }
 
