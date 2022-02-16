a5f7b1fbb1390804391e563c9d1a76e3525ef9fb
ggstone0523 <tanks2438@outlook.kr>
2022-02-17 01:39:56 +0900

    fix: make scrollable create/modify dialog

--- src/components/backend-ai-scaling-group-list.ts
+++ src/components/backend-ai-scaling-group-list.ts
@@ -686,9 +686,8 @@ export default class BackendAIScalingGroupList extends BackendAIPage {
         <vaadin-grid-column flex-grow="1" header="${_t('general.Control')}" .renderer=${this._boundControlRenderer}>
         </vaadin-grid-column>
       </vaadin-grid>
-      <backend-ai-dialog id="create-scaling-group-dialog" fixed backdrop blockscrolling>
+      <backend-ai-dialog id="create-scaling-group-dialog" fixed backdrop blockscrolling scrollable>
         <span slot="title">${_t('resourceGroup.CreateResourceGroup')}</span>
-
         <div slot="content" class="login-panel intro centered">
           <mwc-select
             id="scaling-group-domain"
@@ -764,7 +763,7 @@ export default class BackendAIScalingGroupList extends BackendAIPage {
               @click="${this._createScalingGroup}"></mwc-button>
         </div>
       </backend-ai-dialog>
-      <backend-ai-dialog id="modify-scaling-group-dialog" fixed backdrop blockscrolling>
+      <backend-ai-dialog id="modify-scaling-group-dialog" fixed backdrop blockscrolling scrollable>
         <span slot="title">${_t('resourceGroup.ModifyResourceGroup')}</span>
         <div slot="content" class="vertical layout wrap">
           <div class="horizontal layout flex wrap center justified">
