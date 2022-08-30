a21eb36ad500bc0a10a914ce991d87173ecc682e
Jeongkyu Shin <inureyes@gmail.com>
2022-08-30 11:08:12 +0900

    update: package compatibility test with beta tag (#208)
     * electron should be kept for cookie-based login support (will be
       replaced soon.)
     * chart.js 3.9 has rollup build problem with ESM support. see (
       https://github.com/chartjs/Chart.js/issues/10599 , https://github.com/chartjs/Chart.js/pull/10479 )

--- src/components/backend-ai-webui.ts
+++ src/components/backend-ai-webui.ts
@@ -1434,7 +1434,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
               </div>
               <address class="full-menu">
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">22.09.0.220830</small>
+                <small class="sidebar-footer" style="font-size:9px;">22.09.1-beta.1.220830</small>
               </address>
               <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
                 <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
@@ -1458,7 +1458,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
             </div>
             <address class="full-menu">
               <small class="sidebar-footer">Lablup Inc.</small>
-              <small class="sidebar-footer" style="font-size:9px;">22.09.0.220830</small>
+              <small class="sidebar-footer" style="font-size:9px;">22.09.1-beta.1.220830</small>
             </address>
             <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
               <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
