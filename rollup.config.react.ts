ba797209fdfaaf1016f23d7189c9a744298a8910
Jeongkyu Shin <inureyes@gmail.com>
2022-09-06 14:15:48 +0900

    add: css module integration test

--- rollup.config.react.ts
+++ rollup.config.react.ts
@@ -2,13 +2,16 @@ import typescript from '@rollup/plugin-typescript';
 import babel from "@rollup/plugin-babel";
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import autoprefixer from 'autoprefixer';
+import postcss from 'rollup-plugin-postcss';
 
 export default {
+  //preserveModules: true,
   input: ['src/components-react/hello.tsx'],
   output: {
     // dir: 'build/rollup/dist/components-react',
     dir: 'src/components-react/dist',
-    format: 'es',
+    format: 'esm',
     sourcemap: false
   },
   plugins: [
@@ -18,6 +21,13 @@ export default {
       babelHelpers: "bundled",
       presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
       extensions: [".ts", ".tsx"],
+    }),
+    postcss({
+      plugins: [autoprefixer()],
+      sourceMap: true,
+      modules: true,
+      extract: false,
+      minimize: true
     }),
     typescript({
       tsconfig: "./tsconfig.react.json",
--- src/components-react/hello.tsx
+++ src/components-react/hello.tsx
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 import * as ReactDOM from "react-dom/client";
 import reactToWebComponent from "react-to-webcomponent";
+import styles from "./hello.css";
 
 const Hello = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <p>You clicked {count} times</p>
+      <p className={styles.helloTest}>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
     </div>
   );
 };
+console.log(styles);
 
-const BackendAiReactTestInternalView = reactToWebComponent((Hello as any), (React as any), (ReactDOM as any));
+const BackendAiReactTestInternalView = reactToWebComponent((Hello as any), (React as any), (ReactDOM as any), {
+  shadow: "open",
+});
 customElements.define('backend-ai-react-test-internal-view', (BackendAiReactTestInternalView as any));
 
 declare global {
