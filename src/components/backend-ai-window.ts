dd2db633061018f3ae3673809f9f99fb71818b17
Jeongkyu Shin <inureyes@gmail.com>
2022-09-27 15:49:12 +0900

    update: window shadow

--- src/components/backend-ai-window.ts
+++ src/components/backend-ai-window.ts
@@ -75,7 +75,8 @@ export default class BackendAIWindow extends LitElement {
           border-radius: 5px;
           /*box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;*/
           /*box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;*/
-          box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+          /*box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;*/
+          box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
           position:absolute;
           resize: both;
           overflow: hidden;
