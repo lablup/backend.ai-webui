2a1324473a3d795709520fab001638ef2bcd54af
Jeongkyu Shin <inureyes@gmail.com>
2022-09-30 12:15:28 +0900

    add: backend-ai-desktop skeleton

---
+++ src/components/backend-ai-desktop.ts
@@ -0,0 +1,25 @@
+/**
+ @license
+ Copyright (c) 2015-2022 Lablup Inc. All rights reserved.
+ */
+import {LitElement} from 'lit';
+import {customElement, state} from 'lit/decorators.js';
+import BackendAIWindow from './backend-ai-window';
+import './backend-ai-window';
+
+/**
+ Backend AI Desktop
+
+ Desktop is an base element to put windows, widgets and workspaces.
+
+ @group Backend.AI Desktop
+ @element backend-ai-desktop
+ */
+@customElement('backend-ai-desktop')
+export default class BackendAIDesktop extends LitElement {
+
+  constructor() {
+    super();
+  }
+}
+
