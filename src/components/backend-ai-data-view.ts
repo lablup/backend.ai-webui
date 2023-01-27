f6cd0b389ff8cf1690f697083383ec1efa352448
Jihyun Kang <jihyunkang@lablup.com>
2023-01-27 19:11:13 +0900

    fix: temporally comment out unused folder usage mode

--- src/components/backend-ai-data-view.ts
+++ src/components/backend-ai-data-view.ts
@@ -71,7 +71,7 @@ export default class BackendAIData extends BackendAIPage {
   @property({type: String}) deleteFolderId = '';
   @property({type: String}) vhost = '';
   @property({type: Array}) vhosts = [];
-  @property({type: Array}) usageModes = ['General', 'Data', 'Model'];
+  @property({type: Array}) usageModes = ['General']; // FIXME: temporally hide unused folder usage modes ['Data', 'Model'];
   @property({type: Array}) permissions = ['Read-Write', 'Read-Only', 'Delete'];
   @property({type: Array}) allowedGroups = [];
   @property({type: Array}) allowed_folder_type:string[] = [];
