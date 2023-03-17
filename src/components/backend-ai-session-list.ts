de5ca28abfcc6b52ccc4147e02b0d7235f410856
Jihyun Kang <jihyunkang@lablup.com>
2023-03-17 16:19:34 +0900

    fix: change id of tooltip anchor element reflects to index not session_id

--- src/components/backend-ai-session-list.ts
+++ src/components/backend-ai-session-list.ts
@@ -1864,6 +1864,7 @@ export default class BackendAiSessionList extends BackendAIPage {
    * @param {Object} rowData - the object with the properties related with the rendered item
    * */
   controlRenderer(root, column?, rowData?) {
+    console.log(rowData)
     let mySession = true;
     mySession = (this._connectionMode === 'API' && rowData.item.access_key === globalThis.backendaiclient._config._accessKey) ||
       (rowData.item.user_email === globalThis.backendaiclient.email);
@@ -1876,19 +1877,19 @@ export default class BackendAiSessionList extends BackendAIPage {
              .kernel-image="${rowData.item.kernel_image}"
              .app-services="${rowData.item.app_services}"
              .app-services-option="${rowData.item.app_services_option}">
-          <vaadin-tooltip for="${rowData.item.session_id+'apps'}" text="${_t('session.SeeAppDialog')}" position="top-start"></vaadin-tooltip>
-          <vaadin-tooltip for="${rowData.item.session_id+'terminal'}" text="${_t('session.ExecuteTerminalApp')}" position="top-start"></vaadin-tooltip>
-          <vaadin-tooltip for="${rowData.item.session_id+'power'}" text="${_t('session.TerminateSession')}" position="top-start"></vaadin-tooltip>
-          <vaadin-tooltip for="${rowData.item.session_id+'assignment'}" text="${_t('session.SeeContainerLogs')}" position="top-start"></vaadin-tooltip>
-          <vaadin-tooltip for="${rowData.item.session_id+'archive'}" text="${_t('session.RequestContainerCommit')}" position="top-start"></vaadin-tooltip>
-          <vaadin-tooltip for="${rowData.item.session_id+'nologs'}" text="${_t('session.NoLogMsgAvailable')}" position="top-start"></vaadin-tooltip>
+          <vaadin-tooltip for="${rowData.index+'-apps'}" text="${_t('session.SeeAppDialog')}" position="top-start"></vaadin-tooltip>
+          <vaadin-tooltip for="${rowData.index+'-terminal'}" text="${_t('session.ExecuteTerminalApp')}" position="top-start"></vaadin-tooltip>
+          <vaadin-tooltip for="${rowData.index+'-power'}" text="${_t('session.TerminateSession')}" position="top-start"></vaadin-tooltip>
+          <vaadin-tooltip for="${rowData.index+'-assignment'}" text="${_t('session.SeeContainerLogs')}" position="top-start"></vaadin-tooltip>
+          <vaadin-tooltip for="${rowData.index+'-archive'}" text="${_t('session.RequestContainerCommit')}" position="top-start"></vaadin-tooltip>
+          <vaadin-tooltip for="${rowData.index+'-nologs'}" text="${_t('session.NoLogMsgAvailable')}" position="top-start"></vaadin-tooltip>
           ${rowData.item.appSupport ? html`
             <mwc-icon-button class="fg controls-running green"
-                               id="${rowData.item.session_id+'apps'}"
+                               id="${rowData.index+'-apps'}"
                                @click="${(e) => this._showAppLauncher(e)}"
                                icon="apps"></mwc-icon-button>
             <mwc-icon-button class="fg controls-running"
-                               id="${rowData.item.session_id+'terminal'}"
+                               id="${rowData.index+'-terminal'}"
                                ?disabled="${!mySession}"
                                @click="${(e) => this._runTerminal(e)}">
               <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
@@ -1910,18 +1911,18 @@ export default class BackendAiSessionList extends BackendAIPage {
             </mwc-icon-button>
           ` : html``}
           ${(this._isRunning && !this._isPreparing(rowData.item.status)) || this._isError(rowData.item.status) ? html`
-            <mwc-icon-button class="fg red controls-running" id="${rowData.item.session_id+'power'}" ?disabled=${!this._isPending(rowData.item.status) && rowData.item?.commit_status as CommitSessionStatus === 'ongoing'}
+            <mwc-icon-button class="fg red controls-running" id="${rowData.index+'-power'}" ?disabled=${!this._isPending(rowData.item.status) && rowData.item?.commit_status as CommitSessionStatus === 'ongoing'}
                                icon="power_settings_new" @click="${(e) => this._openTerminateSessionDialog(e)}"></mwc-icon-button>
           ` : html``}
           ${(this._isRunning && !this._isPreparing(rowData.item.status) || this._APIMajorVersion > 4) && !this._isPending(rowData.item.status) ? html`
-            <mwc-icon-button class="fg blue controls-running" id="${rowData.item.session_id+'assignment'}" icon="assignment"
+            <mwc-icon-button class="fg blue controls-running" id="${rowData.index+'-assignment'}" icon="assignment"
                                @click="${(e) => this._showLogs(e)}"></mwc-icon-button>
           ` : html`
-            <mwc-icon-button fab flat inverted disabled class="fg controls-running" id="${rowData.item.session_id+'nologs'}" icon="assignment"></mwc-icon-button>
+            <mwc-icon-button fab flat inverted disabled class="fg controls-running" id="${rowData.index+'-nologs'}" icon="assignment"></mwc-icon-button>
           `}
           ${this._isContainerCommitEnabled ? html`
             <mwc-icon-button class="fg blue controls-running"
-                             id="${rowData.item.session_id+'archive'}"
+                             id="${rowData.index+'-archive'}"
                              ?disabled=${this._isPending(rowData.item.status) ||
                                          this._isPreparing(rowData.item.status) ||
                                          this._isError(rowData.item.status) ||
