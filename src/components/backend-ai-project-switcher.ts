226a4a98057e54fcb4b48182ab6a4302b8839909
Sujin-Kim1 <kimsujin@lablup.com>
2023-04-12 23:22:24 +0900

    refactor: render project list in `render()`

--- src/components/backend-ai-project-switcher.ts
+++ src/components/backend-ai-project-switcher.ts
@@ -4,7 +4,7 @@
  */
 
 
-import {customElement, property, query} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 import {LitElement, html, CSSResultGroup} from 'lit';
 import {translate as _t} from 'lit-translate';
 import '@material/mwc-select';
@@ -33,9 +33,6 @@ import {get as _text} from 'lit-translate/util';
 export default class BackendAIProjectSwitcher extends LitElement {
   @property({type: Array}) projects = [];
   @property({type: String}) currentProject = '';
-  @property({type: Boolean}) isDescending = false;
-
-  @query('#project-select-box') projectSelectionBox: HTMLDivElement | undefined;
 
   constructor() {
     super();
@@ -74,81 +71,41 @@ export default class BackendAIProjectSwitcher extends LitElement {
     document.dispatchEvent(event);
   }
 
-  _sortProjects() {
-    this.isDescending = !this.isDescending;
-    if (this.isDescending) {
-      this.projects.reverse();
+  _sortProjects(e) {
+    const isAscending = e.target.on;
+    if (isAscending) {
+      this.projects = [...this.projects.sort()];
     } else {
-      this.projects.sort();
+      this.projects = [...this.projects.reverse()];
     }
-    this._refreshUserGroupSelector();
   }
 
   _refreshUserGroupSelector(): void {
     this.currentProject = globalThis.backendaiutils._readRecentProjectGroup();
     globalThis.backendaiclient.current_group = this.currentProject;
     this.projects = globalThis.backendaiclient.groups;
-    // Detached from template to support live-update after creating new group (will need it)
-    if (this.projectSelectionBox?.hasChildNodes()) {
-      this.projectSelectionBox?.removeChild(this.projectSelectionBox.firstChild as ChildNode);
-    }
-    const div = document.createElement('div');
-    div.className = 'horizontal center center-justified layout';
-
-    const select = document.createElement('mwc-select');
-    select.id = 'project-select';
-    select.value = this.currentProject;
-    select.style.width = 'auto';
-    select.style.maxWidth = '200px';
-    select.addEventListener('selected', (e) => this.changeGroup(e));
-
-    const projectSortBox = document.createElement('div');
-    projectSortBox.classList.add('horizontal', 'layout', 'center', 'space-between');
-    projectSortBox.style.paddingLeft = '16px';
-    projectSortBox.style.paddingRight = '10px';
-    projectSortBox.style.borderBottom = '1px solid #ccc';
-
-    const selectProjectText = document.createElement('div');
-    selectProjectText.innerHTML = _text('webui.menu.SelectProject');
-
-    const span = document.createElement('span');
-    span.classList.add('flex');
-
-    const sortToggleButton = document.createElement('mwc-icon-button-toggle');
-    sortToggleButton.setAttribute('onIcon', 'arrow_drop_down');
-    sortToggleButton.setAttribute('offIcon', 'arrow_drop_up');
-    if (this.isDescending) {
-      sortToggleButton.setAttribute('on', '');
-    }
-    sortToggleButton.addEventListener('click', () => this._sortProjects());
-
-    projectSortBox.appendChild(selectProjectText);
-    projectSortBox.appendChild(span);
-    projectSortBox.appendChild(sortToggleButton);
-    select.appendChild(projectSortBox);
-
-    let opt;
-    this.projects.map((group) => {
-      opt = document.createElement('mwc-list-item');
-      opt.value = group;
-      if (this.currentProject === group) {
-        opt.selected = true;
-      } else {
-        opt.selected = false;
-      }
-      opt.innerHTML = group;
-      select.appendChild(opt);
-    });
-    // select.updateOptions();
-    div.appendChild(select);
-    this.projectSelectionBox?.appendChild(div);
   }
 
   render() {
     return html`
       <div class="horizontal center center-justified layout">
         <p id="project">${_t('webui.menu.Project')}</p>
-        <div id="project-select-box"></div>
+        <div id="project-select-box">
+          <div class="horizontal center center-justified layout">
+            <mwc-select id="project-select" value="${this.currentProject}"
+                @selected="${(e) => this.changeGroup(e)}">
+              <div id="project-sort-box" class="horizontal layout center space-between" style="padding-left:16px;padding-right:10px;">
+                <div>${_text('webui.menu.SelectProject')}</div>
+                <span class="flex"></span>
+                <mwc-icon-button-toggle on onIcon="arrow_drop_up" offIcon="arrow_drop_down"
+                    @click="${(e) => this._sortProjects(e)}"></mwc-icon-button-toggle>
+              </div>
+              ${this.projects.map((group) => html`
+                <mwc-list-item value="${group}" ?selected="${this.currentProject === group}">${group}</mwc-list-item>
+              `)}
+            </mwc-select>
+          </div>
+        </div>
       </div>
     `;
   }
