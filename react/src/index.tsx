6654a7559558e6cc9a7c0eac2e59e466488d5331
Jihyun Kang <jihyunkang@lablup.com>
2023-06-26 14:34:49 +0900

    fix: add omitted semicolon

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -11,7 +11,6 @@ const DefaultProviders = React.lazy(
 const Information = React.lazy(() => import("./components/Information"));
 const SessionList = React.lazy(() => import("./pages/SessionListPage"));
 const ServingList = React.lazy(() => import("./pages/ServingListPage"));
-
 const ResetPasswordRequired = React.lazy(
   () => import("./components/ResetPasswordRequired")
 );
