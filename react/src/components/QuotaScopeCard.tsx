d37f0b3616895b13034757dd7a78f43500f3cb2b
Jong Eun Lee <jongeun@lablup.com>
2023-07-05 19:31:19 +0800

    feat: compact ui version of storage quota setting (#1765)

--- react/src/components/QuotaScopeCard.tsx
+++ react/src/components/QuotaScopeCard.tsx
@@ -4,7 +4,6 @@ import { useFragment, useMutation } from "react-relay";
 import { QuotaScopeCardUnsetMutation } from "./__generated__/QuotaScopeCardUnsetMutation.graphql";
 
 import {
-  Card,
   CardProps,
   Table,
   Button,
@@ -102,89 +101,85 @@ const QuotaScopeCard: React.FC<Props> = ({
   );
 
   return (
-    <>
-      <Card bordered={false}>
-        <Table
-          bordered
-          columns={[
-            {
-              title: t("storageHost.quotaSettings.QuotaScopeId"),
-              dataIndex: "quota_scope_id",
-              key: "quota_scope_id",
-              render: (value) => <code>{value}</code>,
-            },
-            {
-              title: t("storageHost.HardLimit") + " (GB)",
-              dataIndex: ["details", "hard_limit_bytes"],
-              key: "hard_limit_bytes",
-              render: (value) => <>{bytesToGB(value)}</>,
-            },
-            {
-              title: t("storageHost.Usage") + " (GB)",
-              dataIndex: ["details", "usage_bytes"],
-              key: "usage_bytes",
-              render: (value) => <>{bytesToGB(value)}</>,
-            },
-            {
-              title: t("general.Control"),
-              key: "control",
-              render: () => (
-                <Flex gap={token.marginSM}>
-                  <Button
-                    icon={<EditFilled />}
-                    onClick={() => onClickEdit && onClickEdit()}
-                  >
-                    {t("button.Edit")}
-                  </Button>
-                  <Popconfirm
-                    title={t("storageHost.quotaSettings.UnsetCustomSettings")}
-                    description={t(
-                      "storageHost.quotaSettings.ConfirmUnsetCustomQuota"
-                    )}
-                    placement="bottom"
-                    onConfirm={() => {
-                      if (quota_scope) {
-                        commitUnsetQuotaScope({
-                          variables: {
-                            quota_scope_id: quota_scope.quota_scope_id,
-                            storage_host_name: quota_scope.storage_host_name,
-                          },
-                          onCompleted() {
-                            message.success(
-                              t(
-                                "storageHost.quotaSettings.QuotaScopeSuccessfullyUpdated"
-                              )
-                            );
-                          },
-                          onError(error) {
-                            message.error(error?.message);
-                          },
-                        });
-                      }
-                    }}
-                  >
-                    <Button
-                      loading={isInFlightCommitUnsetQuotaScope}
-                      danger
-                      icon={<CloseOutlined />}
-                    >
-                      {t("button.Unset")}
-                    </Button>
-                  </Popconfirm>
-                </Flex>
-              ),
-            },
-          ]}
-          dataSource={quota_scope ? [quota_scope] : []}
-          locale={{
-            emptyText: showAddButtonWhenEmpty
-              ? addQuotaConfigsWhenEmpty
-              : selectProjectOrUserFirst,
-          }}
-          pagination={false}
-        />
-      </Card>
-    </>
+    <Table
+      bordered
+      columns={[
+        {
+          title: t("storageHost.quotaSettings.QuotaScopeId"),
+          dataIndex: "quota_scope_id",
+          key: "quota_scope_id",
+          render: (value) => <code>{value}</code>,
+        },
+        {
+          title: t("storageHost.HardLimit") + " (GB)",
+          dataIndex: ["details", "hard_limit_bytes"],
+          key: "hard_limit_bytes",
+          render: (value) => <>{bytesToGB(value)}</>,
+        },
+        {
+          title: t("storageHost.Usage") + " (GB)",
+          dataIndex: ["details", "usage_bytes"],
+          key: "usage_bytes",
+          render: (value) => <>{bytesToGB(value)}</>,
+        },
+        {
+          title: t("general.Control"),
+          key: "control",
+          render: () => (
+            <Flex gap={token.marginSM}>
+              <Button
+                icon={<EditFilled />}
+                onClick={() => onClickEdit && onClickEdit()}
+              >
+                {t("button.Edit")}
+              </Button>
+              <Popconfirm
+                title={t("storageHost.quotaSettings.UnsetCustomSettings")}
+                description={t(
+                  "storageHost.quotaSettings.ConfirmUnsetCustomQuota"
+                )}
+                placement="bottom"
+                onConfirm={() => {
+                  if (quota_scope) {
+                    commitUnsetQuotaScope({
+                      variables: {
+                        quota_scope_id: quota_scope.quota_scope_id,
+                        storage_host_name: quota_scope.storage_host_name,
+                      },
+                      onCompleted() {
+                        message.success(
+                          t(
+                            "storageHost.quotaSettings.QuotaScopeSuccessfullyUpdated"
+                          )
+                        );
+                      },
+                      onError(error) {
+                        message.error(error?.message);
+                      },
+                    });
+                  }
+                }}
+              >
+                <Button
+                  loading={isInFlightCommitUnsetQuotaScope}
+                  danger
+                  icon={<CloseOutlined />}
+                >
+                  {t("button.Unset")}
+                </Button>
+              </Popconfirm>
+            </Flex>
+          ),
+        },
+      ]}
+      dataSource={quota_scope ? [quota_scope] : []}
+      locale={{
+        emptyText: showAddButtonWhenEmpty
+          ? addQuotaConfigsWhenEmpty
+          : selectProjectOrUserFirst,
+      }}
+      pagination={false}
+    />
   );
 };
 
