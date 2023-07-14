24a753888b2eedc2263af70739f080ec04949fe9
Sanghun Lee <oper6909@gmail.com>
2023-07-14 15:04:39 +0900

    change fgpu query to cuda-shares

--- src/components/backend-ai-edu-applauncher.ts
+++ src/components/backend-ai-edu-applauncher.ts
@@ -73,7 +73,7 @@ export default class BackendAiEduApplauncher extends BackendAIPage {
     this.resources = {
       cpu: queryParams.get('cpu'),
       mem: queryParams.get('mem'),
-      fgpu: queryParams.get('fgpu')
+      'cuda.shares': queryParams.get('cuda.shares')
     };
     await this._initClient(apiEndpoint);
     const loginSuccess = await this._token_login();
