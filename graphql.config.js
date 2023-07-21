a497d901829dd3756120d5b0b59fc4e33976da4e
Jong Eun Lee <jongeun@lablup.com>
2023-07-21 11:17:18 +0800

    fix vscode graphql extention configuration

--- graphql.config.js
+++ graphql.config.js
@@ -1,11 +1,8 @@
-// To get full GraphQL syntax highlighting and language support, install the GraphQL extension for VSCode.
-// https://marketplace.visualstudio.com/items?itemName=GraphQL.vscode-graphql-syntax
 module.exports = {
   schema: [
     // To update graphql schema, run `./backend.ai mgr gql show` and copy the output to schema.graphql
     "./react/data/schema.graphql",
-    // "./relay-directives.graphql",
-    "./react/data/client-directives.graphql"
+    "./react/data/relay-directives.graphql",
   ],
-  documents: ["./src/**/*.{tsx,ts,jsx,js,graphql}","./react/**/*.{tsx,ts,jsx,js,graphql}"],
+  documents: ["./react/src/**/*.{graphql,ts,tsx}"],
 };
