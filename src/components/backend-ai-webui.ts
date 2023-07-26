20fe0d8ddf69e290c0d85d95b60014b4aa8aefc7
Jeongkyu Shin <inureyes@gmail.com>
2023-07-26 15:30:26 +0900

    fix: typo

--- src/components/backend-ai-webui.ts
+++ src/components/backend-ai-webui.ts
@@ -1243,8 +1243,8 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
         </div>
       </mwc-drawer>
       <div id="mini-tooltips" style="display:${this.mini_ui ? 'block' : 'none'};">
-        <vaddin-tooltip for="notification-icon" position="end" text="${_t('webui.menu.Notifications')}"></vaddin-tooltip>
-        <vaddin-tooltip for="task-icon" position="end" text="${_t('webui.menu.Tasks')}"></vaddin-tooltip>
+        <vaadin-tooltip for="notification-icon" position="end" text="${_t('webui.menu.Notifications')}"></vaadin-tooltip>
+        <vaadin-tooltip for="task-icon" position="end" text="${_t('webui.menu.Tasks')}"></vaadin-tooltip>
       </div>
       <backend-ai-offline-indicator ?active="${this._offlineIndicatorOpened}">
         ${this._offline ? _t('webui.YouAreOffline') : _t('webui.YouAreOnline')}.
