a6e092697afa4b23289196d38104a3b2458483c5
mnxmnz <minzidev@gmail.com>
2023-08-17 18:50:45 +0900

    chore: fix empty div noti text

--- src/components/backend-ai-app-launcher.ts
+++ src/components/backend-ai-app-launcher.ts
@@ -1290,7 +1290,7 @@ export default class BackendAiAppLauncher extends BackendAIPage {
       const textToCopy = divElement.textContent;
 
       if (textToCopy.length === 0) {
-        this.notification.text = _text('emptyDivMessage');
+        this.notification.text = _text('NoExistingConnectionExample');
         this.notification.show();
       } else {
         if (navigator.clipboard !== undefined) { // for Chrome, Safari
