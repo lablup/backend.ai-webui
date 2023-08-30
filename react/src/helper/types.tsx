00a26322b7cf233a4072022f37632d8e314015dd
Jihyun Kang <jihyunkang@lablup.com>
2023-08-30 16:16:56 +0900

    feature: add sorting filter in new column `created_at` in serving list (#1888)
    * add: type definitions for general use
    
    * add: updated graphql query for services and routing list with new column `created_at`
    
    * add: KO/EN translation for created at field in serving page
    
    * update: package-lock.json
    
    * update: package-lock.json
    
    * refactor: simply support sorting filter on serving list
    
    * refactor: temporally sort by latest created service in serving list
    
    * fix: comment out unused imported custom helper function
    
    * refactor: use dayjs instead of initialize Date instance and start sortDirection to descend as a default
    
    * fix: remove unused import function from helper component

---
+++ react/src/helper/types.tsx
@@ -0,0 +1,5 @@
+import { useLazyLoadQuery } from "react-relay";
+
+export type LazyLoadQueryOptions = ArgumentTypes<typeof useLazyLoadQuery>[2];
+
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
