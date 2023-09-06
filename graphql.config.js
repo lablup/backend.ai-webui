f8e2a10a07c66fc8b6789eb2d7284c8f1a6ba144
Jong Eun Lee <jongeun@lablup.com>
2023-09-06 19:18:48 +0800

    add DateTime to customScalars of relay.config.js

--- graphql.config.js
+++ graphql.config.js
@@ -2,7 +2,7 @@ module.exports = {
   schema: [
     // To update graphql schema, run `./backend.ai mgr gql show` and copy the output to schema.graphql
     "./react/data/schema.graphql",
-    // "./react/data/relay-and-local-directives-for-graphql-config.graphql"
+    "./react/data/relay-and-local-directives-for-graphql-config.graphql"
   ],
   documents: ["./react/src/**/*.{graphql,ts,tsx}"],
 };
