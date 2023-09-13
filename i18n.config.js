260cde27218553aadbf38a27cdfc8c1763125c6d
Jeongkyu Shin <inureyes@gmail.com>
2023-09-13 12:46:31 +0900

    update: prepare for automated locale matching

--- i18n.config.js
+++ i18n.config.js
@@ -10,13 +10,13 @@ module.exports = {
   output: './',
   options: {
     debug: true,
-    removeUnusedKeys: false,
+    removeUnusedKeys: true,
     func: {
       list: ['_t', '_tr', '_text', 't'], // _t for normal text, _tr for HTML text, t for react
       extensions: ['.ts', '.js', '.jsx', '.tsx']
     },
     trans: false,
-    lngs: ['en', 'ko', 'ru', 'fr', 'ja', 'zh_CN', 'zh_TW', 'mn', 'id', 'pt'],
+    lngs: ['en', 'ko', 'de', 'el', 'es', 'fi', 'fr', 'id', 'it', 'ja', 'mn', 'ms', 'pl', 'pt', 'pt_BR', 'ru', 'tr', 'vi', 'zh_CN', 'zh_TW'], // supported languages
     defaultLng: 'en',
     defaultNs: 'resource',
     defaultValue: function(lng, ns, key) {
