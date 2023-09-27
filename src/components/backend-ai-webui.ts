0e5106002d85c313863692e855bb629c9a509475
Jeongkyu Shin <inureyes@gmail.com>
2023-09-27 21:26:36 +0900

    update: node.js packages (#1955)

--- src/components/backend-ai-webui.ts
+++ src/components/backend-ai-webui.ts
@@ -1758,7 +1758,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
               </div>
               <address class="full-menu">
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">23.09.0-beta.3.230922</small>
+                <small class="sidebar-footer" style="font-size:9px;">23.09.0.230927</small>
               </address>
               <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
                 <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
@@ -1796,7 +1796,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
             </div>
             <address class="full-menu">
               <small class="sidebar-footer">Lablup Inc.</small>
-              <small class="sidebar-footer" style="font-size:9px;">23.09.0-beta.3.230922</small>
+              <small class="sidebar-footer" style="font-size:9px;">23.09.0.230927</small>
             </address>
             <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
               <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
