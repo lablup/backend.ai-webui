df890a91332d3a3a258c521221207041894dd78a
Jong Eun Lee <jongeun@lablup.com>
2023-10-05 17:28:24 +0800

    handle mem&shem values as string with unit

--- react/src/components/ResourceAllocationFormItems.tsx
+++ react/src/components/ResourceAllocationFormItems.tsx
@@ -32,12 +32,12 @@ const ResourceAllocationFormItems = () => {
               resource: {
                 ...slots,
                 // transform to GB based on preset values
-                mem: iSizeToSize((slots?.mem || 0) + 'b', 'g', 2).number,
+                mem: iSizeToSize((slots?.mem || 0) + 'b', 'g', 2).numberUnit,
                 shmem: iSizeToSize(
                   (options?.preset?.shared_memory || 0) + 'b',
                   'g',
                   2,
-                ).number,
+                ).numberUnit,
               },
             });
           }}
