8c6923c8705c121a9cd3327900a0fdd512ca8f6f
agatha197 <kimsujin@lablup.com>
2023-11-30 10:00:24 +0900

    fix: typo

--- react/src/pages/ModelStoreListPage.tsx
+++ react/src/pages/ModelStoreListPage.tsx
@@ -59,7 +59,7 @@ const ModelStoreListPage: React.FC = () => {
             hasNextPage
             hasPreviousPage
           }
-          ...UserResourcePolicySettingModalFragment
+          ...ModelCardModalFragment
         }
       }
     `,
