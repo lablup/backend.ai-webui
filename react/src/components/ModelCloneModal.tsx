e2cbd6f11fa0b0e885be3d441d310a51e416f011
Jong Eun Lee <jongeun@lablup.com>
2023-12-01 11:28:41 +0800

    feat: clone model card as user type folder
    (cherry picked from commit 5c3f94a30481e0d514d69a1c16e71d91e8462109)

--- react/src/components/ModelCloneModal.tsx
+++ react/src/components/ModelCloneModal.tsx
@@ -4,20 +4,25 @@ import {
   useSuspendedBackendaiClient,
 } from '../hooks';
 import { useTanMutation, useTanQuery } from '../hooks/reactQueryAlias';
+import { usePainKiller } from '../hooks/usePainKiller';
 import BAIModal, { BAIModalProps } from './BAIModal';
-import ProjectSelector from './ProjectSelector';
+import Flex from './Flex';
 import StorageSelector from './StorageSelector';
-import { Form, Input, Select, Switch } from 'antd';
+import { Alert, Form, Input, Select, Switch, message } from 'antd';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 
-interface ModelCloneModalProps extends BAIModalProps {}
-const ModelCloneModal: React.FC<ModelCloneModalProps> = ({ ...props }) => {
+interface ModelCloneModalProps extends BAIModalProps {
+  sourceFolderName?: string;
+}
+const ModelCloneModal: React.FC<ModelCloneModalProps> = ({
+  sourceFolderName,
+  ...props
+}) => {
   const { t } = useTranslation();
-  const currentDomain = useCurrentDomainValue();
   const baiClient = useSuspendedBackendaiClient();
-  const currentProject = useCurrentProjectValue();
   const [form] = Form.useForm();
+  const painKiller = usePainKiller();
 
   const { data: allowed_vfolder_types } = useTanQuery({
     queryKey: ['modelCloneModal', 'vfolder_allowed_types'],
@@ -28,7 +33,7 @@ const ModelCloneModal: React.FC<ModelCloneModalProps> = ({ ...props }) => {
 
   const mutationToClone = useTanMutation({
     // @ts-ignore
-    mutationFn: (input: any, name: string) => {
+    mutationFn: ({ input, name }: { input: any; name: string }) => {
       return baiClient.vfolder.clone(input, name);
     },
   });
@@ -38,52 +43,78 @@ const ModelCloneModal: React.FC<ModelCloneModalProps> = ({ ...props }) => {
       destroyOnClose
       {...props}
       okText={t('button.Clone')}
+      confirmLoading={mutationToClone.isLoading}
       onOk={() => {
         form
           .validateFields()
           .then((values) => {
-            alert(JSON.stringify(values, null, 2));
+            mutationToClone.mutate(
+              {
+                input: values,
+                // TODO: 'testmodel' is a temporary name, please use the folder name of selected model info.
+                name: 'testmodel',
+                // name: sourceFolderName,
+              },
+              {
+                onSuccess(data) {
+                  message.info({
+                    content: t('modelStore.CloneSuccess'),
+                  });
+                  props.onOk?.(data);
+                },
+                onError(error: any) {
+                  // const title = painKiller.relieve(error?.title);
+                  const messageStr = painKiller.relieve(error?.message);
+                  message.error({
+                    content: messageStr,
+                  });
+                },
+              },
+            );
           })
           .catch(() => {});
       }}
     >
-      <Form
-        form={form}
-        layout="vertical"
-        requiredMark="optional"
-        initialValues={{
-          permission: 'rw',
-          project: currentProject.id,
-          type: 'user',
-        }}
-      >
-        <Form.Item
-          name="target_name"
-          label={t('data.Foldername')}
-          rules={[
-            {
-              required: true,
-            },
-            {
-              pattern: /^[a-zA-Z0-9._-]*$/,
-              message: t('data.Allowslettersnumbersand-_dot'),
-            },
-          ]}
+      <Flex direction="column" align="stretch" gap="sm">
+        <Alert showIcon type="info" message={t('modelStore.CloneInfo')} />
+        <Form
+          form={form}
+          layout="vertical"
+          requiredMark="optional"
+          initialValues={{
+            permission: 'rw',
+            // project: currentProject.id,
+            // type: 'user',
+            usage_mode: 'model',
+          }}
         >
-          <Input autoComplete="off" />
-        </Form.Item>
-        <Form.Item
-          name="target_host"
-          label={t('data.Host')}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <StorageSelector autoSelectDefault />
-        </Form.Item>
-        <Form.Item
+          <Form.Item
+            name="target_name"
+            label={t('data.Foldername')}
+            rules={[
+              {
+                required: true,
+              },
+              {
+                pattern: /^[a-zA-Z0-9._-]*$/,
+                message: t('data.Allowslettersnumbersand-_dot'),
+              },
+            ]}
+          >
+            <Input autoComplete="off" />
+          </Form.Item>
+          <Form.Item
+            name="target_host"
+            label={t('data.Host')}
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <StorageSelector autoSelectDefault />
+          </Form.Item>
+          {/* <Form.Item
           name="type"
           label={t('data.Type')}
           rules={[
@@ -120,7 +151,6 @@ const ModelCloneModal: React.FC<ModelCloneModalProps> = ({ ...props }) => {
                   },
                 ]}
               >
-                {/* TODO: refactor PrjectSelector */}
                 <ProjectSelector
                   domain={currentDomain}
                   onSelectProject={() => {}}
@@ -130,17 +160,17 @@ const ModelCloneModal: React.FC<ModelCloneModalProps> = ({ ...props }) => {
               </Form.Item>
             );
           }}
-        </Form.Item>
+        </Form.Item> */}
 
-        {/* _vfolderInnatePermissionSupport */}
-        {baiClient.isAPIVersionCompatibleWith('v4.20191215') && (
-          <Form.Item
-            // label={t('data.UsageMode')}
-            name={'usage_mode'}
-            hidden
-          >
-            <Input value={'model'} />
-            {/* <Select
+          {/* _vfolderInnatePermissionSupport */}
+          {baiClient.isAPIVersionCompatibleWith('v4.20191215') && (
+            <Form.Item
+              // label={t('data.UsageMode')}
+              name={'usage_mode'}
+              hidden
+            >
+              <Input />
+              {/* <Select
               options={[
                 {
                   label: 'General',
@@ -156,30 +186,31 @@ const ModelCloneModal: React.FC<ModelCloneModalProps> = ({ ...props }) => {
                   : []),
               ]}
             /> */}
+            </Form.Item>
+          )}
+          <Form.Item label={t('data.Permission')} name={'permission'}>
+            <Select
+              options={[
+                {
+                  label: 'Read-Write',
+                  value: 'rw',
+                },
+                {
+                  label: 'Read-Only',
+                  value: 'ro',
+                },
+                {
+                  label: 'Delete',
+                  value: 'wd',
+                },
+              ]}
+            />
           </Form.Item>
-        )}
-        <Form.Item label={t('data.Permission')} name={'permission'}>
-          <Select
-            options={[
-              {
-                label: 'Read-Write',
-                value: 'rw',
-              },
-              {
-                label: 'Read-Only',
-                value: 'ro',
-              },
-              {
-                label: 'Delete',
-                value: 'wd',
-              },
-            ]}
-          />
-        </Form.Item>
-        <Form.Item hidden name="cloneable" valuePropName="checked">
-          <Switch checked={false} />
-        </Form.Item>
-      </Form>
+          <Form.Item hidden name="cloneable" valuePropName="checked">
+            <Switch checked={false} />
+          </Form.Item>
+        </Form>
+      </Flex>
     </BAIModal>
   );
 };
