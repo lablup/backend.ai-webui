9244b1fe9e6d9362511fafa097ec69a7039f3d18
gee05053 <gee05053@gmail.com>
2023-12-07 11:30:48 +0900

    fix: apply prettier

--- cypress/helper/FindShadowRoot.cy.ts
+++ cypress/helper/FindShadowRoot.cy.ts
@@ -1,9 +1,9 @@
 export const findShadowRoot = (routes) => {
-  const segments = routes.split(">").map((route) => route.trim());
+  const segments = routes.split('>').map((route) => route.trim());
   let element = cy.get(segments[0]).shadow();
-  for (const [idx,segment] of segments.entries()) {
+  for (const [idx, segment] of segments.entries()) {
     if (idx === 0) {
-      continue
+      continue;
     } else {
       element = element.find(segment).shadow();
     }
