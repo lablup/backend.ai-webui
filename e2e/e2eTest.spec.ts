cd906f34dccbd603ca6a29e8b18aea0cb83af5bb
gee05053 <gee05053@gmail.com>
2023-12-18 15:29:50 +0900

    fix:add login with endpoint

--- e2e/e2eTest.spec.ts
+++ e2e/e2eTest.spec.ts
@@ -5,7 +5,7 @@ import { createSession, deleteSession } from '../tests-examples/SessionStoragePa
 
 test('E2E test', async ({ page }) => {
   await page.goto('http://localhost:9081/');
-  await login(page, 'test@lablup.com', 'test123!');
+  await login(page, 'test@lablup.com', 'test123!', 'http://127.0.0.1:8090');
   await createVfolder(page, 'testvfolder')
   await createSession(page, 'Ubuntu 20.04 aarch64', 'testvfolder', 'testSession');
   await deleteSession(page, 'test@lablup.com', 'testSession')
--- tests-examples/login.spec.ts
+++ tests-examples/login.spec.ts
@@ -1,7 +1,9 @@
-export async function login(page: Page, email: string, password: string) {
+export async function login(page: Page, email: string, password: string, endpoint: string) {
   await page.locator('#id_user_id label').click();
   await page.locator('#id_user_id label').fill(email);
   await page.locator('#id_password label').click();
   await page.locator('#id_password label').fill(password);
+  await page.locator('#id_api_endpoint label').click();
+  await page.locator('#id_api_endpoint label').fill(endpoint);
   await page.locator('#login-button').click();
 }
