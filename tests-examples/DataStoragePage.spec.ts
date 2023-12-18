e063020a929d9eaba42994e6f724df64bc824eee
gee05053 <gee05053@gmail.com>
2023-12-18 15:17:29 +0900

    fix:modify vfolder name to use params

--- tests-examples/DataStoragePage.spec.ts
+++ tests-examples/DataStoragePage.spec.ts
@@ -4,7 +4,7 @@ export async function createVfolder(page: Page, vfolderName: string) {
   await page.locator('#data').click();
   await page.getByRole('button', { name: 'add' }).click();
   await page.locator('#add-folder-name label').click();
-  await page.getByRole('textbox', { name: 'Folder name*' }).fill('testvfolder');
+  await page.getByRole('textbox', { name: 'Folder name*' }).fill(vfolderName);
   await page.getByRole('button', { name: 'Create', exact: true }).click();
-  await expect(page.getByText('testvfolder')).toBeVisible();
+  await expect(page.getByText(vfolderName)).toBeVisible();
 }
