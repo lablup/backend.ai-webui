eb79b5d5e26bee82190bd14c0bb83e753fa49641
Kyujin Cho <kyujin.cho@lablup.com>
2024-01-11 13:05:23 +0900

    fix: model service owner always represented as logged in user (#2138)
    * fix: model service owner always represented as logged in user
    
    * add missing memo dependencies
    
    * update i18n
    
    * reflect review
    
    * reflect review
    
    * remove unused gql fields
    
    ---------
    
    Co-authored-by: Jong Eun Lee <jongeun@lablup.com>

---
+++ react/src/components/EndpointOwnerInfo.tsx
@@ -0,0 +1,56 @@
+import { useSuspendedBackendaiClient } from '../hooks';
+import { EndpointOwnerInfoFragment$key } from './__generated__/EndpointOwnerInfoFragment.graphql';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import { Button, Tooltip, theme } from 'antd';
+import graphql from 'babel-plugin-relay/macro';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useFragment } from 'react-relay';
+
+interface EndpointOwnerInfoProps {
+  endpointFrgmt: EndpointOwnerInfoFragment$key | null;
+}
+const EndpointOwnerInfo: React.FC<EndpointOwnerInfoProps> = ({
+  endpointFrgmt,
+}) => {
+  const { t } = useTranslation();
+  const { token } = theme.useToken();
+  const baiClient = useSuspendedBackendaiClient();
+
+  const endpoint = useFragment(
+    graphql`
+      fragment EndpointOwnerInfoFragment on Endpoint {
+        id
+        created_user_email @since(version: "23.09.8")
+        session_owner_email @since(version: "23.09.8")
+      }
+    `,
+    endpointFrgmt,
+  );
+
+  if (!baiClient.supports('model-serving-endpoint-user-info'))
+    return baiClient.email || '';
+  if (endpoint?.created_user_email === endpoint?.session_owner_email)
+    return endpoint?.session_owner_email || '';
+  else
+    return (
+      <>
+        {endpoint?.session_owner_email || ''}
+        <Tooltip
+          title={t('modelService.ServiceDelegatedFrom', {
+            createdUser: endpoint?.created_user_email || '',
+            sessionOwner: endpoint?.session_owner_email || '',
+          })}
+        >
+          <Button
+            size="small"
+            type="text"
+            icon={<QuestionCircleOutlined />}
+            style={{ color: token.colorTextSecondary }}
+          />
+        </Tooltip>
+      </>
+    );
+};
+
+export default EndpointOwnerInfo;
