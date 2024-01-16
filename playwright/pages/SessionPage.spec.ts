2319d1d4d1615c11eff64bd2a84fef6be448bb9d
gee05053 <gee05053@gmail.com>
2024-01-16 19:47:36 +0900

    fix:modify SessionPage code structure

--- playwright/pages/SessionPage.spec.ts
+++ playwright/pages/SessionPage.spec.ts
@@ -1,67 +1,101 @@
-import { expect } from '@playwright/test';
+import { generateRandomString } from '../helper/helper';
+import { test, expect } from '@playwright/test';
 
-export async function createSession(
-  page: Page,
-  environmnet: string,
-  mountVfolderName: string,
-  sessionName: string,
-) {
-  await page.locator('#session').click();
-  await page.waitForURL('**/job');
-  await page.getByRole('tab', { name: 'Running' }).click();
-  await page
-    .locator('backend-ai-session-view')
-    .locator('#launch-session')
-    .click();
-  await page.locator('backend-ai-session-view').locator('#version').click();
-  await page
-    .locator('backend-ai-session-view')
-    .getByRole('option', { name: environmnet })
-    .click();
-  await page
-    .locator('backend-ai-session-view')
-    .locator('#session-name')
-    .click();
-  await page
-    .locator('backend-ai-session-view')
-    .locator('#session-name label')
-    .fill(sessionName);
-  await page.locator('backend-ai-session-view').locator('#next-button').click();
-  await page.getByText(`${mountVfolderName} local:volume1`).click();
-  await page.locator('backend-ai-session-view').locator('#next-button').click();
-  await page.locator('backend-ai-session-view').locator('#next-button').click();
-  await page
-    .locator('backend-ai-session-view')
-    .locator('#launch-button')
-    .click();
-  await page.waitForTimeout(5000);
-  await page.getByRole('button', { name: 'close' }).click();
-  await page.waitForTimeout(2000);
-  await expect(page.getByText(sessionName)).toBeVisible();
-}
+test.describe('Create Session', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:9081');
+    await page.locator('#id_user_id label').click();
+    await page.locator('#id_user_id label').fill('test@lablup.com');
+    await page.locator('#id_password label').click();
+    await page.locator('#id_password label').fill('test123!');
+    await page.locator('#id_api_endpoint label').click();
+    await page.locator('#id_api_endpoint label').fill('http://localhost:8090');
+    await page.locator('#login-button').click();
+    await page.locator('#session').click();
+    await page.waitForURL('**/job');
+    await page.waitForTimeout(1000);
+  });
+  test('User can create session', async ({ page }) => {
+    const version = 'Ubuntu 20.04 aarch64';
+    const randomSessionName = generateRandomString(
+      4,
+      '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    );
+    const mountVfolderName = 'test';
+    await page
+      .locator('backend-ai-session-view')
+      .locator('#launch-session')
+      .click();
+    await page.locator('backend-ai-session-view').locator('#version').click();
+    await page
+      .locator('backend-ai-session-view')
+      .getByRole('option', { name: version })
+      .click();
+    await page
+      .locator('backend-ai-session-view')
+      .locator('#session-name')
+      .click();
+    await page
+      .locator('backend-ai-session-view')
+      .locator('#session-name label')
+      .fill(randomSessionName);
+    await page
+      .locator('backend-ai-session-view')
+      .locator('#next-button')
+      .click();
+    await page.getByText(`${mountVfolderName} local:volume1`).click();
+    await page
+      .locator('backend-ai-session-view')
+      .locator('#next-button')
+      .click();
+    await page
+      .locator('backend-ai-session-view')
+      .locator('#next-button')
+      .click();
+    await page
+      .locator('backend-ai-session-view')
+      .locator('#launch-button')
+      .click();
+    await page.waitForTimeout(5000);
+    await page.getByRole('button', { name: 'close' }).click();
+    await page.waitForTimeout(2000);
+    expect(await page.getByText(randomSessionName).isVisible()).toBe(true);
+  });
+});
 
-export async function deleteSession(
-  page: Page,
-  email: string,
-  sessionName: string,
-) {
-  await page.locator('#session').click();
-  await page.waitForURL('**/job');
-  await page.getByRole('tab', { name: 'Running' }).click();
-  await page
-    .locator(`#${sessionName}-power`)
-    .getByLabel('power_settings_new')
-    .click();
-  await page
-    .locator(`#terminate-session-dialog`)
-    .locator('mwc-button[class="ok"]')
-    .click();
-  await page.waitForTimeout(5000);
-  await expect(page.getByText(sessionName)).toBeHidden();
-  await page.getByRole('tab', { name: 'Finished' }).click();
-  await expect(
-    page
-      .getByRole('row', { name: `1 ${email} ${sessionName}` })
-      .locator('#vaadin-grid-cell-36'),
-  ).toBeVisible();
-}
+test.describe('Delete Session', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:9081');
+    await page.locator('#id_user_id label').click();
+    await page.locator('#id_user_id label').fill('test@lablup.com');
+    await page.locator('#id_password label').click();
+    await page.locator('#id_password label').fill('test123!');
+    await page.locator('#id_api_endpoint label').click();
+    await page.locator('#id_api_endpoint label').fill('http://localhost:8090');
+    await page.locator('#login-button').click();
+    await page.locator('#session').click();
+    await page.waitForURL('**/job');
+    await page.waitForTimeout(1000);
+  });
+  test('User can delete session', async ({ page }) => {
+    const sessionName = 'ssss';
+    const sessionOwner = 'test@lablup.com';
+    await page
+      .locator(`[id="${sessionName}-power"]`)
+      .getByLabel('power_settings_new')
+      .click();
+    await page
+      .locator(`#terminate-session-dialog`)
+      .locator('mwc-button[class="ok"]')
+      .click();
+    await page.waitForTimeout(5000);
+    expect(await page.getByText(sessionName).isHidden()).toBe(true);
+    await page.getByRole('tab', { name: 'Finished' }).click();
+    await page.waitForTimeout(1000);
+    expect(
+      await page
+        .getByRole('row', { name: `1 ${sessionOwner} ${sessionName}` })
+        .isVisible(),
+    ).toBe(true);
+  });
+});
