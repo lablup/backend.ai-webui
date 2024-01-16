c2aeffd85bfca55cdfc35e2b1aee1c8dfb0d2619
gee05053 <gee05053@gmail.com>
2024-01-16 17:25:00 +0900

    fix:modify code structure and add helper

--- playwright/pages/DataStoragePage.spec.ts
+++ playwright/pages/DataStoragePage.spec.ts
@@ -1,28 +1,79 @@
-import { expect } from '@playwright/test';
+import { generateRandomString } from '../helper/helper';
+import { test, expect } from '@playwright/test';
 
-export async function createVfolder(page: Page, vfolderName: string) {
-  await page.locator('#data').click();
-  await page.waitForURL('**/data');
-  await page.locator('#add-folder').click();
-  await page.locator('#add-folder-name').click();
-  await page.locator('#add-folder-name label').fill(vfolderName);
-  await page.locator('#add-button').click();
-  await expect(page.getByText(vfolderName)).toBeVisible();
-}
+test.describe('Vfolder creation', () => {
+  const randomVfolderName = generateRandomString();
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:9081');
+    await page.locator('#id_user_id label').click();
+    await page.locator('#id_user_id label').fill('test@lablup.com');
+    await page.locator('#id_password label').click();
+    await page.locator('#id_password label').fill('test123!');
+    await page.locator('#id_api_endpoint label').click();
+    await page.locator('#id_api_endpoint label').fill('http://localhost:8090');
+    await page.locator('#login-button').click();
+    await page.locator('#data').click();
+    await page.waitForURL('**/data');
+  });
+  test('User can create vfolder', async ({ page }) => {
+    await page.locator('#add-folder').click();
+    await page.locator('#add-folder-name').click();
+    await page.locator('#add-folder-name label').fill(randomVfolderName);
+    await page.locator('#add-button').click();
+    await page.waitForTimeout(5000);
+    await page
+      .getByRole('treegrid')
+      .evaluate(async (e) => (e.scrollTop = e.scrollHeight));
+    await page.waitForTimeout(1000);
+    expect(
+      await page
+        .getByText(`folder_open ${randomVfolderName}`, { exact: true })
+        .isVisible(),
+    ).toBe(true);
+    expect(
+      await page
+        .locator(
+          `lablup-shields[folder-name=${randomVfolderName}][description="ready"]`,
+        )
+        .isVisible(),
+    ).toBe(true);
+  });
+});
 
-export async function deleteVfolder(page: Page, vfolderName: string) {
-  await page.locator('#data').click();
-  await page.waitForURL('**/data');
-  await page
-    .locator(`#controls[folder-name=${vfolderName}]`)
-    .locator('mwc-icon-button[icon="delete"]')
-    .click();
-  await page.locator('#delete-folder-name').click();
-  await page.locator('#delete-folder-name label').fill(vfolderName);
-  await page.locator('#delete-button').click();
-  await expect(
-    page.locator(
-      `lablup-shields[folder-name=${vfolderName}][description="deleted-complete"]`,
-    ),
-  ).toBeVisible();
-}
+test.describe('Vfolder deletion', () => {
+  const deleteVfolderName =
+    'dBvGpQPVo4V6ArAM3qtwnERwV5W5iL3WKZOJKA3rXX0rm0RgC10r1bA14AQoWC9F';
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:9081');
+    await page.locator('#id_user_id label').click();
+    await page.locator('#id_user_id label').fill('test@lablup.com');
+    await page.locator('#id_password label').click();
+    await page.locator('#id_password label').fill('test123!');
+    await page.locator('#id_api_endpoint label').click();
+    await page.locator('#id_api_endpoint label').fill('http://localhost:8090');
+    await page.locator('#login-button').click();
+    await page.locator('#data').click();
+    await page.waitForURL('**/data');
+  });
+  test('User can delete vfolder', async ({ page }) => {
+    await page
+      .getByRole('treegrid')
+      .evaluate(async (e) => (e.scrollTop = e.scrollHeight));
+    await page.waitForTimeout(1000);
+    await page
+      .locator(`#controls[folder-name=${deleteVfolderName}]`)
+      .locator('mwc-icon-button[icon="delete"]')
+      .click();
+    await page.locator('#delete-folder-name').click();
+    await page.locator('#delete-folder-name label').fill(deleteVfolderName);
+    await page.locator('#delete-button').click();
+    await page.waitForTimeout(5000);
+    expect(
+      await page
+        .locator(
+          `lablup-shields[folder-name=${deleteVfolderName}][description="deleted-complete"]`,
+        )
+        .isVisible(),
+    ).toBe(true);
+  });
+});
