62781c7141c1856a5dae22d9a2220cd763538adf
agatha197 <kimsujin@lablup.com>
2024-01-18 11:14:02 +0900

    feat: Link to the webui docs if user clicks the help icon

--- react/src/components/WEBUIHelpButton.tsx
+++ react/src/components/WEBUIHelpButton.tsx
@@ -1,16 +1,36 @@
-import BAIHelpDrawer from './BAIHelpDrawer';
 import { useCurrentLanguage } from './DefaultProviders';
 import { QuestionCircleOutlined } from '@ant-design/icons';
-import { useToggle } from 'ahooks';
 import { Button, ButtonProps } from 'antd';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 interface WEBUIHelpButtonProps extends ButtonProps {}
 const WEBUIHelpButton: React.FC<WEBUIHelpButtonProps> = ({ ...props }) => {
-  const [isOpenDrawer, { toggle: toggleDrawer }] = useToggle();
   const [lang] = useCurrentLanguage();
   const location = useLocation();
+  const manualURL = `https://webui.docs.backend.ai/${lang}/latest/`;
+  const matchingKey = location.pathname.split('/')[1] || '';
+  const URLMatchingTable = {
+    '': 'summary/summary.html',
+    summary: 'summary/summary.html',
+    job: 'sessions_all/sessions_all.html',
+    serving: 'model_serving/model_serving.html',
+    import: 'import_run/import_run.html',
+    data: 'vfolder/vfolder.html',
+    statistics: 'statistics/statistics.html',
+    credential: 'admin_menu/admin_menu.html',
+    environment: 'admin_menu/admin_menu.html#manage-images',
+    agent: 'admin_menu/admin_menu.html#query-agent-nodes',
+    settings: 'admin_menu/admin_menu.html#system-settings',
+    maintenance: 'admin_menu/admin_menu.html#server-management',
+    information: 'admin_menu/admin_menu.html#detailed-information',
+    usersettings: 'user_settings/user_settings.html',
+  };
+  const URL =
+    manualURL +
+    (matchingKey
+      ? URLMatchingTable[matchingKey as keyof typeof URLMatchingTable] || ''
+      : '');
 
   return (
     <>
@@ -18,30 +38,8 @@ const WEBUIHelpButton: React.FC<WEBUIHelpButtonProps> = ({ ...props }) => {
         size="large"
         icon={<QuestionCircleOutlined />}
         type="text"
-        onClick={toggleDrawer}
+        href={URL}
         {...props}
-      />
-      <BAIHelpDrawer
-        open={isOpenDrawer}
-        onClose={toggleDrawer}
-        manualURL={`https://webui.docs.backend.ai/${lang}/latest/`}
-        URLMatchingTable={{
-          '': 'summary/summary.html',
-          summary: 'summary/summary.html',
-          job: 'sessions_all/sessions_all.html',
-          serving: 'model_serving/model_serving.html',
-          import: 'import_run/import_run.html',
-          data: 'vfolder/vfolder.html',
-          statistics: 'statistics/statistics.html',
-          credential: 'admin_menu/admin_menu.html',
-          environment: 'admin_menu/admin_menu.html#manage-images',
-          agent: 'admin_menu/admin_menu.html#query-agent-nodes',
-          settings: 'admin_menu/admin_menu.html#system-settings',
-          mainetenance: 'admin_menu/admin_menu.html#server-management',
-          information: 'admin_menu/admin_menu.html#detailed-information',
-          usersettings: 'user_settings/user_settings.html',
-        }}
-        matchingKey={location.pathname.split('/')[1] || ''}
       />
     </>
   );
