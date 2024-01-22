32fefb0abaf1ff8d96e1cb8d398731ed19eba696
gee05053 <gee05053@gmail.com>
2024-01-22 17:29:41 +0900

    fix:apply dotenv and waitForResponse

--- playwright/pages/LoginPage.spec.ts
+++ playwright/pages/LoginPage.spec.ts
@@ -1,4 +1,9 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
 test.describe('Login user', () => {
   test.beforeEach(async ({ page }) => {
@@ -6,23 +11,40 @@ test.describe('Login user', () => {
   });
   test('User login success', async ({ page }) => {
     await page.locator('#id_user_id label').click();
-    await page.locator('#id_user_id label').fill('test@lablup.com');
+    await page.locator('#id_user_id label').fill(process.env.SUPER_ADMIN_EMAIL);
     await page.locator('#id_password label').click();
-    await page.locator('#id_password label').fill('test123!');
+    await page
+      .locator('#id_password label')
+      .fill(process.env.SUPER_ADMIN_PASSWORD);
     await page.locator('#id_api_endpoint label').click();
-    await page.locator('#id_api_endpoint label').fill('http://localhost:8090');
+    await page.locator('#id_api_endpoint label').fill(process.env.ENDPOINT);
     await page.locator('#login-button').click();
+    await page.waitForResponse(async (response) => {
+      const responseJSONData = await response.json();
+      return (
+        response.url() === 'http://localhost:8090/server/login' &&
+        response.status() === 200 &&
+        responseJSONData?.authenticated
+      );
+    });
     await expect(page.locator('#app-body')).toBeVisible();
   });
   test('User login fail', async ({ page }) => {
     await page.locator('#id_user_id label').click();
-    await page.locator('#id_user_id label').fill('test11@lablup.com');
+    await page.locator('#id_user_id label').fill('test10@lablup.com');
     await page.locator('#id_password label').click();
     await page.locator('#id_password label').fill('test123!');
     await page.locator('#id_api_endpoint label').click();
-    await page.locator('#id_api_endpoint label').fill('http://localhost:8090');
+    await page.locator('#id_api_endpoint label').fill(process.env.ENDPOINT);
     await page.locator('#login-button').click();
-    await page.waitForTimeout(1000);
+    await page.waitForResponse(async (response) => {
+      const responseJSONData = await response.json();
+      return (
+        response.url() === 'http://localhost:8090/server/login' &&
+        response.status() === 200 && //todo: Need to change status code after resolving this issue at core.
+        responseJSONData?.data?.details === 'User credential mismatch.'
+      );
+    });
     await expect(page.locator('#app-body')).toBeHidden();
   });
 });
