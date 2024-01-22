6448fa4b9c1938314af0f25d3e0c384f4e391cae
gee05053 <gee05053@gmail.com>
2024-01-22 21:21:30 +0900

    fix:apply dotenv and data-testid at CredentialPage

--- playwright/pages/CredentialPage.spec.ts
+++ playwright/pages/CredentialPage.spec.ts
@@ -1,24 +1,34 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
 test.describe('Create account', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:9081');
     await page.locator('#id_user_id label').click();
-    await page.locator('#id_user_id label').fill('test@lablup.com');
+    await page
+      .locator('#id_user_id label')
+      .fill(process.env.SUPER_ADMIN_EMAIL as string);
     await page.locator('#id_password label').click();
-    await page.locator('#id_password label').fill('test123!');
+    await page
+      .locator('#id_password label')
+      .fill(process.env.SUPER_ADMIN_PASSWORD as string);
     await page.locator('#id_api_endpoint label').click();
-    await page.locator('#id_api_endpoint label').fill('http://localhost:8090');
+    await page
+      .locator('#id_api_endpoint label')
+      .fill(process.env.ENDPOINT as string);
     await page.locator('#login-button').click();
-    await page.locator('#credential').click();
+    await page.getByTestId('credential').click();
     await page.waitForURL('**/credential');
-    await page.getByRole('tab', { name: 'Users' }).click();
-    await page.getByRole('tab', { name: 'Active', exact: true }).click();
-    await page.waitForTimeout(1000);
+    await page.locator('mwc-tab[title="user-lists"]').click();
+    await page.locator('mwc-tab[title="active-user-list"]').click();
   });
   test('Admin can create account', async ({ page }) => {
-    const email = 'test4@lablup.com'; //Write new account'email
-    const userName = 'test4'; //Write new account'userName
+    const email = 'test6@lablup.com'; //Write new account'email
+    const userName = 'test6'; //Write new account'userName
     const password = 'test123!'; //Write new account'password
     await page.locator('#add-user').click();
     await page.locator('#new-user-dialog #id_user_email').click();
@@ -34,15 +44,13 @@ test.describe('Create account', () => {
       .locator('#new-user-dialog #id_user_confirm label')
       .fill(password);
     await page.locator('#new-user-dialog #create-user-button').click();
-    await page.waitForTimeout(5000);
     await expect(
       page
         .locator('#active-user-list vaadin-grid-cell-content')
         .filter({ hasText: email }),
     ).toBeVisible();
-    await page.getByRole('tab', { name: 'Credentials' }).click();
-    await page.getByRole('tab', { name: 'Active', exact: true }).click();
-    await page.waitForTimeout(1000);
+    await page.locator('mwc-tab[title="credential-lists"]').click();
+    await page.locator('mwc-tab[title="active-credential-list"]').click();
     await expect(
       page
         .locator('#active-credential-list vaadin-grid-cell-content')
@@ -55,43 +63,48 @@ test.describe('Delete account', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:9081');
     await page.locator('#id_user_id label').click();
-    await page.locator('#id_user_id label').fill('test@lablup.com');
+    await page
+      .locator('#id_user_id label')
+      .fill(process.env.SUPER_ADMIN_EMAIL as string);
     await page.locator('#id_password label').click();
-    await page.locator('#id_password label').fill('test123!');
+    await page
+      .locator('#id_password label')
+      .fill(process.env.SUPER_ADMIN_PASSWORD as string);
     await page.locator('#id_api_endpoint label').click();
-    await page.locator('#id_api_endpoint label').fill('http://localhost:8090');
+    await page
+      .locator('#id_api_endpoint label')
+      .fill(process.env.ENDPOINT as string);
     await page.locator('#login-button').click();
-    await page.locator('#credential').click();
+    await page.getByTestId('credential').click();
     await page.waitForURL('**/credential');
-    await page.getByRole('tab', { name: 'Users' }).click();
-    await page.getByRole('tab', { name: 'Active', exact: true }).click();
-    await page.waitForTimeout(1000);
+    await page.locator('mwc-tab[title="user-lists"]').click();
+    await page.locator('mwc-tab[title="active-user-list"]').click();
   });
   test('Admin can delete account', async ({ page }) => {
-    const email = 'test4@lablup.com'; //Write account's email you want to delete
-    await page
-      .locator(`#controls[user-id="${email}"]`)
-      .locator('mwc-icon-button[icon="delete_forever"]')
-      .click();
-    await page.locator('#active-user-list #deleteOk').click();
-    await page.waitForTimeout(5000);
+    const email = 'test5@lablup.com'; //Write account's email you want to delete
+    await page.getByTestId(email).getByLabel('delete_forever').click();
+    await page.locator('#active-user-list').getByTestId('deleteUser').click();
+    await page.waitForResponse(
+      (response) =>
+        response.url() === 'http://localhost:8090/func/admin/gql' &&
+        response.body().then((b) => {
+          return b.includes('"delete_user": {"ok": true, "msg": "success"}');
+        }),
+    );
     await expect(
       page.getByRole('gridcell', { name: email, exact: true }),
     ).toBeHidden();
-    await page.getByRole('tab', { name: 'Inactive' }).click();
-    await page.waitForTimeout(1000);
+    await page.locator('mwc-tab[title="inactive-user-list"]').click();
     await expect(
       page.getByRole('gridcell', { name: email, exact: true }),
     ).toBeVisible();
-    await page.getByRole('tab', { name: 'Credentials' }).click();
-    await page.waitForTimeout(1000);
+    await page.locator('mwc-tab[title="credential-lists"]').click();
     await expect(
       page
         .locator('#active-credential-list vaadin-grid-cell-content')
         .filter({ hasText: email }),
     ).toBeHidden();
-    await page.getByRole('tab', { name: 'Inactive' }).click();
-    await page.waitForTimeout(1000);
+    await page.locator('mwc-tab[title="inactive-credential-list"]').click();
     await expect(
       page
         .locator('#inactive-credential-list vaadin-grid-cell-content')
