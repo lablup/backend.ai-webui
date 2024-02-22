e962599e8bd5942e9a0365d764e8f0ae10fc06b0
gee05053 <gee05053@gmail.com>
2024-02-22 11:46:41 +0900

    fix: apply create_user GQL query

--- react/src/components/UserGenerationModal.tsx
+++ react/src/components/UserGenerationModal.tsx
@@ -1,9 +1,10 @@
-import { useSuspendedBackendaiClient } from '../hooks';
-import { useTanMutation, useTanQuery } from '../hooks/reactQueryAlias';
 import BAIModal, { BAIModalProps } from './BAIModal';
+import { UserGenerationModalMutation } from './__generated__/UserGenerationModalMutation.graphql';
 import { Form, Input, message } from 'antd';
+import graphql from 'babel-plugin-relay/macro';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useMutation } from 'react-relay';
 
 interface UserGenerationModalProps extends BAIModalProps {
   onRequestClose: () => void;
@@ -15,149 +16,153 @@ const UserGenerationModal: React.FC<UserGenerationModalProps> = ({
 }) => {
   const [form] = Form.useForm();
   const { t } = useTranslation();
-  const baiClient = useSuspendedBackendaiClient();
-  const [messageApi, contextHolder] = message.useMessage();
-  const { data: defaultGroupId } = useTanQuery({
-    queryKey: ['baiClient', 'group', 'list'],
-    queryFn: () =>
-      baiClient.group.list().then((res: any) => {
-        return res.groups.find((x: any) => x.name === 'default').id;
-      }),
-  });
-  const userCreationMutation = useTanMutation({
-    mutationFn: (values: { email: string; name: string; password: string }) => {
-      return baiClient.user.create(values.email, {
-        username: values.name,
-        password: values.password,
-        need_password_change: false,
-        full_name: values.name,
-        description: `${values.name ?? values.email}'s Account`,
-        is_active: true,
-        domain_name: 'default',
-        role: 'user',
-        group_ids: [defaultGroupId],
-      });
-    },
-  });
+
+  const [commitCreateUser, isInFlightCommitCreateUser] =
+    useMutation<UserGenerationModalMutation>(graphql`
+      mutation UserGenerationModalMutation(
+        $email: String!
+        $props: UserInput!
+      ) {
+        create_user(email: $email, props: $props) {
+          ok
+          msg
+          user {
+            id
+            email
+            username
+            need_password_change
+            full_name
+            description
+            status
+            domain_name
+            role
+          }
+        }
+      }
+    `);
+
   const onSubmit = () => {
     form
       .validateFields()
       .then((values) => {
-        console.log(values);
-        userCreationMutation.mutate(
-          {
-            email: values.email,
-            name: values.name ?? values.email,
-            password: values.password,
-          },
-          {
-            onSuccess: () => {
-              messageApi.open({
-                type: 'success',
-                content: t('credential.UserAccountCreated'),
-              });
-              onRequestClose();
-            },
-            onError: (e: any) => {
-              messageApi.open({
-                type: 'error',
-                content: e.message,
-              });
-              onRequestClose();
+        commitCreateUser({
+          variables: {
+            email: values?.email || '',
+            props: {
+              username: values.name ?? values.email,
+              password: values.password,
+              need_password_change: false,
+              domain_name: 'default',
             },
           },
-        );
+          onCompleted(res) {
+            if (res?.create_user?.ok) {
+              message.success(t('credential.UserAccountCreated'));
+            } else {
+              message.error(res?.create_user?.msg);
+            }
+            onRequestClose();
+          },
+          onError(err) {
+            message.error(err?.message);
+          },
+        });
       })
       .catch(() => {});
   };
+
   return (
-    <>
-      <BAIModal
-        {...baiModalProps}
-        title={t('credential.CreateUser')}
-        okText={t('button.Add')}
-        onOk={onSubmit}
-        onCancel={onRequestClose}
+    <BAIModal
+      {...baiModalProps}
+      destroyOnClose={true}
+      centered
+      title={t('credential.CreateUser')}
+      okText={t('button.Add')}
+      onOk={onSubmit}
+      onCancel={onRequestClose}
+      confirmLoading={isInFlightCommitCreateUser}
+    >
+      <Form
+        layout="vertical"
+        labelCol={{ span: 8 }}
+        form={form}
+        preserve={false}
       >
-        <Form
-          layout="vertical"
-          labelCol={{ span: 8 }}
-          form={form}
-          preserve={false}
+        <Form.Item
+          name="email"
+          label={t('general.E-Mail')}
+          required
+          rules={[
+            () => ({
+              validator(_, value) {
+                if (!value) {
+                  return Promise.reject(
+                    new Error(t('webui.menu.InvalidBlankEmail')),
+                  );
+                }
+                return Promise.resolve();
+              },
+            }),
+            {
+              type: 'email',
+              message: t('credential.validation.InvalidEmailAddress'),
+            },
+          ]}
         >
-          <Form.Item
-            name="email"
-            label={t('general.E-Mail')}
-            required
-            rules={[
-              () => ({
-                validator(_, value) {
-                  if (!value) {
-                    return Promise.reject(
-                      new Error(t('webui.menu.InvalidBlankEmail')),
-                    );
-                  }
-                  return Promise.resolve();
-                },
-              }),
-              {
-                type: 'email',
-                message: t('credential.validation.InvalidEmailAddress'),
+          <Input placeholder={t('maxLength.64chars')} maxLength={64} />
+        </Form.Item>
+        <Form.Item name="name" label={t('general.Username')}>
+          <Input placeholder={t('maxLength.64chars')} maxLength={64} />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          label={t('general.Password')}
+          required
+          rules={[
+            () => ({
+              validator(_, value) {
+                if (!value) {
+                  return Promise.reject(
+                    new Error(t('webui.menu.InvalidPasswordMessage')),
+                  );
+                }
+                return Promise.resolve();
               },
-            ]}
-          >
-            <Input placeholder={t('maxLength.64chars')} maxLength={64} />
-          </Form.Item>
-          <Form.Item name="name" label={t('general.Username')}>
-            <Input placeholder={t('maxLength.64chars')} maxLength={64} />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            label={t('general.Password')}
-            required
-            rules={[
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value) {
-                    return Promise.reject(
-                      new Error(t('webui.menu.InvalidPasswordMessage')),
-                    );
-                  }
-                  return Promise.resolve();
-                },
-              }),
-              {
-                pattern: /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[_\W]).{8,}$/,
-                message: t('webui.menu.InvalidPasswordMessage'),
+            }),
+            {
+              pattern: /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[_\W]).{8,}$/,
+              message: t('webui.menu.InvalidPasswordMessage'),
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          name="passwordConfirm"
+          label={t('general.ConfirmPassword')}
+          dependencies={['password']}
+          required
+          rules={[
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (getFieldValue('password') !== value) {
+                  return Promise.reject(
+                    new Error(t('webui.menu.NewPasswordMismatch')),
+                  );
+                } else if (!value) {
+                  return Promise.reject(
+                    new Error(t('webui.menu.InvalidPasswordMessage')),
+                  );
+                }
+                return Promise.resolve();
               },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item
-            name="passwordConfirm"
-            label={t('general.ConfirmPassword')}
-            dependencies={['password']}
-            required
-            rules={[
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (getFieldValue('password') !== value) {
-                    return Promise.reject(
-                      new Error(t('webui.menu.NewPasswordMismatch')),
-                    );
-                  }
-                  return Promise.resolve();
-                },
-              }),
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-        </Form>
-      </BAIModal>
-      {contextHolder}
-    </>
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+      </Form>
+    </BAIModal>
   );
 };
 
