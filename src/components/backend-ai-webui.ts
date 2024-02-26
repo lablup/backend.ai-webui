859ea3352b70ecf3526462d8ee0f7d9bf35e9e7e
Sujin Kim <kimsujin@lablup.com>
2024-02-26 12:06:13 +0900

    revert: version update
    Co-authored-by: Jong Eun Lee <jongeun@lablup.com>

--- src/components/backend-ai-webui.ts
+++ src/components/backend-ai-webui.ts
@@ -1898,7 +1898,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
             </div>
             <address class="full-menu">
               <small class="sidebar-footer">Lablup Inc.</small>
-              <small class="sidebar-footer" style="font-size:9px;">24.03.0-alpha.4.5927</small>
+              <small class="sidebar-footer" style="font-size:9px;">24.03.0-alpha.4.5862</small>
             </address>
             <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
               <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
