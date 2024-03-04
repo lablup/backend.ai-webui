9411071c6f34b7f117fbad745f627fdcaf349e71
Jeongseok Kang <jskang@lablup.com>
2024-03-04 14:45:13 +0900

    fix: Remove comments

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,5 @@ const plugins = [
   '@babel/plugin-syntax-dynamic-import',
   ['@babel/proposal-decorators', { decoratorsBeforeExport: true } ],
 ];
-// warning @babel/plugin-proposal-class-properties@7.18.6: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-class-properties instead.
-// [react-dependencies] npm WARN deprecated @babel/plugin-proposal-private-property-in-object@7.21.11: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-private-property-in-object instead.
 
 module.exports = {plugins};
