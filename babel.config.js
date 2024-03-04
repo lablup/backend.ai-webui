923e7e26b89682facc7702551c76a0e8dae10b5d
Jeongseok Kang <jskang@lablup.com>
2024-03-04 14:40:15 +0900

    deps: Update merged babel proposal plugins

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 const plugins = [
-  '@babel/plugin-proposal-class-properties',
+  '@babel/plugin-transform-class-properties',
   '@babel/plugin-syntax-dynamic-import',
   ['@babel/proposal-decorators', { decoratorsBeforeExport: true } ],
 ];
+// warning @babel/plugin-proposal-class-properties@7.18.6: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-class-properties instead.
+// [react-dependencies] npm WARN deprecated @babel/plugin-proposal-private-property-in-object@7.21.11: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-private-property-in-object instead.
 
 module.exports = {plugins};
