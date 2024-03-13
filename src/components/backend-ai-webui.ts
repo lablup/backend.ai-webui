d3d62edb6214dde2c4c637c9d2f6addc199b49a9
Jeongkyu Shin <inureyes@gmail.com>
2024-03-13 19:34:35 +0900

    fix: merge conflict

--- src/components/backend-ai-webui.ts
+++ src/components/backend-ai-webui.ts
@@ -1861,7 +1861,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
               </div>
               <address class="full-menu">
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">23.09.10-rc.3.5940</small>
+                <small class="sidebar-footer" style="font-size:9px;">23.09.10-rc.3.5952</small>
               </address>
               <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
                 <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
@@ -1903,11 +1903,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
             </div>
             <address class="full-menu">
               <small class="sidebar-footer">Lablup Inc.</small>
-<<<<<<< HEAD
-              <small class="sidebar-footer" style="font-size:9px;">23.09.10-rc.3.5940</small>
-=======
-              <small class="sidebar-footer" style="font-size:9px;">23.09.10-rc.3.5940</small>
->>>>>>> main
+              <small class="sidebar-footer" style="font-size:9px;">23.09.10-rc.3.5952</small>
             </address>
             <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
               <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
