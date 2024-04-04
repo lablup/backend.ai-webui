439048e101c7d6d8432364f1aaf8a5b2fc7c4628
Jong Eun Lee <jongeun@lablup.com>
2024-04-04 21:24:49 +0800

    introduce `ForceTOTPChecker` to open modal when `forceTotp`

---
+++ react/src/components/ForceTOTPChecker.tsx
@@ -0,0 +1,60 @@
+import { useSuspendedBackendaiClient, useUpdatableState } from '../hooks';
+import { useTOTPSupported } from '../hooks/backendai';
+import TOTPActivateModal from './TOTPActivateModal';
+import { ForceTOTPCheckerQuery } from './__generated__/ForceTOTPCheckerQuery.graphql';
+import { useToggle } from 'ahooks';
+import graphql from 'babel-plugin-relay/macro';
+import { useLazyLoadQuery } from 'react-relay';
+
+const ForceTOTPChecker = () => {
+  const [isOpenTOTPActivateModal, { toggle: toggleTOTPActivateModal }] =
+    useToggle(false);
+  const { isTOTPSupported } = useTOTPSupported();
+  const [fetchKey, updateFetchKey] = useUpdatableState('first');
+  const baiClient = useSuspendedBackendaiClient();
+
+  const { user } = useLazyLoadQuery<ForceTOTPCheckerQuery>(
+    graphql`
+      query ForceTOTPCheckerQuery($email: String, $isNotSupportTotp: Boolean!) {
+        user(email: $email) {
+          totp_activated @skipOnClient(if: $isNotSupportTotp)
+          ...TOTPActivateModalFragment
+        }
+      }
+    `,
+    {
+      email: baiClient?.email ?? '',
+      isNotSupportTotp: !isTOTPSupported,
+    },
+    {
+      fetchKey,
+      fetchPolicy: 'network-only',
+    },
+  );
+  const open =
+    baiClient?.supports('force2FA') &&
+    baiClient?._config.force2FA &&
+    user?.totp_activated === false;
+  return (
+    !!isTOTPSupported && (
+      <TOTPActivateModal
+        userFrgmt={user}
+        closable={false}
+        cancelButtonProps={{
+          style: { display: 'none' },
+        }}
+        open={open}
+        onRequestClose={(success) => {
+          if (success) {
+            updateFetchKey();
+          } else {
+            // formRef.current?.setFieldValue('totp_activated', false);
+          }
+          toggleTOTPActivateModal();
+        }}
+      />
+    )
+  );
+};
+
+export default ForceTOTPChecker;
