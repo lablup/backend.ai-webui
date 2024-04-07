9e64868747c72cf25cd28ba00195b94d59296455
Jeongkyu Shin <inureyes@gmail.com>
2024-04-07 21:15:12 +0900

    update: version tag

--- src/components/backend-ai-webui.ts
+++ src/components/backend-ai-webui.ts
@@ -1860,7 +1860,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
               </div>
               <address class="full-menu">
                 <small class="sidebar-footer">Lablup Inc.</small>
-                <small class="sidebar-footer" style="font-size:9px;">24.03.0.5964</small>
+                <small class="sidebar-footer" style="font-size:9px;">24.03.1.5965</small>
               </address>
               <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
                 <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
@@ -1902,7 +1902,7 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
             </div>
             <address class="full-menu">
               <small class="sidebar-footer">Lablup Inc.</small>
-              <small class="sidebar-footer" style="font-size:9px;">24.03.0.5964</small>
+              <small class="sidebar-footer" style="font-size:9px;">24.03.1.5965</small>
             </address>
             <div id="sidebar-navbar-footer" class="vertical start end-justified layout" style="margin-left:16px;">
               <backend-ai-help-button active style="margin-left:4px;"></backend-ai-help-button>
