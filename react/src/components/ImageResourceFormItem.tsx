27cc47b636d099c1b0497a499e27f2e463823ec3
Jong Eun Lee <jongeun@lablup.com>
2024-04-26 12:09:57 +0800

    refactor and change ui of `ImageResourceFormItem`

--- react/src/components/ImageResourceFormItem.tsx
+++ react/src/components/ImageResourceFormItem.tsx
@@ -1,14 +1,14 @@
 import { useResourceSlotsDetails } from '../hooks/backendai';
 import Flex from './Flex';
 import NonLinearSlider, { StepType } from './NonLinearSlider';
-import { Form, Input, theme } from 'antd';
+import { Divider, Form, Input, theme } from 'antd';
+import _ from 'lodash';
 import React from 'react';
 
 interface resourceInfoType {
   [key: string]: {
     label?: string;
     inputNumberProps?: any;
-    sliderProps?: any;
     steps: StepType[];
   };
 }
@@ -30,125 +30,105 @@ const ImageResourceFormItem: React.FC<ImageResourceFormItemProps> = ({
 
   const [resourceSlotsDetails] = useResourceSlotsDetails();
 
-  const resourceInfo: resourceInfoType = {
-    cpu: {
-      label: resourceSlotsDetails?.cpu.description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.cpu.display_unit,
-      },
-      steps: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
-    },
-    mem: {
-      label: resourceSlotsDetails?.mem.description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.mem.human_readable_name,
-      },
-      steps: [
-        '64m',
-        '128m',
-        '256m',
-        '512m',
-        '1g',
-        '2g',
-        '4g',
-        '8g',
-        '16g',
-        '32g',
-        '64g',
-        '128g',
-        '256g',
-        '512g',
-      ],
-    },
-    cuda_device: {
-      label: resourceSlotsDetails?.['cuda.device'].description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.['cuda.device'].display_unit,
-      },
-      steps: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
-    },
-    cuda_shares: {
-      label: resourceSlotsDetails?.['cuda.shares'].description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.['cuda.shares'].display_unit,
-      },
-      steps: ['0.1', '0.2', '0.5', '1.0', '2.0', '4.0', '8.0'],
-    },
-    rocm_devide: {
-      label: resourceSlotsDetails?.['rocm.device'].description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.['rocm.device'].display_unit,
-      },
-      steps: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
-    },
-    tpu_device: {
-      label: resourceSlotsDetails?.['tpu.device'].description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.['tpu.device'].display_unit,
-      },
-      steps: ['0', '1', '2', '3', '4'],
-    },
-    ipu_device: {
-      label: resourceSlotsDetails?.['ipu.device'].description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.['ipu.device'].display_unit,
-      },
-      steps: ['0', '1', '2', '3', '4'],
-    },
-    atom_device: {
-      label: resourceSlotsDetails?.atom.description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.atom.display_unit,
-      },
-      steps: ['0', '1', '2', '3', '4'],
-    },
-    warboy_device: {
-      label: resourceSlotsDetails?.warboy.description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.warboy.display_unit,
-      },
-      steps: ['0', '1', '2', '3', '4'],
-    },
-    hyperaccel_lpu_device: {
-      label: resourceSlotsDetails?.['hyperaccel-lpu'].description,
-      inputNumberProps: {
-        addonAfter: resourceSlotsDetails?.['hyperaccel-lpu'].display_unit,
-      },
-      steps: ['0', '1', '2', '3', '4'],
-    },
+  const getResourceInfo = (type: string) => {
+    return (
+      {
+        cpu: {
+          label: resourceSlotsDetails?.cpu.description,
+          steps: _.map(
+            ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
+            (value) => ({
+              value,
+              label: `${value} ${resourceSlotsDetails?.cpu.display_unit}`,
+            }),
+          ),
+        },
+        mem: {
+          label: resourceSlotsDetails?.mem.description,
+          steps: _.map(
+            [
+              '64m',
+              '128m',
+              '256m',
+              '512m',
+              '1g',
+              '2g',
+              '4g',
+              '8g',
+              '16g',
+              '32g',
+              '64g',
+              '128g',
+              '256g',
+              '512g',
+            ],
+            (v) => ({
+              value: v,
+              label: `${v.slice(0, -1)} ${_.toUpper(_.last(v))}iB`,
+            }),
+          ),
+        },
+        'cuda.device': {
+          label: resourceSlotsDetails?.['cuda.device'].description,
+          steps: _.map(['0', '1', '2', '3', '4', '5', '6', '7', '8'], (v) => ({
+            value: v,
+            label: `${v} ${resourceSlotsDetails?.['cuda.device'].display_unit}`,
+          })),
+        },
+        'cuda.shares': {
+          label: resourceSlotsDetails?.['cuda.shares'].description,
+          steps: _.map(
+            ['0', '0.1', '0.2', '0.5', '1.0', '2.0', '4.0', '8.0'],
+            (v) => ({
+              value: v,
+              label: `${v} ${resourceSlotsDetails?.['cuda.shares'].display_unit}`,
+            }),
+          ),
+        },
+        'rocm.device': {
+          label: resourceSlotsDetails?.['rocm.device'].description,
+          steps: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
+        },
+      }[type] || {
+        label: resourceSlotsDetails?.[type]?.description,
+        steps: _.map(['0', '1', '2', '3', '4'], (v) => ({
+          value: v,
+          label: `${v} ${resourceSlotsDetails?.[type]?.display_unit || ''}`,
+        })),
+      }
+    );
   };
+
+  const formInstance = Form.useFormInstance();
+  const value = Form.useWatch(name, formInstance);
+
+  const stepForValue = _.find(getResourceInfo(name).steps, (step) => {
+    // @ts-ignore
+    return step.value === value || step === value;
+  });
 
   return (
     <Form.Item
-      name={[name]}
-      label={resourceInfo[name]?.label ?? name.toUpperCase()}
-      initialValue={min}
-      style={{ marginBottom: token.marginXXS }}
+      label={getResourceInfo(name)?.label ?? name.toUpperCase()}
+      style={{ flex: 1 }}
     >
-      <Flex gap={token.paddingMD}>
-        <Form.Item
-          shouldUpdate={(prevValues, currentValues) =>
-            prevValues[name] !== currentValues[name]
-          }
-          noStyle
-        >
-          {({ getFieldValue, setFieldsValue }) => (
-            <>
-              <Input
-                style={{ flex: 1 }}
-                value={getFieldValue([name])}
-                onChange={(e) => setFieldsValue({ [name]: e.target.value })}
-                {...resourceInfo[name]?.inputNumberProps}
-              />
-              <NonLinearSlider
-                style={{ flex: 2 }}
-                steps={resourceInfo[name]?.steps}
-                value={getFieldValue([name])}
-                onChange={(value) => setFieldsValue({ [name]: value })}
-                {...resourceInfo[name]?.sliderProps}
-              />
-            </>
-          )}
+      <Flex direction="row" gap={'lg'}>
+        <Input
+          size="large"
+          variant="borderless"
+          readOnly
+          // @ts-ignore
+          value={stepForValue?.label ?? stepForValue}
+          style={{ flex: 1, fontSize: token.fontSizeLG }}
+          tabIndex={-1}
+        />
+        <Divider type="vertical" style={{ height: 40 }} />
+        <Form.Item name={name} noStyle initialValue={min}>
+          <NonLinearSlider
+            style={{ flex: 5 }}
+            steps={getResourceInfo(name)?.steps}
+            // onChange={(value) => {}}
+          />
         </Form.Item>
       </Flex>
     </Form.Item>
