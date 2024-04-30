883bf73eace4f4d96bdf3364513b4cd8a4e0eaeb
agatha197 <kimsujin@lablup.com>
2024-04-30 18:00:29 +0900

    New keypair resource policy page

---
+++ react/src/components/AllowedHostNamesSelect.tsx
@@ -0,0 +1,24 @@
+import { useAllowedHostNames } from '../hooks/backendai';
+import { Select, SelectProps } from 'antd';
+import _ from 'lodash';
+import React from 'react';
+
+interface AllowedHostNamesSelectProps extends SelectProps {}
+
+const AllowedHostNamesSelect: React.FC<AllowedHostNamesSelectProps> = ({
+  ...selectProps
+}) => {
+  const allowedHostNames = useAllowedHostNames();
+
+  return (
+    <Select {...selectProps}>
+      {_.map(allowedHostNames, (hostName) => (
+        <Select.Option key={hostName} value={hostName}>
+          {hostName}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+export default AllowedHostNamesSelect;
