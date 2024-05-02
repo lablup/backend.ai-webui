5d95c4951b98eabbf4eab176b191304f94aac837
Jong Eun Lee <jongeun@lablup.com>
2024-05-02 17:10:16 +0800

    refactor: ManageAppsModal's form items

--- react/src/components/ManageAppsModal.tsx
+++ react/src/components/ManageAppsModal.tsx
@@ -52,75 +52,77 @@ const ManageAppsModal: React.FC<BAIModalProps> = ({ ...baiModalProps }) => {
       }
     `);
 
-  const handleOnClick = async () => {
-    try {
-      await formRef.current?.validateFields();
+  const handleOnClick = () => {
+    formRef.current
+      ?.validateFields()
+      .then(() => {
+        const values = formRef.current
+          ?.getFieldValue('apps')
+          .map((item: { app: string; protocol: string; port: number }) => {
+            return `${item.app}:${item.protocol}:${item.port}`;
+          })
+          .join(',');
 
-      const values = formRef.current
-        ?.getFieldValue('apps')
-        .map((item: { app: string; protocol: string; port: number }) => {
-          return `${item.app}:${item.protocol}:${item.port}`;
-        })
-        .join(',');
-
-      const labels = _.map(
-        image.labels as { [key in string]: string },
-        (value, key) => {
-          if (key.includes('service-ports')) {
-            return { key: key, value: values };
-          } else {
-            return { key: key, value: value?.toString() || '' };
-          }
-        },
-      );
-
-      const commitRequest = () =>
-        commitModifyImageInput({
-          variables: {
-            target: `${image.registry}/${image.name}:${image.tag}`,
-            architecture: image.architecture,
-            props: {
-              labels: labels,
-              resource_limits: null,
-            },
-          },
-          onCompleted: (res, err) => {
-            if (err) {
-              message.error(t('dialog.ErrorOccurred'));
+        const labels = _.map(
+          image.labels as { [key in string]: string },
+          (value, key) => {
+            if (key.includes('service-ports')) {
+              return { key: key, value: values };
             } else {
-              message.success(t('environment.DescImagePortsModified'));
-              dispatchEvent('ok', null);
+              return { key: key, value: value?.toString() || '' };
             }
-            return;
           },
-          onError: (err) => {
-            message.error(t('dialog.ErrorOccurred'));
-          },
-        });
+        );
 
-      if (image.installed) {
-        app.modal.confirm({
-          title: 'Image reinstallation required',
-          content: (
-            <>
-              <Trans
-                i18nKey={
-                  'envrionment.ModifyImageResourceLimitReinstallRequired'
-                }
-              />
-            </>
-          ),
-          onOk: commitRequest,
-          getContainer: () => document.body,
-          closable: true,
-        });
-      } else {
-        commitRequest();
-      }
-    } catch (info: any) {
-      setValidateDetail(info.errorFields[0].errors[0]);
-      return;
-    }
+        const commitRequest = () =>
+          commitModifyImageInput({
+            variables: {
+              target: `${image.registry}/${image.name}:${image.tag}`,
+              architecture: image.architecture,
+              props: {
+                labels: labels,
+                resource_limits: null,
+              },
+            },
+            onCompleted: (res, err) => {
+              if (err) {
+                message.error(t('dialog.ErrorOccurred'));
+              } else {
+                message.success(t('environment.DescImagePortsModified'));
+                dispatchEvent('ok', null);
+              }
+              return;
+            },
+            onError: (err) => {
+              message.error(t('dialog.ErrorOccurred'));
+            },
+          });
+
+        if (image.installed) {
+          app.modal.confirm({
+            title: 'Image reinstallation required',
+            content: (
+              <>
+                <Trans
+                  i18nKey={
+                    'envrionment.ModifyImageResourceLimitReinstallRequired'
+                  }
+                />
+              </>
+            ),
+            onOk: commitRequest,
+            getContainer: () => document.body,
+            closable: true,
+          });
+        } else {
+          commitRequest();
+        }
+      })
+      .catch((e) => {
+        if (e?.errorFields?.length > 0) {
+          setValidateDetail(e.errorFields[0].errors[0]);
+        }
+      });
   };
 
   return (
@@ -157,105 +159,104 @@ const ManageAppsModal: React.FC<BAIModalProps> = ({ ...baiModalProps }) => {
             {(fields, { add, remove }) => (
               <Flex direction="column" style={{ width: '100%' }}>
                 {_.map(fields, (field, index) => (
-                  <Flex
-                    direction="row"
-                    key={field.key}
-                    gap={'xs'}
-                    align={index === 0 ? undefined : 'start'}
-                  >
-                    <Form.Item
-                      label={index === 0 && t('environment.AppName')}
-                      {...field}
-                      name={[field.name, 'app']}
-                      help=""
-                      rules={[
-                        {
-                          required: true,
-                          message: t('environment.AppNameMustNotBeEmpty'),
-                        },
-                      ]}
-                    >
-                      <Input />
-                    </Form.Item>
-                    <Form.Item
-                      label={index === 0 && t('environment.Protocol')}
-                      {...field}
-                      name={[field.name, 'protocol']}
-                      help=""
-                      rules={[
-                        {
-                          pattern: /^(http|tcp|pty|preopen)$/,
-                          required: true,
-                          message: t(
-                            'environment.ProtocolMustBeOneOfSupported',
-                          ),
-                        },
-                      ]}
-                    >
-                      <Input />
-                    </Form.Item>
-                    <Form.Item
-                      label={index === 0 && t('environment.Port')}
-                      {...field}
-                      name={[field.name, 'port']}
-                      help=""
-                      rules={[
-                        {
-                          required: true,
-                          validator: (_, value) => {
-                            if (
-                              isNaN(Number(value)) ||
-                              value < 0 ||
-                              value >= 65535
-                            ) {
-                              return Promise.reject(
-                                t('environment.PortMustBeInRange'),
-                              );
-                            }
-                            return Promise.resolve();
+                  <Form.Item>
+                    <Flex direction="row" key={field.key} gap={'xs'}>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'app']}
+                        noStyle
+                        rules={[
+                          {
+                            required: true,
+                            message: t('environment.AppNameMustNotBeEmpty'),
                           },
-                        },
-                        {
-                          validator: (_, value) => {
-                            const apps = formRef.current?.getFieldValue('apps');
-                            if (
-                              apps.some(
-                                (item: any, itemIndex: number) =>
-                                  itemIndex !== index && item?.port === value,
-                              )
-                            ) {
-                              return Promise.reject(
-                                t('environment.PortMustBeUnique'),
-                              );
-                            }
-                            if (
-                              [2000, 2001, 2002, 2003, 2200, 7681].includes(
-                                Number(value),
-                              )
-                            ) {
-                              return Promise.reject(
-                                t('environment.PortReservedForInternalUse'),
-                              );
-                            }
-                            return Promise.resolve();
+                        ]}
+                      >
+                        <Input placeholder={t('environment.AppName')} />
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'protocol']}
+                        noStyle
+                        rules={[
+                          {
+                            pattern: /^(http|tcp|pty|preopen)$/,
+                            required: true,
+                            message: t(
+                              'environment.ProtocolMustBeOneOfSupported',
+                            ),
                           },
-                        },
-                      ]}
-                    >
-                      <Input />
-                    </Form.Item>
-                    <Button
-                      type="text"
-                      danger
-                      onClick={() => remove(field.name)}
-                      style={
-                        index === 0
-                          ? { width: '10%', marginTop: 8 }
-                          : { width: '10%' }
-                      }
-                      icon={<DeleteOutlined />}
-                    />
-                  </Flex>
+                        ]}
+                      >
+                        <Input placeholder={t('environment.Protocol')} />
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, 'port']}
+                        noStyle
+                        rules={[
+                          {
+                            validator: (rules, rawValue) => {
+                              const value =
+                                _.isUndefined(rawValue) || rawValue === ''
+                                  ? NaN
+                                  : _.toNumber(rawValue);
+                              if (
+                                _.isNaN(value) ||
+                                value < 0 ||
+                                value >= 65535
+                              ) {
+                                return Promise.reject(
+                                  t('environment.PortMustBeInRange'),
+                                );
+                              } else if (
+                                [2000, 2001, 2002, 2003, 2200, 7681].includes(
+                                  Number(value),
+                                )
+                              ) {
+                                return Promise.reject(
+                                  t('environment.PortReservedForInternalUse'),
+                                );
+                              }
+                              return Promise.resolve();
+                            },
+                          },
+                          {
+                            validator: (rule, value) => {
+                              const apps =
+                                formRef.current?.getFieldValue('apps');
+                              if (
+                                _.isString(value) &&
+                                value.length > 0 &&
+                                apps.some(
+                                  (item: any, itemIndex: number) =>
+                                    itemIndex !== index && item?.port === value,
+                                )
+                              ) {
+                                return Promise.reject(
+                                  t('environment.PortMustBeUnique'),
+                                );
+                              }
+                              return Promise.resolve();
+                            },
+                          },
+                        ]}
+                      >
+                        <Input placeholder={t('environment.Port')} />
+                      </Form.Item>
+                      <Button
+                        type="text"
+                        danger
+                        onClick={() => remove(field.name)}
+                        style={
+                          index === 0
+                            ? { width: '10%', marginTop: 8 }
+                            : { width: '10%' }
+                        }
+                        icon={<DeleteOutlined />}
+                      />
+                    </Flex>
+                  </Form.Item>
                 ))}
                 <Button
                   type="dashed"
