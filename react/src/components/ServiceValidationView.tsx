1d3eee3bbe67331b51a0de2e470e8ecc759c01e2
Jihyun Kang <jihyunkang@lablup.com>
2024-05-24 12:41:23 +0900

    feature: provide model service validation (#2061)
    * add: draft for validation button in service launcher
    
    * add: translations for model service validation
    
    * add: validation status tag
    
    * refactor: split service info and validation info section in service launcher
    
    * add: suspense in validation tag
    
    * add: draft validation request
    
    * add: validation status tag with icon and translation
    
    * refactor: apply collapse opening condition only when validation button clicked
    
    * add: ansiup pkg for showing container log in model serving validation
    
    * refactor: add finished status in validationStatusTag
    
    * refactor: fetch container log and display in model serving validation modal
    
    * add: translation for model validation check
    
    * fix: temporally insert vfolderName which is required field on validation
    
    * refactor: replace message to app.message
    
    * misc: use token defined padding and borderRadius instead of magic number
    
    * fix: remove unused theme, token variables
    
    * refactor: split validation modal as a separate modal
    
    * refactor: split service validation modal as a separate component
    
    * fix: close server-sent event when service validation modal closed
    
    * fix: set to Ready status when opened
    
    * fix: close server-sent event when routing of service validation set in pending status more than 5 sec
    
    * fix: manually add vfolderName when validation regardless of service creation
    
    * fix: linting error
    
    * refactor: ServiceValidation Component
    
    * rename to `ServiceValidationView`
    
    * refactor: remove unused code and move handler code to JSX
    
    * refactor: use message instead of assign App for notification
    
    * misc: replace endpoint check for conditional rendering on ServiceLauncherModal with simpler expression
    
    * misc: simplify endpoint check for conditional rendering in ServiceLauncherModal
    
    * backward compatibility
    
    * specify z-index for nested modals.
    
    * fix lint
    
    ---------
    
    Co-authored-by: Jong Eun Lee <jongeun@lablup.com>
    Co-authored-by: Sujin Kim <kimsujin@lablup.com>

---
+++ react/src/components/ServiceValidationView.tsx
@@ -0,0 +1,205 @@
+import { baiSignedRequestWithPromise, compareNumberWithUnits } from '../helper';
+import { useCurrentDomainValue, useSuspendedBackendaiClient } from '../hooks';
+import { useTanMutation } from '../hooks/reactQueryAlias';
+import Flex from './Flex';
+import FlexActivityIndicator from './FlexActivityIndicator';
+import {
+  ServiceCreateType,
+  ServiceLauncherFormValue,
+} from './ServiceLauncherModal';
+import ValidationStatusTag from './ValidationStatusTag';
+import { AnsiUp } from 'ansi_up';
+import { App, Tag, theme } from 'antd';
+import dayjs from 'dayjs';
+import _ from 'lodash';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+interface ServiceValidationModalProps {
+  serviceData: any;
+}
+
+const ServiceValidationView: React.FC<ServiceValidationModalProps> = ({
+  serviceData,
+}) => {
+  const { t } = useTranslation();
+  const { token } = theme.useToken();
+  const { message } = App.useApp();
+  const [validationStatus, setValidationStatus] = useState('');
+  const [containerLogSummary, setContainerLogSummary] = useState('');
+  const [validationTime, setValidationTime] = useState('Before validation');
+  const baiClient = useSuspendedBackendaiClient();
+  const currentDomain = useCurrentDomainValue();
+
+  async function getLogs(sessionId: string) {
+    return baiClient
+      .get_logs(sessionId, baiClient._config.accessKey, 0)
+      .then((req: any) => {
+        const ansi_up = new AnsiUp();
+        const logs = ansi_up.ansi_to_html(req.result.logs);
+        return logs;
+      });
+  }
+
+  const mutationsToValidateService = useTanMutation<
+    unknown,
+    {
+      message?: string;
+    },
+    ServiceLauncherFormValue
+  >({
+    mutationFn: (values) => {
+      const image: string = `${values.environments.image?.registry}/${values.environments.image?.name}:${values.environments.image?.tag}`;
+      const body: ServiceCreateType = {
+        name: values.serviceName,
+        desired_session_count: values.desiredRoutingCount,
+        image: image,
+        architecture: values.environments.image?.architecture as string,
+        group: baiClient.current_group, // current Project Group,
+        domain: currentDomain, // current Domain Group,
+        cluster_size: values.cluster_size,
+        cluster_mode: values.cluster_mode,
+        open_to_public: values.openToPublic,
+        config: {
+          model: values.vFolderName,
+          model_mount_destination: '/models', // FIXME: hardcoded. change it with option later
+          environ: {}, // FIXME: hardcoded. change it with option later
+          scaling_group: values.resourceGroup,
+          resources: {
+            // FIXME: manually convert to string since server-side only allows [str,str] tuple
+            cpu: values.resource.cpu.toString(),
+            mem: values.resource.mem,
+            ...(values.resource.accelerator > 0
+              ? {
+                  [values.resource.acceleratorType]:
+                    values.resource.accelerator,
+                }
+              : undefined),
+          },
+          resource_opts: {
+            shmem:
+              compareNumberWithUnits(values.resource.mem, '4g') > 0 &&
+              compareNumberWithUnits(values.resource.shmem, '1g') < 0
+                ? '1g'
+                : values.resource.shmem,
+          },
+        },
+      };
+
+      return baiSignedRequestWithPromise({
+        method: 'POST',
+        url: '/services/_/try',
+        body,
+        client: baiClient,
+      });
+    },
+  });
+
+  const isRunningRef = useRef<boolean>(false);
+  useEffect(() => {
+    if (isRunningRef.current) return;
+
+    const validationDateTime = dayjs().format('LLL');
+
+    let sse: EventSource | undefined = undefined;
+
+    mutationsToValidateService
+      .mutateAsync(serviceData)
+      .then((data: any) => {
+        setValidationTime(validationDateTime);
+        // setContainerLogSummary('loading...');
+        const response = data;
+        sse = baiClient.maintenance.attach_background_task(response['task_id']);
+
+        // TODO:
+        const timeoutId = setTimeout(() => {
+          sse?.close();
+          // something went wrong during validation
+          setValidationStatus('error');
+          message.error(t('modelService.CannotValidateNow'));
+        }, 10000);
+
+        sse?.addEventListener('bgtask_updated', async (e) => {
+          const data = JSON.parse(e['data']);
+          const msg = JSON.parse(data.message);
+          if (['session_started', 'session_terminated'].includes(msg.event)) {
+            const logs = await getLogs(msg.session_id);
+            setContainerLogSummary(logs);
+            clearTimeout(timeoutId);
+            // temporally close sse manually when session is terminated
+            if (msg.event === 'session_terminated') {
+              sse?.close();
+              return;
+            }
+          }
+          setValidationStatus('processing');
+        });
+        sse?.addEventListener('bgtask_done', async (e) => {
+          setValidationStatus('finished');
+          clearTimeout(timeoutId);
+          sse?.close();
+        });
+        sse?.addEventListener('bgtask_failed', async (e) => {
+          const data = JSON.parse(e['data']);
+          const msg = JSON.parse(data.message);
+          const logs = await getLogs(msg.session_id);
+          setContainerLogSummary(logs);
+          setValidationStatus('error');
+          sse?.close();
+          throw new Error(e['data']);
+        });
+        sse?.addEventListener('bgtask_cancelled', async (e) => {
+          setValidationStatus('error');
+          sse?.close();
+          throw new Error(e['data']);
+        });
+      })
+      .catch((error) => {
+        message.error(
+          error?.message
+            ? _.truncate(error?.message, { length: 200 })
+            : t('modelService.FormValidationFailed'),
+        );
+      })
+      .finally(() => {
+        isRunningRef.current = false;
+      });
+    isRunningRef.current = true;
+
+    return () => {
+      sse?.close();
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Suspense fallback={<FlexActivityIndicator />}>
+      <Flex direction="row" justify="between" align="center">
+        <h3>{t('modelService.Result')}</h3>
+        <ValidationStatusTag status={validationStatus}></ValidationStatusTag>
+      </Flex>
+      <Flex direction="row" justify="between" align="center">
+        <h3>{t('modelService.TimeStamp')}</h3>
+        <Tag>{validationTime}</Tag>
+      </Flex>
+      <h3>{t('modelService.SeeContainerLogs')}</h3>
+      <Flex
+        direction="column"
+        justify="start"
+        align="start"
+        style={{
+          overflowX: 'scroll',
+          color: 'white',
+          backgroundColor: 'black',
+          padding: token.paddingSM,
+          borderRadius: token.borderRadius,
+        }}
+      >
+        <pre dangerouslySetInnerHTML={{ __html: containerLogSummary }} />
+      </Flex>
+    </Suspense>
+  );
+};
+
+export default ServiceValidationView;
--- src/lib/backend.ai-client-esm.ts
+++ src/lib/backend.ai-client-esm.ts
@@ -690,6 +690,7 @@ class Client {
       this._features['model-store'] = true;
       this._features['per-user-image'] = true;
       this._features['user-committed-image'] = true;
+      this._features['model-service-validation'] = true;
     }
   }
 
