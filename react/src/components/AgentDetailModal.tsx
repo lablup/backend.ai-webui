6d812645a9e3e02fb129c4e5f584ada00fb08d62
Sujin Kim <kimsujin@lablup.com>
2024-05-24 14:11:27 +0900

    style: set footer to null and styling AgentDetailModal (#2424)
    Set footer to null and modify gutter of `AgentDetailModal`.
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/2HueYSdFvL8pOB5mgrUQ/2e8c7d8c-38d5-45ba-8249-d74ab54ac13b.png)
    
    
    ---
    
    <!--
    Please precisely, concisely, and concretely describe what this PR changes, the rationale behind codes,
    and how it affects the users and other developers.
    -->
    
    **Checklist:** (if applicable)
    
    - [ ] Mention to the original issue
    - [ ] Documentation
    - [ ] Minium required manager version
    - [ ] Specific setting for review (eg., KB link, endpoint or how to setup)
    - [ ] Minimum requirements to check during review
    - [ ] Test case(s) to demonstrate the difference of before/after

--- react/src/components/AgentDetailModal.tsx
+++ react/src/components/AgentDetailModal.tsx
@@ -4,7 +4,7 @@ import BAIModal, { BAIModalProps } from './BAIModal';
 import BAIProgressWithLabel from './BAIProgressWithLabel';
 import Flex from './Flex';
 import { AgentDetailModalFragment$key } from './__generated__/AgentDetailModalFragment.graphql';
-import { Col, Row, Typography } from 'antd';
+import { Col, Row, theme, Typography } from 'antd';
 import graphql from 'babel-plugin-relay/macro';
 import _ from 'lodash';
 import React from 'react';
@@ -31,6 +31,7 @@ const AgentDetailModal: React.FC<AgentDetailModalProps> = ({
   ...modalProps
 }) => {
   const { t } = useTranslation();
+  const { token } = theme.useToken();
   const [resourceSlotsDetails] = useResourceSlotsDetails();
   const agent = useFragment(
     graphql`
@@ -51,162 +52,170 @@ const AgentDetailModal: React.FC<AgentDetailModalProps> = ({
   return (
     <BAIModal
       {...modalProps}
+      styles={{
+        body: {
+          padding: 0,
+        },
+      }}
       centered
       title={`${t('agent.DetailedInformation')}: ${agent?.id}`}
       onCancel={onRequestClose}
       destroyOnClose
-      footer={<></>}
+      footer={null}
     >
-      <Row gutter={[16, 16]}>
-        <Col xs={{ flex: '100%' }} sm={{ flex: '50%' }}>
-          {parsedLiveStat?.devices?.cpu_util ? (
-            <Flex direction="column" gap="xxs" align="stretch">
-              <Typography.Title level={5}>
-                {resourceSlotsDetails?.cpu?.human_readable_name}
-              </Typography.Title>
-              {_.map(parsedLiveStat?.devices?.cpu_util, (value, key) => (
-                <Flex justify="between">
-                  <Typography.Text
-                    key={key}
-                    type="secondary"
-                    style={{ flex: 0.5 }}
-                  >
-                    {resourceSlotsDetails?.cpu?.human_readable_name}
-                    {key}
+      <Flex direction="column" align="stretch" gap={'md'}>
+        <Row gutter={[24, 24]}>
+          <Col xs={24} sm={12}>
+            {parsedLiveStat?.devices?.cpu_util ? (
+              <Flex direction="column" gap="xxs" align="stretch">
+                <Typography.Title level={5} style={{ marginTop: 0 }}>
+                  {resourceSlotsDetails?.cpu?.human_readable_name}
+                </Typography.Title>
+                {_.map(parsedLiveStat?.devices?.cpu_util, (value, key) => (
+                  <Flex justify="between">
+                    <Typography.Text
+                      key={key}
+                      type="secondary"
+                      style={{ flex: 0.5 }}
+                    >
+                      {resourceSlotsDetails?.cpu?.human_readable_name}
+                      {key}
+                    </Typography.Text>
+                    <BAIProgressWithLabel
+                      percent={value?.pct}
+                      valueLabel={value?.pct + '%'}
+                    />
+                  </Flex>
+                ))}
+              </Flex>
+            ) : null}
+          </Col>
+          <Col xs={24} sm={12}>
+            {parsedAvailableSlots?.mem ? (
+              <Flex direction="column" gap="xxs" align="stretch">
+                <Typography.Title level={5} style={{ marginTop: 0 }}>
+                  {resourceSlotsDetails?.mem?.human_readable_name}
+                </Typography.Title>
+                <BAIProgressWithLabel
+                  percent={
+                    ((iSizeToSize(_.toString(agent?.mem_cur_bytes), 'g')
+                      ?.number ?? 0) /
+                      (iSizeToSize(parsedAvailableSlots?.mem, 'g')?.number ??
+                        0)) *
+                      100 ?? 0
+                  }
+                  valueLabel={`${
+                    iSizeToSize(_.toString(agent?.mem_cur_bytes), 'g')
+                      ?.numberUnit
+                  }iB / ${iSizeToSize(parsedAvailableSlots?.mem, 'g')?.numberUnit}iB`}
+                />
+              </Flex>
+            ) : null}
+            {parsedLiveStat?.node ? (
+              <Flex direction="column" gap="xxs" align="start">
+                <Typography.Title level={5} style={{ marginTop: 0 }}>
+                  {t('session.launcher.Network')}
+                </Typography.Title>
+                <Flex gap="xl">
+                  <Typography.Text>TX:</Typography.Text>
+                  <Typography.Text>
+                    {
+                      iSizeToSize(parsedLiveStat?.node?.net_tx?.current, 'm', 1)
+                        ?.numberUnit
+                    }
+                    iB
                   </Typography.Text>
-                  <BAIProgressWithLabel
-                    percent={value?.pct}
-                    valueLabel={value?.pct + '%'}
-                  />
                 </Flex>
-              ))}
-            </Flex>
-          ) : null}
-        </Col>
-        <Col xs={{ flex: '100%' }} sm={{ flex: '50%' }}>
-          {parsedAvailableSlots?.mem ? (
-            <Flex direction="column" gap="xxs" align="stretch">
-              <Typography.Title level={5}>
-                {resourceSlotsDetails?.mem?.human_readable_name}
-              </Typography.Title>
-              <BAIProgressWithLabel
-                percent={
-                  ((iSizeToSize(_.toString(agent?.mem_cur_bytes), 'g')
-                    ?.number ?? 0) /
-                    (iSizeToSize(parsedAvailableSlots?.mem, 'g')?.number ??
-                      0)) *
-                    100 ?? 0
-                }
-                valueLabel={`${
-                  iSizeToSize(_.toString(agent?.mem_cur_bytes), 'g')?.numberUnit
-                }iB / ${iSizeToSize(parsedAvailableSlots?.mem, 'g')?.numberUnit}iB`}
-              />
-            </Flex>
-          ) : null}
-          {parsedLiveStat?.node ? (
-            <Flex direction="column" gap="xxs" align="start">
-              <Typography.Title level={5}>
-                {t('session.launcher.Network')}
-              </Typography.Title>
-              <Flex gap="xl">
-                <Typography.Text>TX:</Typography.Text>
-                <Typography.Text>
-                  {
-                    iSizeToSize(parsedLiveStat?.node?.net_tx?.current, 'm', 1)
-                      ?.numberUnit
-                  }
-                  iB
-                </Typography.Text>
+                <Flex gap="xl">
+                  <Typography.Text>RX:</Typography.Text>
+                  <Typography.Text>
+                    {
+                      iSizeToSize(parsedLiveStat?.node?.net_rx?.current, 'm', 1)
+                        ?.numberUnit
+                    }
+                    iB
+                  </Typography.Text>
+                </Flex>
               </Flex>
-              <Flex gap="xl">
-                <Typography.Text>RX:</Typography.Text>
-                <Typography.Text>
-                  {
-                    iSizeToSize(parsedLiveStat?.node?.net_rx?.current, 'm', 1)
-                      ?.numberUnit
-                  }
-                  iB
-                </Typography.Text>
-              </Flex>
-            </Flex>
-          ) : null}
-        </Col>
-      </Row>
-      <Row gutter={[16, 16]}>
-        {_.map(_.keys(parsedLiveStat?.devices), (key) => {
-          if (['cpu_util', 'mem', 'disk', 'net_rx', 'net_tx'].includes(key)) {
-            return null;
-          } else if (_.includes(key, '_util')) {
-            const deviceName = _.split(key, '_')[0] + '.device';
-            return (
-              <Col xs={{ flex: '100%' }} sm={{ flex: '50%' }}>
-                <Flex direction="column" gap="xxs" align="stretch">
-                  <Typography.Title level={5}>
-                    {resourceSlotsDetails?.[deviceName]?.human_readable_name}{' '}
-                    {t('session.Utilization')}
-                  </Typography.Title>
-                  {_.map(
-                    _.toPairs(parsedLiveStat?.devices[key]),
-                    (value, index) => (
-                      <Flex justify="between">
-                        <Typography.Text
-                          key={index}
-                          type="secondary"
-                          style={{ flex: 0.5 }}
-                        >
-                          {
-                            resourceSlotsDetails?.[deviceName]
-                              ?.human_readable_name
-                          }
-                          {index}
-                        </Typography.Text>
-                        <BAIProgressWithLabel
-                          percent={_.toFinite((value?.[1] as LiveStat)?.pct)}
-                          valueLabel={(value?.[1] as LiveStat)?.pct + '%'}
-                        />
-                      </Flex>
-                    ),
-                  )}
-                </Flex>
-              </Col>
-            );
-          } else if (_.includes(key, '_mem')) {
-            const deviceName = _.split(key, '_')[0] + '.device';
-            return (
-              <Col xs={{ flex: '100%' }} sm={{ flex: '50%' }}>
-                <Flex direction="column" gap="xxs" align="stretch">
-                  <Typography.Title level={5}>
-                    {resourceSlotsDetails?.[deviceName]?.human_readable_name}{' '}
-                    {t('session.launcher.Memory')}
-                  </Typography.Title>
-                  {_.map(
-                    _.toPairs(parsedLiveStat?.devices[key]),
-                    (value, index) => (
-                      <Flex justify="between">
-                        <Typography.Text
-                          key={index}
-                          type="secondary"
-                          style={{ flex: 0.5 }}
-                        >
-                          {
-                            resourceSlotsDetails?.[deviceName]
-                              ?.human_readable_name
-                          }
-                          {index}
-                        </Typography.Text>
-                        <BAIProgressWithLabel
-                          percent={_.toFinite((value?.[1] as LiveStat)?.pct)}
-                          valueLabel={(value?.[1] as LiveStat)?.pct + '%'}
-                        />
-                      </Flex>
-                    ),
-                  )}
-                </Flex>
-              </Col>
-            );
-          }
-        })}
-      </Row>
+            ) : null}
+          </Col>
+        </Row>
+        <Row gutter={[24, 24]} style={{ marginBottom: token.marginSM }}>
+          {_.map(_.keys(parsedLiveStat?.devices), (key) => {
+            if (['cpu_util', 'mem', 'disk', 'net_rx', 'net_tx'].includes(key)) {
+              return null;
+            } else if (_.includes(key, '_util')) {
+              const deviceName = _.split(key, '_')[0] + '.device';
+              return (
+                <Col xs={24} sm={12}>
+                  <Flex direction="column" gap="xxs" align="stretch">
+                    <Typography.Title level={5} style={{ marginTop: 0 }}>
+                      {resourceSlotsDetails?.[deviceName]?.human_readable_name}{' '}
+                      {t('session.Utilization')}
+                    </Typography.Title>
+                    {_.map(
+                      _.toPairs(parsedLiveStat?.devices[key]),
+                      (value, index) => (
+                        <Flex justify="between">
+                          <Typography.Text
+                            key={index}
+                            type="secondary"
+                            style={{ flex: 0.5 }}
+                          >
+                            {
+                              resourceSlotsDetails?.[deviceName]
+                                ?.human_readable_name
+                            }
+                            {index}
+                          </Typography.Text>
+                          <BAIProgressWithLabel
+                            percent={_.toFinite((value?.[1] as LiveStat)?.pct)}
+                            valueLabel={(value?.[1] as LiveStat)?.pct + '%'}
+                          />
+                        </Flex>
+                      ),
+                    )}
+                  </Flex>
+                </Col>
+              );
+            } else if (_.includes(key, '_mem')) {
+              const deviceName = _.split(key, '_')[0] + '.device';
+              return (
+                <Col xs={24} sm={12}>
+                  <Flex direction="column" gap="xxs" align="stretch">
+                    <Typography.Title level={5} style={{ marginTop: 0 }}>
+                      {resourceSlotsDetails?.[deviceName]?.human_readable_name}{' '}
+                      {t('session.launcher.Memory')}
+                    </Typography.Title>
+                    {_.map(
+                      _.toPairs(parsedLiveStat?.devices[key]),
+                      (value, index) => (
+                        <Flex justify="between">
+                          <Typography.Text
+                            key={index}
+                            type="secondary"
+                            style={{ flex: 0.5 }}
+                          >
+                            {
+                              resourceSlotsDetails?.[deviceName]
+                                ?.human_readable_name
+                            }
+                            {index}
+                          </Typography.Text>
+                          <BAIProgressWithLabel
+                            percent={_.toFinite((value?.[1] as LiveStat)?.pct)}
+                            valueLabel={(value?.[1] as LiveStat)?.pct + '%'}
+                          />
+                        </Flex>
+                      ),
+                    )}
+                  </Flex>
+                </Col>
+              );
+            }
+          })}
+        </Row>
+      </Flex>
     </BAIModal>
   );
 };
