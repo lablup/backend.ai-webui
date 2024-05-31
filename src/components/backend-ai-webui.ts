d7ade2ce9bedcc237b60f57b402b294064a50edb
SungChul Hong <hsc6263@gmail.com>
2024-05-31 13:39:00 +0900

    refactor: start menu in summary page items

--- src/components/backend-ai-webui.ts
+++ src/components/backend-ai-webui.ts
@@ -418,7 +418,6 @@ export default class BackendAIWebUI extends connect(store)(LitElement) {
   }
 
   loadConfig(config): void {
-    console.log('!');
     if (
       typeof config.general !== 'undefined' &&
       'siteDescription' in config.general
