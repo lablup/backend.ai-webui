88112afa3b1b3678205d6a9bdb013cded20fcdab
SungChul Hong <hsc6263@gmail.com>
2024-10-17 18:22:19 +0900

    feat: migrate user list in user credential page to react component

--- src/backend-ai-app.ts
+++ src/backend-ai-app.ts
@@ -123,10 +123,6 @@ const loadPage =
       case 'storage-settings':
         import('./components/backend-ai-storage-host-settings-view.js');
         break;
-      case 'credential':
-      case 'user':
-        import('./components/backend-ai-credential-view.js');
-        break;
       case 'environment':
         import('./components/backend-ai-environment-view.js');
         break;
