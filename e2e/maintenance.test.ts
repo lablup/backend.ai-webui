5c5c23fd90f05c967660f2c04dc1bd68ba13b492
nowgnuesLee <seungwon@lablup.com>
2025-01-23 22:55:21 +0900

    feature: enhance maintenance page tests with notification checks

--- e2e/maintenance.test.ts
+++ e2e/maintenance.test.ts
@@ -1,7 +1,44 @@
 import { loginAsAdmin } from './test-util';
-import test from '@playwright/test';
+import {
+  getNotificationDescriptionBox,
+  getNotificationMessageBox,
+} from './test-util-antd';
+import { test, expect } from '@playwright/test';
 
-test.beforeAll(async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await loginAsAdmin(page);
-  await page.getByRole('menuitem', { name: '' });
+  await page.getByRole('menuitem', { name: 'Maintenance' }).click();
+});
+
+test.describe('test maintenance page', () => {
+  test.describe('Recalculate Usage', () => {
+    test('click the Recalculate Usage button', async ({ page }) => {
+      await page.getByRole('button', { name: 'Recalculate Usage' }).click();
+
+      await expect(getNotificationMessageBox(page)).toContainText(
+        'Recalculate Usage',
+      );
+      // skip the Recalculating message because it's too fast
+      await expect(getNotificationDescriptionBox(page)).toContainText(
+        'Recalculation finished',
+      );
+    });
+  });
+
+  test.describe('Rescan Images', () => {
+    test('click the Rescan Images button', async ({ page }) => {
+      await page.getByRole('button', { name: 'Rescan Images' }).click();
+
+      await expect(getNotificationMessageBox(page)).toContainText(
+        'Rescan Images',
+      );
+
+      await expect(getNotificationDescriptionBox(page)).toContainText(
+        'Rescanning...',
+      );
+      await expect(getNotificationDescriptionBox(page)).toContainText(
+        'Rescanning image finished.',
+      );
+    });
+  });
 });
