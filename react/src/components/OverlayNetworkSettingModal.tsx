2d75439d6dd880798e8d51fc9fb0c4b45aab9d7c
nowgnuesLee <seungwon@lablup.com>
2025-01-26 18:26:27 +0900

    refactor: change configurations items into settinglist items

---
+++ react/src/components/OverlayNetworkSettingModal.tsx
@@ -0,0 +1,93 @@
+import BAIModal, { BAIModalProps } from './BAIModal';
+import { NetworkOptions } from './ConfigurationsSettingList';
+import Flex from './Flex';
+import FormItemWithCheckbox from './FormItemWithCheckbox';
+import { InfoCircleOutlined } from '@ant-design/icons';
+import { Button, Form, InputNumber, theme, Tooltip } from 'antd';
+import { FormInstance } from 'antd/lib';
+import { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+
+interface OverlayNetworkSettingsModalProps extends BAIModalProps {
+  onRequestClose: () => void;
+  networkOptions: NetworkOptions;
+  onSave: (value: { [key: keyof NetworkOptions]: string }) => void;
+  onDelete: (key: string) => void;
+}
+
+const OverlayNetworkSettingModal = ({
+  onRequestClose,
+  open,
+  networkOptions,
+  onSave,
+  onDelete,
+}: OverlayNetworkSettingsModalProps) => {
+  const { t } = useTranslation();
+  const { token } = theme.useToken();
+  const formRef = useRef<FormInstance>(null);
+
+  return (
+    <BAIModal
+      open={open}
+      title={
+        <Flex gap="xs">
+          {t('settings.OverlayNetworkSettings')}
+          <Tooltip title={t('settings.OverlayNetworkSettingsDescription')}>
+            <InfoCircleOutlined
+              style={{
+                color: token.colorTextTertiary,
+              }}
+            />
+          </Tooltip>
+        </Flex>
+      }
+      onCancel={onRequestClose}
+      centered
+      width={'auto'}
+      footer={[
+        <Button key="overlayNetworkClose" onClick={onRequestClose}>
+          {t('button.Cancel')}
+        </Button>,
+        <Button
+          key="overlayNetworkSave"
+          onClick={() => {
+            if (formRef.current) {
+              formRef.current
+                .validateFields()
+                .then((values) => {
+                  Object.entries(values).forEach(([key, value]) => {
+                    if (value === null || !value || value === '') {
+                      onDelete(key);
+                    } else {
+                      onSave({ [key]: value } as {
+                        [key: keyof NetworkOptions]: string;
+                      });
+                    }
+                  });
+                  onRequestClose();
+                })
+                .catch(() => {});
+            }
+          }}
+          type="primary"
+        >
+          {t('button.Save')}
+        </Button>,
+      ]}
+      destroyOnClose
+    >
+      <Form ref={formRef} layout="vertical" initialValues={networkOptions}>
+        <FormItemWithCheckbox
+          label="MTU"
+          tooltip={t('settings.MTUDescription')}
+          required
+          name="mtu"
+        >
+          <InputNumber min={0} max={15000} style={{ width: '100%' }} />
+        </FormItemWithCheckbox>
+      </Form>
+    </BAIModal>
+  );
+};
+
+export default OverlayNetworkSettingModal;
