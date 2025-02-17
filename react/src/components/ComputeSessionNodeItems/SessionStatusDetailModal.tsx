0d6c2a4bf2b1af14b057c90b10c6037f57bc2145
SungChul Hong <hsc6263@gmail.com>
2025-02-17 18:06:52 +0900

    feat: Introduce session status info modal in session detail panel

---
+++ react/src/components/ComputeSessionNodeItems/SessionStatusDetailModal.tsx
@@ -0,0 +1,11 @@
+import { ModalProps } from 'antd';
+
+interface SessionStatusDetailModalProps extends ModalProps {}
+
+const SessionStatusDetailModal: React.FC<
+  SessionStatusDetailModalProps
+> = () => {
+  return <div>123</div>;
+};
+
+export default SessionStatusDetailModal;
