a4d63586538cca099ff0a1edb61ff0b140f09b82
Jong Eun Lee <jongeun@lablup.com>
2025-02-20 13:50:41 +0800

    refactor: Endpoint list loading state management

--- react/src/pages/ServingPage.tsx
+++ react/src/pages/ServingPage.tsx
@@ -1,50 +1,53 @@
+import BAICard from '../components/BAICard';
+import BAIFetchKeyButton from '../components/BAIFetchKeyButton';
 import Flex from '../components/Flex';
 import { filterEmptyItem } from '../helper';
-import { Card, Skeleton, theme } from 'antd';
+import { useUpdatableState, useWebUINavigate } from '../hooks';
+import { ReloadOutlined } from '@ant-design/icons';
+import { Button, Card, Skeleton, theme } from 'antd';
 import React, { Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 import { StringParam, useQueryParam, withDefault } from 'use-query-params';
 
-// FIXME: need to apply filtering type of service later
-type TabKey = 'services' | 'chatting'; //  "running" | "finished" | "others";
 
-const EndpointListPage = React.lazy(() => import('../components/EndpointList'));
+const EndpointList = React.lazy(() => import('../components/EndpointList'));
 
 interface ServingPageProps {}
 
-const tabParam = withDefault(StringParam, 'services');
 
 const ServingPage: React.FC<ServingPageProps> = ({ ...props }) => {
   const { t } = useTranslation();
   const { token } = theme.useToken();
-  const [curTabKey, setCurTabKey] = useQueryParam('tab', tabParam, {
-    updateType: 'replace',
-  });
+  const webuiNavigate = useWebUINavigate();
 
-  const tabList = filterEmptyItem([
-    { key: 'services', label: t('modelService.Services') },
-    // FIXME: need to apply filtering type of service later
-    // {
-    //   key: "running",
-    //   label: t("session.Running"),
-    // },
-    // {
-    //   key: "finished",
-    //   label: t("session.Finished"),
-    // },
-    // {
-    //   key: "others",
-    //   label: t("session.Others"),
-    // },
-  ]);
+  const [fetchKey, updateFetchKey] = useUpdatableState('initial-fetch');
+
   return (
     <Flex direction="column" align="stretch" gap={'md'}>
-      <Card
-        activeTabKey={curTabKey}
-        onTabChange={(key) => {
-          setCurTabKey(key as TabKey);
-        }}
-        tabList={tabList}
+      <BAICard
+        title={t('modelService.Services')}
+        extra={
+          <Flex direction="row" gap={'xs'}>
+            <Flex gap={'xs'}>
+              <BAIFetchKeyButton
+                value={fetchKey}
+                onChange={() => {
+                  updateFetchKey();
+                  // startRefetchTransition(() => updateServicesFetchKey());
+                }}
+                autoUpdateDelay={7000}
+              />
+              <Button
+                type="primary"
+                onClick={() => {
+                  webuiNavigate('/service/start');
+                }}
+              >
+                {t('modelService.StartService')}
+              </Button>
+            </Flex>
+          </Flex>
+        }
         styles={{
           body: {
             padding: 0,
@@ -53,18 +56,19 @@ const ServingPage: React.FC<ServingPageProps> = ({ ...props }) => {
           },
         }}
       >
-        {curTabKey === 'services' ? (
-          <Suspense
-            fallback={<Skeleton active style={{ padding: token.paddingMD }} />}
-          >
-            <EndpointListPage
-              style={{
-                padding: token.paddingMD,
-              }}
-            />
-          </Suspense>
-        ) : null}
-      </Card>
+        <Suspense
+          fallback={<Skeleton active style={{ padding: token.paddingMD }} />}
+        >
+          <EndpointList
+            style={{
+              padding: token.paddingMD,
+            }}
+            onDeleted={() => {
+              updateFetchKey();
+            }}
+          />
+        </Suspense>
+      </BAICard>
     </Flex>
   );
 };
