e40726f6622ce97d7ebbbf1124c1505b728a68b3
agatha197 <28584164+agatha197@users.noreply.github.com>
2025-02-28 09:17:38 +0000

    feat(FR-512): NEO storage status panel (#3176)
    resolves #3150 (FR-512)
    
    >[!NOTE]
    The BAICard styles will be handled in another PR.
    
    Introduces a new storage status panel card component that displays folder statistics in a more organized layout. The panel shows:
    
    - Created folders count with user quota limits
    - Project folders count with project quota limits
    - Invited folders count
    
    The component includes translations for all supported languages and uses the BackendAI theme system for consistent styling.
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/2HueYSdFvL8pOB5mgrUQ/0b5c2ea8-1ade-4602-b3a6-05d901a14894.png)
    
    ### How to test:
    checkout #3232
    
    **Checklist:**
    - [x] Documentation
    - [ ] Minium required manager version
    - [ ] Specific setting for review
    - [x] Minimum requirements to check during review:
      - Verify folder counts display correctly
      - Confirm quota limits appear when configured
      - Check translations render properly
    - [x] Test case: Compare folder statistics between old and new panel layouts

---
+++ react/src/components/StorageStatusPanelCard.tsx
@@ -0,0 +1,125 @@
+import { useSuspendedBackendaiClient } from '../hooks';
+import { useSuspenseTanQuery } from '../hooks/reactQueryAlias';
+import { useCurrentProjectValue } from '../hooks/useCurrentProject';
+import BAICard, { BAICardProps } from './BAICard';
+import BAIPanelItem from './BAIPanelItem';
+import { StorageStatusPanelCardQuery } from './__generated__/StorageStatusPanelCardQuery.graphql';
+import { Col, Row, theme } from 'antd';
+import graphql from 'babel-plugin-relay/macro';
+import _ from 'lodash';
+import React, { useDeferredValue } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useLazyLoadQuery } from 'react-relay';
+
+interface StorageStatusPanelProps extends BAICardProps {
+  fetchKey?: string;
+}
+
+const StorageStatusPanelCard: React.FC<StorageStatusPanelProps> = ({
+  fetchKey,
+  ...cardProps
+}) => {
+  const { t } = useTranslation();
+  const { token } = theme.useToken();
+  const baiClient = useSuspendedBackendaiClient();
+  const currentProject = useCurrentProjectValue();
+  const deferredFetchKey = useDeferredValue(fetchKey);
+
+  const isExcludedCount = (status: string) => {
+    return _.includes(
+      ['delete-ongoing', 'delete-complete', 'delete-error'],
+      status,
+    );
+  };
+
+  const { data: vfolders } = useSuspenseTanQuery({
+    queryKey: ['vfolders', { deferredFetchKey }],
+    queryFn: () => {
+      return baiClient.vfolder.list(currentProject?.id);
+    },
+  });
+  const createdCount = vfolders?.filter(
+    (item: any) =>
+      item.is_owner &&
+      item.ownership_type === 'user' &&
+      !isExcludedCount(item.status),
+  ).length;
+  const projectCount = vfolders?.filter(
+    (item: any) =>
+      item.ownership_type === 'group' && !isExcludedCount(item.status),
+  ).length;
+  const invitedCount = vfolders?.filter(
+    (item: any) =>
+      !item.is_owner &&
+      item.ownership_type === 'user' &&
+      !isExcludedCount(item.status),
+  ).length;
+
+  const { user_resource_policy, project_resource_policy } =
+    useLazyLoadQuery<StorageStatusPanelCardQuery>(
+      graphql`
+        query StorageStatusPanelCardQuery($name: String!) {
+          user_resource_policy {
+            max_vfolder_count
+          }
+          project_resource_policy(name: $name) {
+            max_vfolder_count
+          }
+        }
+      `,
+      {
+        name: currentProject?.name,
+      },
+    );
+
+  return (
+    <BAICard {...cardProps} title={t('data.StorageStatus')}>
+      <Row gutter={[24, 16]}>
+        <Col
+          span={8}
+          style={{
+            borderRight: `1px solid ${token.colorBorderSecondary}`,
+            justifyItems: 'center',
+          }}
+        >
+          <BAIPanelItem
+            title={t('data.CreatedFolders')}
+            value={createdCount}
+            unit={
+              user_resource_policy?.max_vfolder_count
+                ? `/ ${user_resource_policy?.max_vfolder_count}`
+                : undefined
+            }
+          />
+        </Col>
+        <Col
+          span={8}
+          style={{
+            borderRight: `1px solid ${token.colorBorderSecondary}`,
+            justifyItems: 'center',
+          }}
+        >
+          <BAIPanelItem
+            title={t('data.ProjectFolders')}
+            value={projectCount}
+            unit={
+              project_resource_policy?.max_vfolder_count
+                ? `/ ${project_resource_policy?.max_vfolder_count}`
+                : undefined
+            }
+          />
+        </Col>
+        <Col
+          span={8}
+          style={{
+            justifyItems: 'center',
+          }}
+        >
+          <BAIPanelItem title={t('data.InvitedFolders')} value={invitedCount} />
+        </Col>
+      </Row>
+    </BAICard>
+  );
+};
+
+export default StorageStatusPanelCard;
