313313b3775c45035f8589ea92392a1fe6e4098f
Jihyun Kang <jihyunkang@lablup.com>
2025-03-17 07:38:52 +0900

    fix: remove API KEY

--- react/src/hooks/index.tsx
+++ react/src/hooks/index.tsx
@@ -541,6 +541,7 @@ type BackendAIConfig = {
   enableExtendLoginSession: boolean;
   showNonInstalledImages: boolean;
   enableInteractiveLoginAccountSwitch: boolean;
+  ngcAPIKey: string;
   debug: boolean;
   [key: string]: any;
 };
