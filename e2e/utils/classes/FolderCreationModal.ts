63a26eca90e3851083051fda0278361fb0c24b61
nowgnuesLee <seungwon@lablup.com>
2025-04-04 17:56:02 +0900

    e2e: add FolderCreationModal class for testing

---
+++ e2e/utils/classes/FolderCreationModal.ts
@@ -0,0 +1,32 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class FolderCreationModal {
+  private readonly modal: Locator;
+  constructor(page: Page) {
+    this.modal = page.locator('#create-folder-modal');
+  }
+
+  async getFolderNameInput(): Promise<Locator> {
+    const folderNameInput = this.modal
+      .locator('ant-form-item-row:has-text("Folder name")')
+      .locator('input');
+    await expect(folderNameInput).toBeVisible();
+    return folderNameInput;
+  }
+
+  async fillFolderName(folderName: string): Promise<void> {
+    const folderNameInput = await this.getFolderNameInput();
+    await folderNameInput.fill(folderName);
+    await expect(folderNameInput).toHaveValue(folderName);
+  }
+
+  async getLocationSelector(): Promise<Locator> {
+    const locationSelector = this.modal
+      .locator('ant-form-item-row:has-text("Location")')
+      .locator(
+        '.ant-form-item-control-input-content > .ant-select > .ant-select-selector',
+      );
+    await expect(locationSelector).toBeVisible();
+    return locationSelector;
+  }
+}
