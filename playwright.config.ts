d0b9aa110a3117654081e3baf54d11d57b134a36
nowgnuesLee <seungwon@lablup.com>
2025-04-04 16:13:03 +0900

    chore: change double quote to single qoute in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig, devices } from "@playwright/test";
-import * as dotenv from "dotenv";
-dotenv.config({ path: "./e2e/envs/.env.playwright" });
+import { defineConfig, devices } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: './e2e/envs/.env.playwright' });
 
 /**
  * Read environment variables from file.
@@ -13,7 +14,7 @@ dotenv.config({ path: "./e2e/envs/.env.playwright" });
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,21 +24,21 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"], locale: "en-US" },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], locale: 'en-US' },
     },
 
     // {
