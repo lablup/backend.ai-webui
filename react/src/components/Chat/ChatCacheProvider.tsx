5a7276e868f94562d58182c14696b3ff5b6697d8
MoonHyounKyoung <hkmoon@lablup.com>
2025-05-08 11:23:25 +0900

    update current conversation

--- react/src/components/Chat/ChatCacheProvider.tsx
+++ react/src/components/Chat/ChatCacheProvider.tsx
@@ -374,6 +374,7 @@ export function useConversation(conversationId: string) {
         (chatId) => cache.get(chatId) as ChatData,
       );
       resetList([...chats]);
+      conversationRef.current = conversation;
     }
   }, [conversationId, resetList, cache]);
 
