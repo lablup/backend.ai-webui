f9ef59d220c6c4d2e2ff876340b810937c50c097
MoonHyounKyoung <hkmoon@lablup.com>
2025-05-09 19:40:58 +0900

    add parameter setting

--- react/src/components/Chat/ChatHeader.tsx
+++ react/src/components/Chat/ChatHeader.tsx
@@ -4,16 +4,37 @@ import { AIAgent } from '../../hooks/useAIAgent';
 import { useBAISettingUserState } from '../../hooks/useBAISetting';
 import Flex from '../Flex';
 import AIAgentSelect from './AIAgentSelect';
-import { BAIModel } from './ChatModel';
+import { BAIModel, ChatParameter } from './ChatModel';
 import EndpointSelect, { EndpointSelectProps } from './EndpointSelect';
 import ModelSelect from './ModelSelect';
 import { ChatHeader_Endpoint$key } from './__generated__/ChatHeader_Endpoint.graphql';
-import { CloseOutlined, MoreOutlined, PlusOutlined } from '@ant-design/icons';
-import { Dropdown, Button, theme, MenuProps, Typography, Switch } from 'antd';
+import {
+  CloseOutlined,
+  ControlOutlined,
+  InfoCircleOutlined,
+  MoreOutlined,
+  PlusOutlined,
+} from '@ant-design/icons';
+import {
+  Dropdown,
+  Button,
+  theme,
+  MenuProps,
+  Typography,
+  Switch,
+  Popover,
+  Slider,
+  Row,
+  Col,
+  Collapse,
+  Alert,
+} from 'antd';
+import { createStyles } from 'antd-style';
 import graphql from 'babel-plugin-relay/macro';
+import { t } from 'i18next';
 import { isEmpty } from 'lodash';
 import { Scale as ScaleIcon, Eraser as EraserIcon } from 'lucide-react';
-import React, { startTransition, useTransition } from 'react';
+import React, { startTransition, useState, useTransition } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useFragment } from 'react-relay';
 
@@ -53,7 +74,228 @@ interface ChatHeaderProps {
   onClickDeleteChatHistory?: () => void;
   onClickClose?: () => void;
   onClickCreate?: () => void;
+  parameter: ChatParameter;
+  onChangeParameter: (parameter: ChatParameter) => void;
 }
+
+type ChatParameterChangeEventHander = (event: {
+  name: string;
+  value: number;
+}) => void;
+
+interface ChatParameterProps {
+  name: string;
+  label: string;
+  max: number;
+  min: number;
+  step: number;
+  value?: number;
+  description?: string;
+  onChange?: ChatParameterChangeEventHander;
+}
+
+const ChatParameterSlider = ({
+  name,
+  label,
+  max,
+  min,
+  step,
+  value,
+  description,
+  onChange,
+}: ChatParameterProps) => {
+  const { token } = theme.useToken();
+  const [inputValue, setInputValue] = useState(value ?? min);
+
+  return (
+    <>
+      <Row justify="space-between" style={{ width: '240px' }}>
+        <Col span={12}>
+          <Typography.Text
+            style={{ fontSize: token.fontSizeSM, marginLeft: token.marginXXS }}
+            strong={true}
+          >
+            {label}
+          </Typography.Text>
+          {description && (
+            <Popover content={description} trigger="hover">
+              <Button type="link" icon={<InfoCircleOutlined />} size="small" />
+            </Popover>
+          )}
+        </Col>
+        <Col span={4} style={{ textAlign: 'right' }}>
+          <Typography.Text
+            style={{ fontSize: token.fontSizeSM, marginRight: token.marginXXS }}
+            strong={true}
+          >
+            {inputValue}
+          </Typography.Text>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <Slider
+            min={min}
+            max={max}
+            value={inputValue}
+            onChange={(value) => {
+              onChange?.({
+                name,
+                value,
+              });
+              setInputValue(value);
+            }}
+            step={step ?? null}
+            style={{
+              marginLeft: token.marginXXS,
+              marginRight: token.marginXXS,
+            }}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+const defaultChatParameters: Record<
+  string,
+  Omit<ChatParameterProps, 'name' | 'value'>
+> = {
+  maxTokens: {
+    label: 'Max Tokens',
+    min: 0,
+    max: 16384,
+    step: 1,
+    description: t('chatui.chat.parameter.maxTokens'),
+  },
+  temperature: {
+    label: 'Temperature',
+    min: 0.0,
+    max: 1,
+    step: 0.01,
+    description: t('chatui.chat.parameter.temperature'),
+  },
+  topP: {
+    label: 'Top P',
+    min: 0.0,
+    max: 1,
+    step: 0.01,
+    description: t('chatui.chat.parameter.topP'),
+  },
+};
+
+const advancedChatParameters: Record<
+  string,
+  Omit<ChatParameterProps, 'name' | 'value'>
+> = {
+  topK: {
+    label: 'Top K',
+    min: 1,
+    max: 500,
+    step: 1,
+    description: t('chatui.chat.parameter.topK'),
+  },
+  frequencyPenalty: {
+    label: 'Frequency Penalty',
+    min: 0,
+    max: 2,
+    step: 0.01,
+    description: t('chatui.chat.parameter.frequencyPenalty'),
+  },
+  presencePenalty: {
+    label: 'Presence Penalty',
+    min: 0,
+    max: 2,
+    step: 0.01,
+    description: t('chatui.chat.parameter.presencePenalty'),
+  },
+};
+
+const useStyles = createStyles(({ css, token }) => ({
+  collapse: css`
+    border: none !important;
+    border-radius: 0 !important;
+    background-color: transparent !important;
+    .ant-collapse-content {
+      border-top: none !important;
+    }
+    .ant-collapse-content-box {
+      padding: 0 !important;
+    }
+    .ant-collapse-header {
+      padding-left: 0 !important;
+    }
+  `,
+}));
+
+const ChatParameterSliders = ({
+  parameter,
+  onChange,
+}: {
+  parameter: ChatParameter;
+  onChange: ChatParameterChangeEventHander;
+}) => {
+  const { styles } = useStyles();
+  return (
+    <>
+      {Object.entries(defaultChatParameters).map(([paramName, param]) => (
+        // @FIXME: use factory function
+        <ChatParameterSlider
+          key={paramName}
+          name={paramName}
+          value={parameter[paramName as keyof ChatParameter]}
+          onChange={onChange}
+          {...param}
+        />
+      ))}
+      <Collapse
+        items={[
+          {
+            key: 'Advanced Parameters',
+            label: 'Advanced Parameters',
+            style: {
+              backgroundColor: 'transparent',
+              border: 'none',
+              borderRadius: 'none',
+            },
+            children: (
+              <>
+                <div>
+                  <Alert
+                    type="warning"
+                    showIcon
+                    message={
+                      // @FIXME: use t
+                      'Supported settings may vary depending on the model.'
+                    }
+                    style={{
+                      textWrap: 'wrap',
+                      maxWidth: '240px',
+                      marginBottom: '8px',
+                    }}
+                  />
+                </div>
+                {Object.entries(advancedChatParameters).map(
+                  ([paramName, param]) => (
+                    <ChatParameterSlider
+                      key={paramName}
+                      name={paramName}
+                      // @FIXME: without type cast
+                      value={parameter[paramName as keyof ChatParameter]}
+                      onChange={onChange}
+                      {...param}
+                    />
+                  ),
+                )}
+              </>
+            ),
+          },
+        ]}
+        className={styles.collapse}
+      />
+    </>
+  );
+};
 
 const ChatHeader: React.FC<ChatHeaderProps> = ({
   showCompareMenuItem,
@@ -63,7 +305,6 @@ const ChatHeader: React.FC<ChatHeaderProps> = ({
   onChangeModel,
   endpointFrgmt,
   onChangeEndpoint,
-  agents,
   agent,
   onChangeAgent,
   sync,
@@ -72,6 +313,8 @@ const ChatHeader: React.FC<ChatHeaderProps> = ({
   onClickCreate,
   fetchKey,
   onClickDeleteChatHistory,
+  parameter,
+  onChangeParameter,
 }) => {
   const { t } = useTranslation();
   const { token } = theme.useToken();
@@ -199,6 +442,25 @@ const ChatHeader: React.FC<ChatHeaderProps> = ({
             }}
           />
         )}
+        <Popover
+          content={
+            <ChatParameterSliders
+              parameter={parameter}
+              onChange={({ name, value }) => {
+                startTransition(() => {
+                  onChangeParameter({
+                    ...parameter,
+                    [name]: value,
+                  });
+                });
+              }}
+            />
+          }
+          trigger="click"
+          placement="bottom"
+        >
+          <Button icon={<ControlOutlined />} />
+        </Popover>
         <Button onClick={() => onClickCreate?.()} icon={<PlusOutlined />} />
         <Dropdown menu={{ items }} trigger={['click']}>
           <Button
