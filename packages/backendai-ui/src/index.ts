c3c6d3d65391dbc697fe20fe55279537d7d61410
nowgnuesLee <192685612+nowgnuesLee@users.noreply.github.com>
2025-06-05 08:12:06 +0000

    feat(FR-955): move Flex component to backend.ai-ui project (#3640)
    resolves #3618 [(FR-955)](https://lablup.atlassian.net/browse/FR-955)
    <!-- replace NNN, MMM with the GitHub issue number and the corresponding Jira issue number. -->
    
    <!--
    Please precisely, concisely, and concretely describe what this PR changes, the rationale behind codes,
    and how it affects the users and other developers.
    -->
    
    In this PR, move custom `Flex` component in react project to `backend.ai-ui` project
    
    **changes**
    * add `Flex` component to `backend.ai-ui` project
    * set up `storybook`
    * add stories about `Flex` component
    
    **how to test**
    1. Please go to `packages/backendai-ui` and run `pnpm build`.
    2. Run `pnpm install` in the root.
    3. Run `pnpm build:d` and `pnpm server:d`.
    4. Import `Flex` from `backend.ai-ui` on Webui.
    5. If you want to see it with storybook, please run `pnpm run storybook` in `packages/backendai-ui`
    
    **Checklist:** (if applicable)
    
    - [ ] Documentation
    - [ ] Minium required manager version
    - [ ] Specific setting for review (eg., KB link, endpoint or how to setup)
    - [ ] Minimum requirements to check during review
    - [ ] Test case(s) to demonstrate the difference of before/after

--- packages/backendai-ui/src/index.ts
+++ packages/backendai-ui/src/index.ts
@@ -1,3 +1,1 @@
-// Export all components from this file
-export { default as Button } from './components/Button';
-// Add more component exports as you create them
+export { default as Flex } from './components/Flex';
