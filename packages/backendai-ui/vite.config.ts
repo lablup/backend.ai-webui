b0a2cff4fb939e92eab8711b50c429fd04ba6a37
nowgnuesLee <192685612+nowgnuesLee@users.noreply.github.com>
2025-06-05 08:12:06 +0000

    feat(FR-954) setup BAI UI package environment (#3622)
    resolves #3617 [(FR-954)](https://lablup.atlassian.net/browse/FR-954)
    In this PR, prepare the setup for the UI package of backend.ai.
    
    **changes**
    * create a `backendai-ui` project and add to `pnpm-workspace.yaml`
    * setup environment with TS and rollup.
    * add a temporary component `Button` which extended antd's Button component.
    * add `backend.ai-ui` to react's dependencies.
    * change tsconfig's target `es5` to `es2016`
    
    **how to test**
    1. Please go to `packages/backendai-ui` and run `pnpm build`.
    2. Run `pnpm install` in the root.
    3. Run `pnpm build:d` and `pnpm server:d`.
    4. Import `Button` from `backendai-ui` in webui project.
    
    ![CleanShot 2025-05-08 at 14.21.45@2x.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/xbCemO1RqqcSjEXCRK3p/c5688b52-47b9-4648-825a-d3089036f7c7.png)
    
    **Checklist:** (if applicable)
    
    - [ ] Documentation
    - [ ] Minium required manager version
    - [ ] Specific setting for review (eg., KB link, endpoint or how to setup)
    - [ ] Minimum requirements to check during review
    - [ ] Test case(s) to demonstrate the difference of before/after

---
+++ packages/backendai-ui/vite.config.ts
@@ -0,0 +1,37 @@
+import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'backend.ai-ui',
+      fileName: 'backendai-ui',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    outDir: 'dist',
+  },
+  plugins: [
+    react(),
+    dts({
+      include: ['src/**/*'],
+      exclude: ['src/__generated__/**/*', 'src/*/*/*.stories.ts'],
+      rollupTypes: true,
+    }),
+  ],
+});
--- react/src/components/FolderLink.tsx
+++ react/src/components/FolderLink.tsx
@@ -1,12 +1,14 @@
-import BAILink, { BAILinkProps } from './BAILink';
+import { FolderLink_vfolderNode$key } from '../__generated__/FolderLink_vfolderNode.graphql';
+import BAILink from './BAILink';
 import { useFolderExplorerOpener } from './FolderExplorerOpener';
-import { FolderLink_vfolderNode$key } from './__generated__/FolderLink_vfolderNode.graphql';
 import { FolderOutlined } from '@ant-design/icons';
+import { theme } from 'antd';
 import graphql from 'babel-plugin-relay/macro';
 import { useFragment } from 'react-relay';
 
-interface FolderLinkBase extends BAILinkProps {
+interface FolderLinkBase {
   showIcon?: boolean;
+  disabled?: boolean;
 }
 
 interface FolderLinkWithFragment extends FolderLinkBase {
@@ -28,9 +30,11 @@ const FolderLink = ({
   folderId,
   folderName,
   showIcon,
-  ...baiLinkProps
+  disabled,
 }: FolderLinkProps) => {
   const { generateFolderPath } = useFolderExplorerOpener();
+  const { token } = theme.useToken();
+
   const vfolderNode = useFragment(
     graphql`
       fragment FolderLink_vfolderNode on VirtualFolderNode {
@@ -43,9 +47,18 @@ const FolderLink = ({
 
   return (
     <BAILink
-      to={generateFolderPath(folderId ?? vfolderNode?.row_id ?? '')}
-      style={{ wordBreak: 'break-all' }}
-      {...baiLinkProps}
+      to={
+        disabled
+          ? undefined
+          : generateFolderPath(folderId ?? vfolderNode?.row_id ?? '')
+      }
+      style={{
+        wordBreak: 'break-all',
+        ...(disabled && {
+          color: token.colorTextDisabled,
+          cursor: 'not-allowed',
+        }),
+      }}
     >
       {showIcon && (
         <>
