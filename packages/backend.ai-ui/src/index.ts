1901a11b9b2b72c59e81717684fc8471de33feb6
nowgnuesLee <seungwon@lablup.com>
2025-06-19 14:30:19 +0900

    feat(FR-1108): set up envrionment for relay component with storybook

--- packages/backend.ai-ui/src/index.ts
+++ packages/backend.ai-ui/src/index.ts
@@ -1,7 +1,7 @@
 export { default as Flex } from './components/Flex';
 export { default as BAISessionTypeTag } from './components/fragments/BAISessionTypeTag';
 export { default as BAIConfigProvider } from './components/BAIConfigProvider';
-export { default as BAITestButton } from './components/tests/BAITestButton';
+export { default as BAITestButton } from './tests/BAITestButton';
 export { default as BAIResourceWithSteppedProgress } from './components/BAIResourceWithSteppedProgress';
 export type { BAIResourceWithSteppedProgressProps } from './components/BAIResourceWithSteppedProgress';
 export { default as BAICard } from './components/BAICard';
