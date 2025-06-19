538106ba72ec0823e42330ef858f924d966cc071
nowgnuesLee <seungwon@lablup.com>
2025-06-19 14:30:19 +0900

    feat(FR-1108): set up envrionment for relay component with storybook

---
+++ packages/backend.ai-ui/src/components/fragments/BAISessionTypeTag.stories.tsx
@@ -0,0 +1,87 @@
+import { BAISessionTypeTagStoriesQuery } from '../../__generated__/BAISessionTypeTagStoriesQuery.graphql';
+import RelayResolver from '../RelayResolver';
+import BAISessionTypeTag from './BAISessionTypeTag';
+import { Meta, StoryObj } from '@storybook/react-vite/*';
+import { graphql, useLazyLoadQuery } from 'react-relay';
+
+type Story = StoryObj<typeof BAISessionTypeTag>;
+
+const meta: Meta<typeof BAISessionTypeTag> = {
+  title: 'Fragments/BAISessionTypeTag',
+  component: BAISessionTypeTag,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+const QueryResolver = () => {
+  const { compute_session_node } =
+    useLazyLoadQuery<BAISessionTypeTagStoriesQuery>(
+      graphql`
+        query BAISessionTypeTagStoriesQuery {
+          compute_session_node(id: "test-id") {
+            ...BAISessionTypeTagFragment
+          }
+        }
+      `,
+      {},
+    );
+  return (
+    compute_session_node && (
+      <BAISessionTypeTag sessionFrgmt={compute_session_node} />
+    )
+  );
+};
+
+export const Default: Story = {
+  name: 'INTERACTIVE',
+  render: () => {
+    return (
+      <RelayResolver
+        mockResolvers={{
+          String() {
+            return 'INTERACTIVE';
+          },
+        }}
+      >
+        <QueryResolver />
+      </RelayResolver>
+    );
+  },
+};
+
+export const Batch: Story = {
+  name: 'BATCH',
+  render: () => {
+    return (
+      <RelayResolver
+        mockResolvers={{
+          String() {
+            return 'BATCH';
+          },
+        }}
+      >
+        <QueryResolver />
+      </RelayResolver>
+    );
+  },
+};
+
+export const Inference: Story = {
+  name: 'INFERENCE',
+  render: () => {
+    return (
+      <RelayResolver
+        mockResolvers={{
+          String() {
+            return 'INFERENCE';
+          },
+        }}
+      >
+        <QueryResolver />
+      </RelayResolver>
+    );
+  },
+};
+
+export default meta;
