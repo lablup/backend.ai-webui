c9640c946636ca213814dc0984fb52dcd375acb5
agatha197 <kimsujin@lablup.com>
2025-07-10 17:10:44 +0900

    feat(FR-1228): My resource usage/capacity

--- packages/backend.ai-ui/src/index.ts
+++ packages/backend.ai-ui/src/index.ts
@@ -2,3 +2,6 @@ export { default as Flex } from './components/Flex';
 export { default as BAISessionTypeTag } from './components/fragments/BAISessionTypeTag';
 export { default as BAIConfigProvider } from './components/BAIConfigProvider';
 export { default as BAITestButton } from './components/tests/BAITestButton';
+export { default as BAIResourceWithSteppedProgress } from './components/BAIResourceWithSteppedProgress';
+export { default as BAICard } from './components/BAICard';
+export type { BAICardProps } from './components/BAICard';
