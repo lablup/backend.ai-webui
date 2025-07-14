9581869290fefe957417d73ccc04ca411f5b2ac9
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-07-14 16:21:52 +0900

    e2e(FR-1221): add visual regression test for summary and session page

---
+++ e2e/visual_regression/summary_page.test.ts
@@ -0,0 +1,41 @@
+import { loginAsUser2, webuiEndpoint } from '../utils/test-util';
+import { expect, test } from '@playwright/test';
+
+test.describe.configure({ mode: 'parallel' });
+test.describe('Summary page Visual Regression Test', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.setViewportSize({
+      width: 1500,
+      height: 1200,
+    });
+    await loginAsUser2(page);
+  });
+
+  test(`Test in light mode`, async ({ page }) => {
+    await page.goto(`${webuiEndpoint}/summary`);
+    await page.waitForLoadState('networkidle');
+    await expect(page).toHaveScreenshot('summary_light.png', {
+      mask: [
+        page.locator('lablup-progress-bar'),
+        page.locator('span.percentage.start-bar'),
+        page.locator('span.percentage.end-bar'),
+      ],
+      fullPage: true,
+    });
+  });
+
+  test(`Test in dark mode`, async ({ page }) => {
+    await page.goto(`${webuiEndpoint}/summary`);
+    await page.waitForLoadState('networkidle');
+    await page.getByRole('button', { name: 'moon' }).click();
+
+    await expect(page).toHaveScreenshot('summary_dark.png', {
+      mask: [
+        page.locator('lablup-progress-bar'),
+        page.locator('span.percentage.start-bar'),
+        page.locator('span.percentage.end-bar'),
+      ],
+      fullPage: true,
+    });
+  });
+});
