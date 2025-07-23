4b1739c70132a74f47b53c228fb2d4381fd18804
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-07-23 17:39:16 +0900

    e2e(FR-1221): add visual regression test for config, information, login and maintenance page

---
+++ e2e/vrt_information.test.ts
@@ -0,0 +1,20 @@
+import { loginAsAdmin } from './utils/test-util';
+import { test, expect } from '@playwright/test';
+
+test.describe('Information page Visual Regression Test', () => {
+  test.beforeEach(async ({ page }) => {
+    await loginAsAdmin(page);
+    await page.setViewportSize({
+      width: 1500,
+      height: 1400,
+    });
+  });
+
+  test('Information page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Information' }).click();
+    await page.waitForLoadState('networkidle');
+    await expect(page).toHaveScreenshot('information_page.png', {
+      fullPage: true,
+    });
+  });
+});
