06ce45c3b3fafe213048ff6fcecc4aebf9bffd88
Chaeyeong Lee <84303489+hummingbbird@users.noreply.github.com>
2025-07-25 15:53:43 +0900

    e2e(FR-1221): add visual regression test for serving, ai agents, dashboard and my environments

---
+++ e2e/visual_regression/myenvironments/myenvironments_page.test.ts
@@ -0,0 +1,28 @@
+import { loginAsVisualRegressionUser2 } from '../../utils/test-util';
+import { expect, test } from '@playwright/test';
+
+test.describe('Summary page Visual Regression Test', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.setViewportSize({
+      width: 2000,
+      height: 1200,
+    });
+    await loginAsVisualRegressionUser2(page);
+    await page.getByRole('link', { name: 'My Environments' }).click();
+    await page.waitForLoadState('networkidle');
+  });
+
+  test(`my environments full page`, async ({ page }) => {
+    await expect(page).toHaveScreenshot('myenvironments_page.png', {
+      fullPage: true,
+    });
+  });
+
+  test('column setting modal', async ({ page }) => {
+    await page.getByRole('button', { name: 'setting' }).click();
+    const columnSettingModal = page.locator('div.ant-modal').first();
+    await expect(columnSettingModal).toHaveScreenshot(
+      'column_setting_modal.png',
+    );
+  });
+});
