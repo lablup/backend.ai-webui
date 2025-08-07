c0c9b3e07be9e9708fd5a52da0e8111fdf5ce654
nowgnuesLee <192685612+nowgnuesLee@users.noreply.github.com>
2025-08-07 05:03:23 +0000

    feat(FR-1108): set up envrionment for relay component with storybook (#3811)
    resolves #NNN [(FR-1108)](https://lablup.atlassian.net/browse/FR-1108)
    <!-- replace NNN, MMM with the GitHub issue number and the corresponding Jira issue number. -->
    
    <!--
    Please precisely, concisely, and concretely describe what this PR changes, the rationale behind codes,
    and how it affects the users and other developers.
    -->
    
    In this PR, I added `BAISessionTypeTag.stories.tsx` as an example of how to make a story for a relay component
    
    **how to test**
    * please run `pnpm storybook` on `packages/backend.ai-ui`.
    * goto [http://localhost:6006](http://localhost:6006).
    * click the `BAISessionTypeTag` on side menu.
    
    ![CleanShot 2025-06-19 at 14.30.30@2x.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/xbCemO1RqqcSjEXCRK3p/4c3c1c46-30f8-4f99-9610-85b54b597050.png)
    
    **Checklist:** (if applicable)
    
    - [ ] Documentation
    - [ ] Minium required manager version
    - [ ] Specific setting for review (eg., KB link, endpoint or how to setup)
    - [ ] Minimum requirements to check during review
    - [ ] Test case(s) to demonstrate the difference of before/after

---
+++ packages/backend.ai-ui/src/components/fragments/BAISessionTypeTag.stories.tsx
@@ -0,0 +1,81 @@
+import { BAISessionTypeTagStoriesQuery } from '../../__generated__/BAISessionTypeTagStoriesQuery.graphql';
+import RelayResolver from '../../tests/RelayResolver';
+import BAISessionTypeTag from './BAISessionTypeTag';
+import { Meta, StoryObj } from '@storybook/react-vite/*';
+import { graphql, useLazyLoadQuery } from 'react-relay';
+
+type Story = StoryObj<typeof BAISessionTypeTag>;
+
+const meta: Meta<typeof BAISessionTypeTag> = {
+  title: 'Fragments/BAISessionTypeTag',
+  component: BAISessionTypeTag,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+const QueryResolver = () => {
+  const { compute_session_node } =
+    useLazyLoadQuery<BAISessionTypeTagStoriesQuery>(
+      graphql`
+        query BAISessionTypeTagStoriesQuery {
+          compute_session_node(id: "test-id") {
+            ...BAISessionTypeTagFragment
+          }
+        }
+      `,
+      {},
+    );
+  return (
+    compute_session_node && (
+      <BAISessionTypeTag sessionFrgmt={compute_session_node} />
+    )
+  );
+};
+
+export const Default: Story = {
+  name: 'INTERACTIVE',
+  render: () => {
+    return (
+      <RelayResolver
+        mockResolvers={{
+          ComputeSessionNode: () => ({ type: 'INTERACTIVE' }),
+        }}
+      >
+        <QueryResolver />
+      </RelayResolver>
+    );
+  },
+};
+
+export const Batch: Story = {
+  name: 'BATCH',
+  render: () => {
+    return (
+      <RelayResolver
+        mockResolvers={{
+          ComputeSessionNode: () => ({ type: 'BATCH' }),
+        }}
+      >
+        <QueryResolver />
+      </RelayResolver>
+    );
+  },
+};
+
+export const Inference: Story = {
+  name: 'INFERENCE',
+  render: () => {
+    return (
+      <RelayResolver
+        mockResolvers={{
+          ComputeSessionNode: () => ({ type: 'INFERENCE' }),
+        }}
+      >
+        <QueryResolver />
+      </RelayResolver>
+    );
+  },
+};
+
+export default meta;
--- packages/backend.ai-ui/src/index.ts
+++ packages/backend.ai-ui/src/index.ts
@@ -4,3 +4,4 @@ export * from './components';
 export * from './helper';
 export * from './hooks';
 export * from './icons';
+export * from './tests';
