04c97d621ac722ce388e3f1a30d898de0f202de5
nowgnuesLee <seungwon@lablup.com>
2025-08-27 10:29:06 +0900

    feat: implementing import and delete for revision

--- packages/backend.ai-ui/src/components/fragments/index.ts
+++ packages/backend.ai-ui/src/components/fragments/index.ts
@@ -6,7 +6,30 @@ export type { BAIArtifactRevisionTableProps } from './BAIArtifactRevisionTable';
 export { default as BAIArtifactTable } from './BAIArtifactTable';
 export type { BAIArtifactTableProps } from './BAIArtifactTable';
 export { default as BAIImportArtifactModal } from './BAIImportArtifactModal';
-export type { BAIImportArtifactModalProps } from './BAIImportArtifactModal';
-
+export type {
+  BAIImportArtifactModalProps,
+  BAIImportArtifactModalArtifactFragmentKey,
+  BAIImportArtifactModalArtifactRevisionFragmentKey,
+} from './BAIImportArtifactModal';
+export { default as BAIObjectStorageSelect } from './BAIObjectStorageSelect';
+export type { BAIObjectStorageSelectProps } from './BAIObjectStorageSelect';
+export { default as BAIBucketSelect } from './BAIBucketSelect';
+export type { BAIBucketSelectProps } from './BAIBucketSelect';
 export { default as BAIPullingArtifactRevisionAlert } from './BAIPullingArtifactRevisionAlert';
 export type { BAIPullingArtifactRevisionAlertProps } from './BAIPullingArtifactRevisionAlert';
+export { default as BAIDeleteArtifactModal } from './BAIDeleteArtifactModal';
+export type {
+  BAIDeleteArtifactModalProps,
+  BAIDeleteArtifactModalArtifactFragmentKey,
+  BAIDeleteArtifactModalArtifactRevisionFragmentKey,
+} from './BAIDeleteArtifactModal';
+export { default as BAIArtifactTypeTag } from './BAIArtifactTypeTag';
+export type { BAIArtifactTypeTagProps } from './BAIArtifactTypeTag';
+export { default as BAIArtifactStatusTag } from './BAIArtifactStatusTag';
+export type { BAIArtifactStatusTagProps } from './BAIArtifactStatusTag';
+export { default as BAIArtifactDescriptions } from './BAIArtifactDescriptions';
+export type { BAIArtifactDescriptionsProps } from './BAIArtifactDescriptions';
+export { default as BAIArtifactRevisionDownloadButton } from './BAIArtifactRevisionDownloadButton';
+export type { BAIArtifactRevisionDownloadButtonProps } from './BAIArtifactRevisionDownloadButton';
+export { default as BAIArtifactRevisionDeleteButton } from './BAIArtifactRevisionDeleteButton';
+export type { BAIArtifactRevisionDeleteButtonProps } from './BAIArtifactRevisionDeleteButton';
---
+++ packages/backend.ai-ui/src/hooks/useEventNotStable.ts
@@ -0,0 +1,21 @@
+import { useCallback, useLayoutEffect, useRef } from 'react';
+
+export function useEventNotStable<Args extends unknown[], Return>(
+  handler: (...args: Args) => Return,
+) {
+  // eslint-disable-next-line
+  const handlerRef = useRef<typeof handler | undefined>(undefined);
+
+  // In a real implementation, this would run before layout effects
+  useLayoutEffect(() => {
+    handlerRef.current = handler;
+  });
+
+  // eslint-disable-next-line
+  // @ts-ignore
+  return useCallback((...args: Args) => {
+    // In a real implementation, this would throw if called during render
+    const fn = handlerRef.current;
+    return fn ? fn(...args) : undefined;
+  }, []);
+}
