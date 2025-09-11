67f3a35ae1679afbd226d0f570ef71abd22205eb
nowgnuesLee <192685612+nowgnuesLee@users.noreply.github.com>
2025-09-11 02:35:27 +0000

    feat(FR-1439): setup dayjs language settings (#4261)
    # Import and Configure dayjs Locales in BAIConfigProvider
    
    resolves #4239 (FR-1439)
    
    This PR adds support for multiple language locales in the date/time formatting by:
    
    1. Importing dayjs locale modules for various languages (German, Greek, Spanish, Finnish, French, Indonesian, Italian, Japanese, Korean, Mongolian, Malay, Polish, Portuguese, Russian, Thai, Turkish, Vietnamese, Chinese) in both BAIConfigProvider and DefaultProviders components.
    
    2. Configuring dayjs plugins (weekday, localeData, localizedFormat, relativeTime, utc, timezone, duration) in the BAIConfigProvider.
    
    3. Setting the dayjs locale to match the application's language when it changes.
    
    **Checklist:**
    
    - [ ] Documentation
    - [ ] Minium required manager version
    - [ ] Specific setting for review (eg., KB link, endpoint or how to setup)
    - [ ] Minimum requirements to check during review
    - [ ] Test case(s) to demonstrate the difference of before/after

--- packages/backend.ai-ui/src/components/provider/BAIConfigProvider/BAIConfigProvider.tsx
+++ packages/backend.ai-ui/src/components/provider/BAIConfigProvider/BAIConfigProvider.tsx
@@ -1,6 +1,34 @@
 import { BAILocale, i18n } from '../../../locale';
 import { BAIClient, BAIClientProvider } from '../BAIClientProvider';
 import { ConfigProvider, ConfigProviderProps } from 'antd';
+import dayjs from 'dayjs';
+import 'dayjs/locale/de';
+import 'dayjs/locale/el';
+import 'dayjs/locale/es';
+import 'dayjs/locale/fi';
+import 'dayjs/locale/fr';
+import 'dayjs/locale/id';
+import 'dayjs/locale/it';
+import 'dayjs/locale/ja';
+import 'dayjs/locale/ko';
+import 'dayjs/locale/mn';
+import 'dayjs/locale/ms';
+import 'dayjs/locale/pl';
+import 'dayjs/locale/pt';
+import 'dayjs/locale/pt-br';
+import 'dayjs/locale/ru';
+import 'dayjs/locale/th';
+import 'dayjs/locale/tr';
+import 'dayjs/locale/vi';
+import 'dayjs/locale/zh-cn';
+import 'dayjs/locale/zh-tw';
+import duration from 'dayjs/plugin/duration';
+import localeData from 'dayjs/plugin/localeData';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+import weekday from 'dayjs/plugin/weekday';
 import { useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
 
@@ -10,6 +38,14 @@ export interface BAIConfigProviderProps
   clientPromise: Promise<BAIClient>;
   anonymousClientFactory: (api_endpoint: string) => BAIClient;
 }
+
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+dayjs.extend(localizedFormat);
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(duration);
 
 const BAIConfigProvider = ({
   children,
@@ -21,6 +57,7 @@ const BAIConfigProvider = ({
   useEffect(() => {
     if (locale?.lang) {
       i18n.changeLanguage(locale.lang);
+      dayjs.locale(locale.lang);
     }
   }, [locale?.lang]);
   return (
