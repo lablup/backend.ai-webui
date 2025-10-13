2c7ce0bdf1192ff3c5bc5112f52844ce7e0be9e8
agatha197 <kimsujin@lablup.com>
2025-10-13 15:25:11 +0900

    fix(FR-1462): Ellipsis display UI error of a endpoint's tokens in Safari browser

--- src/components/backend-ai-session-list.ts
+++ src/components/backend-ai-session-list.ts
@@ -2010,7 +2010,7 @@ export default class BackendAISessionList extends BackendAIPage {
    * Returns agent-oriented array from session list by traversing
    *
    * @param sessionList
-   * @returns [{agent: '...', containers: ['...', '...']}]
+   * @return [{agent: '...', containers: ['...', '...']}]
    */
   static _parseAgentBasedContainers(sessionList) {
     return Object.values(
