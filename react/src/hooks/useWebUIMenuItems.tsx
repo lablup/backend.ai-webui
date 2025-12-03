d931680238eb9e53a3b174097f8dc00ef38bfed1
Sujin Kim <kimsujin@lablup.com>
2025-12-03 11:25:37 +0900

    feat(FR-1763): combine configurations and maintenance into single page.

--- react/src/hooks/useWebUIMenuItems.tsx
+++ react/src/hooks/useWebUIMenuItems.tsx
@@ -279,13 +279,6 @@ export const useWebUIMenuItems = ({
     },
     {
       label: (
-        <WebUILink to="/maintenance">{t('webui.menu.Maintenance')}</WebUILink>
-      ),
-      icon: <ToolOutlined style={{ color: token.colorInfo }} />,
-      key: 'maintenance',
-    },
-    {
-      label: (
         <WebUILink to="/information">{t('webui.menu.Information')}</WebUILink>
       ),
       icon: <InfoCircleOutlined style={{ color: token.colorInfo }} />,
