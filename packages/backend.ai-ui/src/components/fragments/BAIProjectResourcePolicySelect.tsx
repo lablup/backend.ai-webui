554e875834d0614950d13b8093a5752da71c637c
SungChul Hong <sungchul@lablup.com>
2026-03-10 22:44:42 +0900

    feat(FR-2256): integrate BAIProjectResourcePolicySelect into RBAC permission modal

--- packages/backend.ai-ui/src/components/fragments/BAIProjectResourcePolicySelect.tsx
+++ packages/backend.ai-ui/src/components/fragments/BAIProjectResourcePolicySelect.tsx
@@ -1,14 +1,19 @@
 import { BAIProjectResourcePolicySelectQuery } from '../../__generated__/BAIProjectResourcePolicySelectQuery.graphql';
 import BAISelect, { BAISelectProps } from '../BAISelect';
 import _ from 'lodash';
+import { useTranslation } from 'react-i18next';
 import { graphql, useLazyLoadQuery } from 'react-relay';
 
-export interface BAIProjectResourcePolicySelectProps
-  extends Omit<BAISelectProps, 'options'> {}
+export interface BAIProjectResourcePolicySelectProps extends Omit<
+  BAISelectProps,
+  'options'
+> {}
 
 const BAIProjectResourcePolicySelect = ({
   ...selectProps
 }: BAIProjectResourcePolicySelectProps) => {
+  'use memo';
+  const { t } = useTranslation();
   const { project_resource_policies } =
     useLazyLoadQuery<BAIProjectResourcePolicySelectQuery>(
       graphql`
@@ -25,6 +30,9 @@ const BAIProjectResourcePolicySelect = ({
 
   return (
     <BAISelect
+      placeholder={t(
+        'comp:BAIProjectResourcePolicySelect.SelectProjectResourcePolicy',
+      )}
       options={_.map(_.sortBy(project_resource_policies, 'name'), (policy) => {
         return {
           label: policy?.name,
