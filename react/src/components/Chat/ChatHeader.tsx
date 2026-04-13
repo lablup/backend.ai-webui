bc52dee521b8bc7f00826e950377f4c5d1e20173
Jong Eun Lee <jongeun@lablup.com>
2026-04-13 13:36:52 +0800

    style(FR-2545): improve endpoint select layout and detail page button

--- react/src/components/Chat/ChatHeader.tsx
+++ react/src/components/Chat/ChatHeader.tsx
@@ -217,7 +217,7 @@ const ChatHeader: React.FC<ChatHeaderProps> = ({
             }}
             value={endpoint?.endpoint_id}
             popupMatchSelectWidth={false}
-            showInfoButton
+            showDetailPageButton
           />
         )}
         {!isEmpty(models) && (
